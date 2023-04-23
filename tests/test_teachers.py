3d7b37658d02db2970a558ca4200dcc9c4f8bbaa
Denis Tadzhibaev <incinerator88@mail.ru>
2023-04-23 03:54:37 +0300

    feat: add test_fill_search_city_field

--- tests/test_teachers.py
+++ tests/test_teachers.py
@@ -1,4 +1,7 @@
 from selenium.webdriver.common.by import By
+import pytest
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 URL = 'https://openweathermap.org/'
 
@@ -11,3 +14,24 @@ def test_should_open_given_link(driver):
 def test_check_page_title(driver):
     driver.get('https://openweathermap.org/')
     assert driver.title == 'Сurrent weather and forecast - OpenWeatherMap'
+
+cities = ['New York', 'Los Angeles', 'Paris']
+
+
+@pytest.mark.parametrize('city', cities)
+def test_fill_search_city_field(driver, city):
+    driver.get('https://openweathermap.org/')
+    WebDriverWait(driver, 10).until_not(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, 'div.owm-loader-container > div')))
+    search_city_field = driver.find_element(By.CSS_SELECTOR, "input[placeholder='Search city']")
+    search_city_field.send_keys(city)
+    search_button = driver.find_element(By.CSS_SELECTOR, "button[class ='button-round dark']")
+    search_button.click()
+    search_option = WebDriverWait(driver, 15).until(EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, 'ul.search-dropdown-menu li:nth-child(1) span:nth-child(1)')))
+    search_option.click()
+    expected_city = city
+    WebDriverWait(driver, 20).until(EC.text_to_be_present_in_element(
+        (By.CSS_SELECTOR, '.grid-container.grid-4-5 h2'), city))
+    displayed_city = driver.find_element(By.CSS_SELECTOR, '.grid-container.grid-4-5 h2').text
+    assert expected_city in displayed_city
