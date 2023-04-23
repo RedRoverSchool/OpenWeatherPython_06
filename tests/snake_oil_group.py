b2759eb55555fdfe01c187960bb80a22f584a9d7
Ibragim Visitaev <abeazovsky@Ibragims-Air.lan>
2023-04-23 13:49:53 -0700

    first commit

---
+++ tests/snake_oil_group.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def test_open_page(driver):
+    driver.get('https://openweathermap.org/')
+    driver.maximize_window()
+    assert 'openweathermap' in driver.current_url
+
+
+def test_check_page_title(driver):
+    driver.get('https://openweathermap.org/')
+    assert driver.title == 'Сurrent weather and forecast - OpenWeatherMap'
+
