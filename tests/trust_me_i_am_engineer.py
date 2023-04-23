acb4506f182d0043e0b3033d10d2a30ed2a04f59
Evgeniia Demchenko <115625127+Devgeni-SQA@users.noreply.github.com>
2023-04-23 22:06:19 +0300

    Add group_file (#5)

---
+++ tests/trust_me_i_am_engineer.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+
+
+def test_nav_bar_api_title(driver):
+    driver.get('https://openweathermap.org/')
+    WebDriverWait(driver, 10).until_not(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, 'div.owm-loader-container > div')))
+    button_nav_bar_api = WebDriverWait(driver, 35).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "#desktop-menu>ul>li:nth-child(2)>a")))
+    button_nav_bar_api.click()
+    nav_bar_api_title_text = driver.find_element(By.CSS_SELECTOR, "h1[class]").text
+    assert nav_bar_api_title_text == "Weather API"
