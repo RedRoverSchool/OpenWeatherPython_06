d428d806aede2cf15084e1af47a5e93b7500bf00
Happy Strawberry <irliatuz@gmail.com>
2023-04-23 13:54:28 -0500

    Initial new group test offer 100_000

---
+++ tests/test_Group_offer_100_000.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+URL = 'https://openweathermap.org/'
+
+def test_open_page(driver):
+    driver.get('https://openweathermap.org/%27')
+    assert 'openweathermap' in driver.current_url
+    print(driver.current_url)
