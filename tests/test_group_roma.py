d9cf62581eab05acac38e7636cf78eb57895a409
okun91 <r.zinkevich1991@gmail.com>
2023-04-24 17:59:12 +0300

    Add first code

---
+++ tests/test_group_roma.py
@@ -0,0 +1,33 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+
+def test_open_page():
+    driver.get("https://openweathermap.org/")
+
+    blog_btn = WebDriverWait(driver, 15).until(EC.element_to_be_clickable(
+        (By.LINK_TEXT, "Blog")))
+    time.sleep(5)
+    blog_btn.click()
+
+    driver.switch_to.window(driver.window_handles[1])
+
+    all_btn = driver.find_element(By.LINK_TEXT, "ALL")
+    all_btn.click()
+
+    first_article = driver.find_element(By.CSS_SELECTOR, "[class=post__title]")
+    first_article.click()
+
+    cookie = driver.find_element(By.CSS_SELECTOR, "button[type='button']")
+    cookie.click()
+
+    return_btn = WebDriverWait(driver, 15).until(EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, ".return [href='/blog']")))
+    return_btn.click()
+
+    time.sleep(5)
