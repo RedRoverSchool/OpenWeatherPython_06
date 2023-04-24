6a546ba201414eba70deebb9d93010fba6b75ea0
Natalia <nsirtak@gmail.com>
2023-04-23 22:16:30 -0500

    test_verify_page_title

--- tests/test_group_100_000.py
+++ tests/test_group_100_000.py
@@ -13,13 +13,7 @@ def test_check_page_title(driver):
     driver.get('https://openweathermap.org')
     assert driver.title == 'Сurrent weather and forecast - OpenWeatherMap'
 
-def test_api_title1(driver):
-    driver.get('https://openweathermap.org/')
-    WebDriverWait(driver, 10).until_not(EC.presence_of_element_located(
-        (By.CSS_SELECTOR, 'div.owm-loader-container > div')))
-    button_nav_bar_api = WebDriverWait(driver, 35).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "#desktop-menu>ul>li:nth-child(2)>a")))
-    button_nav_bar_api.click()
-    nav_bar_api_title_text = driver.find_element(By.CSS_SELECTOR, "h1[class]").text
-    assert nav_bar_api_title_text == "Weather API"
+def test_verify_page_title(driver):
+    driver.get('https://openweathermap.org')
+    assert driver.title == 'Сurrent weather and forecast - OpenWeatherMap'
 
