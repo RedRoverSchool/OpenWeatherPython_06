101278ef06fc59020ba7d0df77c2e415b614c723
Natalya Mastyaeva <71374230+Nat754@users.noreply.github.com>
2023-04-24 15:50:52 +0400

    Test mastyaevanatalya (#26)
    * add: test_group_future_Auto_QA.py
    
    * add: test_group_future_Auto_QA.py
    
    ---------
    
    Co-authored-by: Natalya Mastyaeva <na_mastyaeva@mail.ru>

---
+++ tests/test_group_future_Auto_QA.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+import pytest
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+URL = 'https://openweathermap.org/'
+displayed_current_location = (By.CSS_SELECTOR, '.icon-current-location')
+logo = (By.XPATH, "//ul[@id='first-level-nav']/li/a/img")
+
+
+def test_should_open_url(driver):
+    driver.get(URL)
+    assert 'openweathermap' in driver.current_url
+
+
