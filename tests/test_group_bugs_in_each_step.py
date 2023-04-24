91fea769d39aa5834bbe04fb29ee89db1cde6cd0
Nikita <devil732704@gmail.com>
2023-04-24 22:33:43 +0500

    Added autotests for openweathermap

--- tests/test_group_bugs_in_each_step.py
+++ tests/test_group_bugs_in_each_step.py
@@ -1,6 +1,13 @@
+from webdriver_manager.core import driver
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+
 URL = 'https://openweathermap.org/'
 
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
 
 def test_should_open_given_link(driver):
     driver.get(URL)
@@ -9,3 +16,20 @@ def test_should_open_given_link(driver):
 def test_should_open_given_link(driver):
     driver.get(URL)
     assert 'openweathermap' in driver.current_url
+
+
+def test_open_page():
+    driver.get('https://openweathermap.org/')
+    assert 'openweathermap' in driver.current_url
+    print(driver.current_url)
+
+
+def test_check_page_title():
+    test_should_open_given_link(driver)
+    assert driver.title == 'Сurrent weather and forecast - OpenWeatherMap'
+
+
+def test_fill_search_city_field():
+    driver.get('https://openweathermap.org/')
+    search_city_field = driver.find_element(By.XPATH, "//div[@id='weather-widget']//input")
+    search_city_field.send_keys('New York')
