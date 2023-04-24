efdb449ef2445010fd2851acd9df18431e8dc2a7
natalia sirtak <nsirtak@gmail.com>
2023-04-23 22:27:29 -0500

    Test verify page  (#23)
    * added API title test
    
    * test_api_title1
    
    * test_verify_page_title

--- tests/test_group_100_000.py
+++ tests/test_group_100_000.py
@@ -1,5 +1,4 @@
 from selenium.webdriver.common.by import By
-import pytest
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -13,3 +12,8 @@ def test_check_page_title(driver):
     # function checks page title
     driver.get('https://openweathermap.org')
     assert driver.title == 'Сurrent weather and forecast - OpenWeatherMap'
+
+def test_verify_page_title(driver):
+    driver.get('https://openweathermap.org')
+    assert driver.title == 'Сurrent weather and forecast - OpenWeatherMap'
+
