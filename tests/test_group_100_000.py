042957f32afcd5396c0b7deee88c33ee3ee09371
Natalia <nsirtak@gmail.com>
2023-04-23 21:58:14 -0500

    added API title test

--- tests/test_group_100_000.py
+++ tests/test_group_100_000.py
@@ -1,5 +1,4 @@
 from selenium.webdriver.common.by import By
-import pytest
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -13,3 +12,14 @@ def test_check_page_title(driver):
     # function checks page title
     driver.get('https://openweathermap.org')
     assert driver.title == 'Сurrent weather and forecast - OpenWeatherMap'
+
+def test_api_title(driver):
+    driver.get('https://openweathermap.org/')
+    WebDriverWait(driver, 10).until_not(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, 'div.owm-loader-container > div')))
+    button_nav_bar_api = WebDriverWait(driver, 35).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "#desktop-menu>ul>li:nth-child(2)>a")))
+    button_nav_bar_api.click()
+    nav_bar_api_title_text = driver.find_element(By.CSS_SELECTOR, "h1[class]").text
+    assert nav_bar_api_title_text == "Weather API"
+
