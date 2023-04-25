e14259e1a64a802bc943ae79614eafdfaa79a6df
Jenny_Kadzova <kadzovaea@gmail.com>
2023-04-25 14:24:28 -0800

    feat: add test check_title_mp_page & new branch (#48)
    * feat: add test check_title_mp_page
    
    ---------
    
    Co-authored-by: Jenny_Kadzova <ekadzovaea@gmail.com>

--- tests/test_the_hardworking_club.py
+++ tests/test_the_hardworking_club.py
@@ -1,9 +1,19 @@
-
+import time
+from selenium import webdriver
 from selenium.webdriver.common.by import By
+import pytest
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 URL = 'https://openweathermap.org/'
+
 
 def test_should_open_given_link(driver):
     driver.get(URL)
     assert 'openweathermap' in driver.current_url
 
+
+def test_check_page_title(driver):  # check title name
+    driver.get('https://home.openweathermap.org/marketplace')
+    assert driver.title == 'Marketplace: History Bulk, History Forecast Bulk, ' \
+                           'Historical Weather Data by State for all ZIP codes, USA - OpenWeather'
