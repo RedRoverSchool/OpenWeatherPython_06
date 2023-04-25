f89de257131e6c893e63d6f5c616bba0c5260ec6
ChosenOnelru <chosenonex1@gmail.com>
2023-04-25 18:13:48 +0300

    tests added: - test_sing_in_empty_fields - test_sing_in_positive - test_open_weather_map - test_current_weather_api

--- tests/test_pandoras_box.py
+++ tests/test_pandoras_box.py
@@ -1,3 +1,4 @@
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -6,7 +7,7 @@ import requests
 test_email = 'chosenonex1@gmail.com'
 test_password = 'gNrts5W?K_.qLFu'
 API_key = '2c254a2efb0b9008ce295e94a0939a2f'
-city_name = 'Moscow'
+cities = ['Moscow', 'Paris']
 URL = 'https://openweathermap.org/'
 
 def test_open_page(driver):
@@ -35,27 +36,30 @@ def test_sing_in_positive(driver):
     alert = driver.find_element(By.XPATH, '//*[@class="panel-body"]')
     assert alert.text == 'Signed in successfully.'
 
-def test_open_weather_map(driver):
+def test_open_weather_map_empty(driver):
     driver.get(URL)
     WebDriverWait(driver, 15).until_not(EC.presence_of_element_located(
         (By.CSS_SELECTOR, 'div.owm-loader-container > div')))
-    driver.implicitly_wait(15)
-    zoom_map = driver.find_element(By.XPATH, '//a[contains(@href, "weathermap?zoom")]')
+    # driver.implicitly_wait(15)
+    zoom_map = WebDriverWait(driver, 15).until(
+        EC.presence_of_element_located((By.XPATH, '//a[contains(@href, "weathermap?zoom")]')))
+    # zoom_map = driver.find_element(By.XPATH, '//a[contains(@href, "weathermap?zoom")]')
     zoom_map.click()
     window_weathermap_zoom = driver.window_handles[1] # возвращаем дескриптор новой страницы
     driver.switch_to.window(window_weathermap_zoom) # переключаем selenium на новую страницу
     assert driver.title == 'Interactive weather maps - OpenWeatherMap'
 
-def test_current_weather_api():
-    payload = {"q": f"{city_name}",
-               "appid": f"{API_key}"}
+@pytest.mark.parametrize('city', cities)
+def test_current_weather_api(city):
+    payload = {'q': f'{city}',
+               'appid': f'{API_key}'}
     response = requests.get('https://api.openweathermap.org/data/2.5/weather', params=payload)
     response_data = response.json()
     assert response.status_code == 200
     expected_keys = ['coord', 'weather', 'main', 'id', 'name']
     for key in expected_keys:
         assert key in response_data.keys()
-    assert response.json()['name'] == city_name
+    assert response.json()['name'] == city
 
 
 
