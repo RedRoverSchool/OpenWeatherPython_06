dc6e66cc50b15b28261c9758c6b8590e08f10c75
SvetlanaIM <markelovasvetlana7@gmail.com>
2023-04-25 20:03:08 +0300

    add test_button_search_exist

--- tests/test_group_roma.py
+++ tests/test_group_roma.py
@@ -9,3 +9,9 @@ def test_should_open_given_link(driver):
     driver.get(URL)
     assert 'openweathermap' in driver.current_url
 
+
+def test_button_search_exist(driver):
+    driver.get(URL)
+    btn = driver.find_element(By.XPATH, "//button[@type='submit']")
+    assert btn.text == "Search"
+
