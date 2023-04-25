0f416de2c7fd2c66a2b906c9bd0b86053cee78d7
Iuliia Patriarkh <yuliia.patriarh14@gmail.com>
2023-04-24 21:50:09 -0500

    init: start the_hardworking_club task

--- tests/test_the_hardworking_club.py
+++ tests/test_the_hardworking_club.py
@@ -1,10 +1,61 @@
-from selenium.webdriver.common.by import By
+import requests
+import pytest
 
-URL = 'https://openweathermap.org/'
+BASE_URL = 'https://restful-booker.herokuapp.com/booking'
+AUTH_URL = 'https://restful-booker.herokuapp.com/auth'
 
 
-def test_should_open_given_link(driver):
-    driver.get(URL)
-    assert 'openweathermap' in driver.current_url
+STATUS_OK = 200
+
+@pytest.fixture(scope='module')
+def auth_token():
+    payload = {
+            "username": "admin",
+            "password": "password123"
+    }
+    response = requests.post(AUTH_URL, json=payload)
+    response_data = response.json()
+    token = response_data['token']
+    assert response.status_code == STATUS_OK
+    yield token
+
+
+@pytest.fixture(scope='module')
+def booking_id():
+    payload = {
+        "firstname": "John",
+        "lastname": "Smith",
+        "totalprice": 111,
+        "depositpaid": True,
+        "bookingdates": {
+            "checkin": "2018-01-01",
+            "checkout": "2019-01-01"
+        },
+        "additionalneeds": "Breakfast"
+    }
+    response = requests.post(BASE_URL, json=payload)
+    booking_id = response.json()['bookingid']
+    assert response.status_code == 200
+    yield booking_id
+
+
+
+def test_get_all_booking():
+    response = requests.get(BASE_URL)
+    # print(response.status_code)
+    assert response.status_code == 200
+
+    print(len(response.headers))
+
+    print(f'{response.headers}')
+    print(response.json())
+
+    headers = {'Connection', 'keep-alive'}
+    assert headers in response.headers.items()
+
+    print(f'\n{len(response.json())}')
+
+    key = 'Connection'
+    assert key in response.headers
 
 
