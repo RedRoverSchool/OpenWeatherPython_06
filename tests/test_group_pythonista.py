2f2f56896bd2368786bc92078c948fc876bf24df
Tanya Butakova <tetiana1007@outlook.com>
2023-04-25 16:25:17 -0400

    Add def for testing Guide tab and back Home button

--- tests/test_group_pythonista.py
+++ tests/test_group_pythonista.py
@@ -13,5 +13,26 @@ def test_check_page_title(driver):
     driver.get('https://openweathermap.org')
     assert driver.title == 'Сurrent weather and forecast - OpenWeatherMap'
 
-def test_python():
-    print('Hello girls!')
+
+
+def test_guide_button(driver):
+    #  testing Guide tab button
+    driver.get('https://openweathermap.org')
+    time.sleep(5)
+    tab_name_guide = driver.find_element(By.XPATH, '//div[@id="desktop-menu"]//a[contains(@href, "guide")]')
+    tab_name_guide.click()
+    assert driver.title == 'OpenWeatherMap API guide - OpenWeatherMap'
+
+
+def test_home_button(driver):
+    #  testing going back to home from Guide page
+    driver.get('https://openweathermap.org')
+    time.sleep(5)
+    tab_name_guide = driver.find_element(By.XPATH, '//div[@id="desktop-menu"]//a[contains(@href, "guide")]')
+    tab_name_guide.click()
+    time.sleep(5)
+    tab_home_link = driver.find_element(By.XPATH, '//div[@class="col-sm-5"]/ol/li/a')
+    tab_home_link.click()
+    time.sleep(5)
+    assert driver.title == 'Сurrent weather and forecast - OpenWeatherMap'
+
