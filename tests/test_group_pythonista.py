73cc65e08893faa16d67b70e5f5743ef787efaab
KatyaMy <20FamGam20@gmail.com>
2023-04-25 13:07:03 -0400

    added new tests test_checkout_menu_tab_guide;test_checkout_menu_tab_api; and created fixture(25 line)

--- tests/test_group_pythonista.py
+++ tests/test_group_pythonista.py
@@ -1,17 +1,46 @@
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+URL = "https://openweathermap.org/"
+load_div = (By.CSS_SELECTOR, 'div.owm-loader-container > div')
+bt_go_home = (By.XPATH, "//a[contains(text(),'Home')]")
+tab_guide = (By.XPATH, '//*[@id="desktop-menu"]/ul/li[1]/a')
+tab_api = (By.XPATH, '//*[@id="desktop-menu"]/ul/li[2]/a')
 
 def test_open_page(driver):
     driver.get('https://openweathermap.org/')
     driver.maximize_window()
     assert 'openweathermap' in driver.current_url
 
+
 def test_check_page_title(driver):
     # function checks page title
     driver.get('https://openweathermap.org')
     assert driver.title == 'Сurrent weather and forecast - OpenWeatherMap'
 
+
+@pytest.fixture(scope="function")
+def load_div_s(driver):
+    driver.get(URL)
+    wait = WebDriverWait(driver, 10)
+    wait.until_not(EC.presence_of_element_located(load_div))
+
+
 def test_python():
     print('Hello girls!')
+
+
+def test_checkout_menu_tab_guide(driver, load_div_s):
+    driver.find_element(*tab_guide).click()
+    assert driver.current_url == 'https://openweathermap.org/guide'
+    driver.find_element(*bt_go_home).click()
+    assert driver.current_url == URL
+
+
+def test_checkout_menu_tab_api(driver, load_div_s):
+    driver.find_element(*tab_api).click()
+    assert driver.current_url == 'https://openweathermap.org/api'
+    driver.find_element(*bt_go_home).click()
+    assert driver.current_url == URL
