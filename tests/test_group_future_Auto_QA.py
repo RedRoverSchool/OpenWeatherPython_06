cb9524d5a321bea992bed3fa16c0179784fa99fa
verafes <verafes@gmail.com>
2023-04-25 12:36:16 -0700

    ADD: test homePage Header

--- tests/test_group_future_Auto_QA.py
+++ tests/test_group_future_Auto_QA.py
@@ -12,3 +12,9 @@ def test_should_open_url(driver):
     assert 'openweathermap' in driver.current_url
 
 
+def test_home_page_header(driver):
+    driver.get(URL)
+    WebDriverWait(driver, 10).until_not(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, 'div.owm-loader-container > div')))
+    header = driver.find_element(By.CSS_SELECTOR, "h1")
+    assert header.text == "OpenWeather", "Wrong h1 Header"
