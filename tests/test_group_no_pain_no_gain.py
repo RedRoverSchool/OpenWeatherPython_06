9c6d3a0dd13b9e4bd73e3dcce994e1c19e452d0d
Elena Shorohova <eshorohova89@gmail.com>
2023-04-25 20:23:19 +0300

    Add test check search city

--- tests/test_group_no_pain_no_gain.py
+++ tests/test_group_no_pain_no_gain.py
@@ -1,4 +1,7 @@
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 URL = 'https://openweathermap.org/'
 
@@ -14,3 +17,17 @@ def test_check_logo_visibility(driver):
     driver.get(URL)
     logo = driver.find_element(By.CSS_SELECTOR, "#first-level-nav > li.logo > a > img")
     assert logo.is_displayed() == True
+
+def test_check_search_city(driver):
+    driver.get('https://openweathermap.org/')
+    search_field = driver.find_element(By.CSS_SELECTOR, 'input[placeholder="Search city"]')
+    search_field.send_keys('Moscow')
+    try:
+        driver.find_element(By.XPATH, '//button[text()="Search"]').click()
+    except NoSuchElementException:
+        raise NoSuchElementException('Поле поиска на найдено')
+    driver.implicitly_wait(10)
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, 'ul.search-dropdown-menu li:nth-child(1) span:nth-child(1)'))).click()
+    WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element(
+        (By.CSS_SELECTOR, '.grid-container.grid-4-5 h2'), "Moscow, RU"))
