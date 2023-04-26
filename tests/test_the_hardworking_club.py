2884f9a96a415c83b150a7069583c74187dd3af6
Ekaterina Zotova <ekatzotova@gmail.com>
2023-04-26 03:06:30 +0300

    feat: add test support drop down

--- tests/test_the_hardworking_club.py
+++ tests/test_the_hardworking_club.py
@@ -65,3 +65,28 @@ def test_top_bar_nav(driver):
 def test_dashboard_title(driver):
     driver.get('https://openweathermap.org/weather-dashboard')
     assert driver.title == 'Weather dashboard - OpenWeatherMap'
+
+search_dropdown = (By.CSS_SELECTOR, 'ul.search-dropdown-menu li')
+search_dropdown_option = (By.CSS_SELECTOR, 'ul.search-dropdown-menu li:nth-child(1) span:nth-child(1)')
+
+urls = [
+    'https://openweathermap.org/faq',
+    'https://openweathermap.org/appid',
+    'https://home.openweathermap.org/questions'
+]
+
+texts = [
+    'FAQ',
+    'How to start',
+    'Ask a question'
+]
+
+def test_support_drop_down(driver):
+    driver.get(URL)
+
+    el_list = driver.find_element(By.ID, "support-dropdown-menu")
+    items = el_list.find_elements(By.TAG_NAME, "li")
+    assert len(items) == 3
+    for i, el in enumerate(items):
+        assert el.find_element(By.TAG_NAME, 'a').get_attribute('href') == urls[i]
+        assert el.find_element(By.TAG_NAME, 'a').get_attribute("innerHTML") == texts[i]
