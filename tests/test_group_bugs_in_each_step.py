812df8bc51284dc8498b6dd807e94b6a86aafe3e
Elena <peregovorun@gmail.com>
2023-04-26 14:19:11 +0300

    add 1 test for buttons checking and printing

--- tests/test_group_bugs_in_each_step.py
+++ tests/test_group_bugs_in_each_step.py
@@ -13,4 +13,16 @@ def test_check_page_title(driver):
     print(f'The site title is: {driver.title}')
 
 
+def test_look_for_buttons(driver):
+    driver.get(URL)
+    search_button = driver.find_element(By.CSS_SELECTOR, "button[class='button-round dark']")
+    weather_widget_weather_button = driver.find_element(By.XPATH, "//*[contains(text(),'Different Weather')]")
+    weather_widget_metric_button = driver.find_element(By.XPATH, "//*[contains(text(),'Metric')]")
+    weather_widget_imperial_button = driver.find_element(By.XPATH, "//*[contains(text(),'Imperial')]")
+    print('\n')
+    print(f'The Search button text is: {search_button.text}')
+    print(f'The Weather button text is: {weather_widget_weather_button.text}')
+    print(f'The Metric button text is: {weather_widget_metric_button.text}')
+    print(f'The Imperial button text is: {weather_widget_imperial_button.text}')
+
 
