42b85f1edae79800e048b70956b14be1318e96ff
Alexander_Dulalaev <camel1985@mail.ru>
2023-04-26 23:35:12 +0300

    added parametrize for test

--- tests/test_group_pytest.py
+++ tests/test_group_pytest.py
@@ -1,8 +1,10 @@
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 URL = 'https://openweathermap.org/'
+cities = ['New York', 'Los Angeles', 'Paris']
 
 
 def test_should_open_given_link(driver):
@@ -16,21 +18,20 @@ def test_check_page_title(driver):
     assert driver.title == 'Сurrent weather and forecast - OpenWeatherMap'
 
 
-def test_fill_search_city_field(driver):
+@pytest.mark.parametrize('city', cities)
+def test_fill_search_city_field(driver, city):
     driver.get('https://openweathermap.org/')
     WebDriverWait(driver, 10).until_not(EC.presence_of_element_located(
         (By.CSS_SELECTOR, 'div.owm-loader-container > div')))
     search_city_field = driver.find_element(By.CSS_SELECTOR, "input[placeholder='Search city']")
-    search_city_field.send_keys('New York')
+    search_city_field.send_keys(city)
     search_button = driver.find_element(By.CSS_SELECTOR, "button[class ='button-round dark']")
     search_button.click()
     search_option = WebDriverWait(driver, 15).until((EC.element_to_be_clickable(
         (By.CSS_SELECTOR, 'ul.search-dropdown-menu li:nth-child(1) span:nth-child(1)'))))
     search_option.click()
-    expected_city = 'New York City, US'
+    expected_city = city
     WebDriverWait(driver, 20).until(EC.text_to_be_present_in_element(
-        (By.CSS_SELECTOR, '.grid-container.grid-4-5 h2'), 'New York'))
+        (By.CSS_SELECTOR, '.grid-container.grid-4-5 h2'), city))
     displayed_city = driver.find_element(By.CSS_SELECTOR, '.grid-container.grid-4-5 h2').text
-    # displayed_city_text = displayed_city.text
-    # print(displayed_city_text)
     assert displayed_city == expected_city
