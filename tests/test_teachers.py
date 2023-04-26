c0dce1d64ddb71fda9ecc4c7fc13310453c6d2c8
Maksym Maksimov <maksymmaksimov88@gmail.com>
2023-04-26 23:21:04 +0800

    Added test_check_log_in

--- tests/test_teachers.py
+++ tests/test_teachers.py
@@ -1,7 +1,20 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 URL = 'https://openweathermap.org/'
 
 
 def test_should_open_given_link(driver):
     driver.get(URL)
     assert 'openweathermap' in driver.current_url
+
+
+def test_check_log_in(driver):
+    driver.get('https://openweathermap.org/')
+    WebDriverWait(driver, 10).until_not(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, 'div.owm-loader-container > div')))
+    expected_log = 'Sign in'
+    search_option_log = WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, '.user-li a'))).text
+    assert expected_log == search_option_log
