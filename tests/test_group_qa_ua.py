bb491c1954837c269c31ac1f9fa2ec473e0a7d43
Aleksandra Kozhevnikova <aleksandra.kozhe@gmail.com>
2023-04-26 14:22:54 +0300

    change test_chack_log_in

--- tests/test_group_qa_ua.py
+++ tests/test_group_qa_ua.py
@@ -34,9 +34,9 @@ def test_fill_search_city_field(driver):
 
 def test_chack_log_in(driver):
     driver.get('https://openweathermap.org/')
-    WebDriverWait(driver, 25).until_not(EC.presence_of_element_located(
+    WebDriverWait(driver, 30).until_not(EC.presence_of_element_located(
         (By.CSS_SELECTOR, 'div.owm-loader-container > div')))
     expected_log = 'Sign in'
-    displayed_log = WebDriverWait(driver, 25).until(EC.text_to_be_present_in_element(
-        (By.CSS_SELECTOR, '#desktop-menu > ul > li.user-li > a'), 'Sign in')).text
+    displayed_log = WebDriverWait(driver, 30).until(EC.visibility_of_element_located(
+        (By.CSS_SELECTOR, '#desktop-menu > ul > li.user-li > a'))).text
     assert expected_log == displayed_log
