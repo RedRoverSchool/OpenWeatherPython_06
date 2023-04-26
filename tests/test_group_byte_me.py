31b75e6b333e5c541ef948550576eb46573a3fea
Kate <katerina091294@gmail.com>
2023-04-25 21:36:00 -0600

    Added sign in test

--- tests/test_group_byte_me.py
+++ tests/test_group_byte_me.py
@@ -1,34 +1,22 @@
-import time
-
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def test_open_page(driver):
-    driver.get('https://openweathermap.org/')
-    driver.maximize_window()
-    assert 'openweathermap' in driver.current_url
-    print(driver.current_url)
+URL = 'https://openweathermap.org/'
 
-def test_check_page_title(driver):
-    driver.get('https://openweathermap.org/')
-    assert driver.title == 'Сurrent weather and forecast - OpenWeatherMap'
 
-def test_fill_search_city_field(driver):
-    driver.get('https://openweathermap.org/')
-    search_city_field = driver.find_element(By.CSS_SELECTOR, '[placeholder="Search city"]')
-    search_city_field.send_keys('New York')
-    time.sleep(10)
-    search_button = driver.find_element(By.CSS_SELECTOR, "button[class='button-round dark']")
-    search_button.click()
-    driver.implicitly_wait(10)
-    search_option = WebDriverWait(driver, 15).until(EC.element_to_be_clickable(
-        (By.CSS_SELECTOR, "ul.search-dropdown-menu li:nth-child(1) span:nth-child(1)")))
-    search_option.click()
-    time.sleep(5)
-    expected_city = 'New York City, US'
-    displayed_city = WebDriverWait(driver, 15).until(EC.presence_of_element_located(
-        (By.CSS_SELECTOR, '.grid-container.grid-4-5 h2')))
-    displayed_city_text = displayed_city.text
-    print(displayed_city_text)
-    assert displayed_city_text == expected_city
+@pytest.fixture()
+def open_page(driver):
+    driver.get(URL)
+    assert driver.current_url == URL, "--------Wrong URL-----------"
+
+
+def test_go_to_sign_in_page(driver, open_page):
+    wait = WebDriverWait(driver, 15)
+    wait.until_not(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, 'div.owm-loader-container > div'))
+    )
+    sign_button = driver.find_element(By.CSS_SELECTOR, '.user-li>a')
+    sign_button.click()
+    assert "sign_in" in driver.current_url, "--------Wrong URL-----------"
