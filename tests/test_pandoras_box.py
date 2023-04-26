2c3901e13c440d71cc0b652c52cfc52576d2f461
VyacheslavBeliaev <vbelyaev86@gmail.com>
2023-04-26 19:44:01 +0300

    Vyacheslav Added and fixed code

--- tests/test_pandoras_box.py
+++ tests/test_pandoras_box.py
@@ -2,11 +2,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-import time
-from selenium import webdriver
+
 from selenium.webdriver import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+
 
 URL = 'https://openweathermap.org/'
 
@@ -14,11 +14,12 @@ def test_open_page(driver):
     driver.get(URL)
     assert 'openweathermap' in driver.current_url # проверка наличия строки в url
 
-def test_open_page():
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+def test_fahrenheit_click(driver):
     driver.get('https://openweathermap.org/')
     driver.maximize_window()
     temp_change = driver.find_element(By.XPATH, '//*[@id="weather-widget"]/div[1]/div/div/div[1]/div[2]')
-    time.sleep(10)
+    WebDriverWait(driver, 15).until_not(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, 'div.owm-loader-container > div')))
     ActionChains(driver).drag_and_drop_by_offset(temp_change, 72, 0).perform()
-    time.sleep(5)
+    WebDriverWait(driver, 15).until_not(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, 'div.owm-loader-container > div')))
