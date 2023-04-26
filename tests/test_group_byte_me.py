c062102765d47b861830b00043609d1ea3032d28
Kate <katerina091294@gmail.com>
2023-04-25 18:53:59 -0600

    Added first tests

---
+++ tests/test_group_byte_me.py
@@ -0,0 +1,33 @@
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def test_open_page(driver):
+    driver.get('https://openweathermap.org/')
+    driver.maximize_window()
+    assert 'openweathermap' in driver.current_url
+    print(driver.current_url)
+
+def test_check_page_title(driver):
+    driver.get('https://openweathermap.org/')
+    assert driver.title == 'Сurrent weather and forecast - OpenWeatherMap'
+
+def test_fill_search_city_field(driver):
+    search_city_field = driver.find_element(By.CSS_SELECTOR, '[placeholder="Search city"]')
+    search_city_field.send_keys('New York')
+    time.sleep(10)
+    search_button = driver.find_element(By.CSS_SELECTOR, "button[class='button-round dark']")
+    search_button.click()
+    driver.implicitly_wait(10)
+    search_option = WebDriverWait(driver, 15).until(EC.element_to_be_clickable(
+        (By.CSS_SELECTOR, "ul.search-dropdown-menu li:nth-child(1) span:nth-child(1)")))
+    search_option.click()
+    time.sleep(5)
+    expected_city = 'New York City, US'
+    displayed_city = WebDriverWait(driver, 15).until(EC.presence_of_element_located(
+        (By.CSS_SELECTOR, '.grid-container.grid-4-5 h2')))
+    displayed_city_text = displayed_city.text
+    print(displayed_city_text)
+    assert displayed_city_text == expected_city
