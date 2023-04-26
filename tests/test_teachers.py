8ca4e918186db6522143fbd9d7f3efc0d7c615c4
aigulmustafina <aimustafina2016@gmail.com>
2023-04-27 05:21:39 +0600

    ref: add actionchains for sing in element text

--- tests/test_teachers.py
+++ tests/test_teachers.py
@@ -86,6 +86,8 @@ def test_should_be_valid_title_on_price_page(driver, open_and_load_page, wait):
 def test_should_be_valid_text_in_sign_in_tab(driver, open_and_load_page, wait):
     driver.find_element(*accept_cookies).click()
     expected_text = 'Sign in'
-    wait.until(EC.text_to_be_present_in_element(sign_in_link, expected_text))
-    sign_in_text = driver.find_element(*sign_in_link).text
+    element = driver.find_element(*sign_in_link)
+    action_chains = ActionChains(driver)
+    action_chains.move_to_element(element)
+    sign_in_text = element.text
     assert sign_in_text == expected_text
