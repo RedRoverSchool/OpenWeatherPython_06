0bbba2a3aa1d4459b03fcf6266a8c5b7f599a7aa
okun91 <r.zinkevich1991@gmail.com>
2023-04-26 14:16:44 +0300

    Add time part two

--- tests/test_group_roma.py
+++ tests/test_group_roma.py
@@ -9,7 +9,7 @@ URL = "https://openweathermap.org/"
 def test_open_blog_article(driver):
     driver.get(URL)
 
-    blog_btn = WebDriverWait(driver, 15).until(EC.element_to_be_clickable(
+    blog_btn = WebDriverWait(driver, 45).until(EC.element_to_be_clickable(
         (By.LINK_TEXT, "Blog")))
     time.sleep(5)
     blog_btn.click()
@@ -20,7 +20,7 @@ def test_open_blog_article(driver):
     all_btn = driver.find_element(By.LINK_TEXT, "ALL")
     all_btn.click()
     assert "Blog" in driver.title, "Заголовок страницы не содержит слово 'Blog'"
-    time.sleep(7)
+
 
     first_article = driver.find_element(By.CSS_SELECTOR, "[class=post__title]")
     first_article.click()
