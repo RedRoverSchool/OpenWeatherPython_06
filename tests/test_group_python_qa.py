5960cc628250301aa76f442b34022dccbe0aec9f
Anastasiia Zubenko <astassia2@gmail.com>
2023-04-27 15:41:23 -0700

    fix: add more wait time from5 to 15

--- tests/test_group_python_qa.py
+++ tests/test_group_python_qa.py
@@ -96,17 +96,17 @@ def test_temperature_f_conversion(driver):
 
 def test_temperature_c_conversion(driver):
     driver.get(URL)
-    WebDriverWait(driver, 5).until_not(EC.presence_of_element_located(
+    WebDriverWait(driver, 15).until_not(EC.presence_of_element_located(
         (By.CSS_SELECTOR, 'div.owm-loader-container > div')))
-    search_city_field = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+    search_city_field = WebDriverWait(driver, 15).until(EC.element_to_be_clickable(
         (By.CSS_SELECTOR, "input[placeholder='Search city']")))
     search_city_field.send_keys(city)
     search_button = driver.find_element(By.CSS_SELECTOR, "button[class ='button-round dark']")
     search_button.click()
-    search_option = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+    search_option = WebDriverWait(driver, 15).until(EC.element_to_be_clickable(
         (By.CSS_SELECTOR, 'ul.search-dropdown-menu li:first-child span:first-child')))
     search_option.click()
-    WebDriverWait(driver, 10).until_not(EC.presence_of_element_located(
+    WebDriverWait(driver, 15).until_not(EC.presence_of_element_located(
         (By.CSS_SELECTOR, 'div.owm-loader-container > div')))
     f_temp = driver.find_element(By.CSS_SELECTOR, '.switch-container .option:nth-child(2)')
     f_temp.click()
