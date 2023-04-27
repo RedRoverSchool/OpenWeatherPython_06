6115ce61ccfcf7012b0efd540408601468b4aa80
irinagorlach <Goirinagorlach@yandex.by>
2023-04-27 23:29:11 +0300

    feat: add test_go_to_blog

--- tests/test_group_lutz_squad.py
+++ tests/test_group_lutz_squad.py
@@ -8,7 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def test_go_to_blog(driver):
     driver.get('https://openweathermap.org/')
-    WebDriverWait(driver, 10).until_not(
+    WebDriverWait(driver, 20).until_not(
         EC.presence_of_element_located((By.CSS_SELECTOR, 'div.owm-loader-container > div')))
     go_to_blog = WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, '#desktop-menu li:nth-child(9)')))
