dcbcab67e8df553507ce28d2e2fab7bb182cd6cf
irinagorlach <Goirinagorlach@yandex.by>
2023-04-28 02:38:04 +0300

    feat: add test_check_page_title

--- tests/test_group_lutz_squad.py
+++ tests/test_group_lutz_squad.py
@@ -6,9 +6,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def test_check_tab_blog(driver):
+def test_check_page_title(driver):
     driver.get('https://openweathermap.org/')
-    tab_blog = driver.find_element(By.XPATH, "//div//li/a[contains(text(), 'Blog')][1]").text
-    assert tab_blog == "Blog"
+    assert driver.title == 'Сurrent weather and forecast - OpenWeatherMap'
 
 
