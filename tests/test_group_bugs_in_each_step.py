07a8f23253371ae0ff310c2ba7cbedea95622036
Liliya <lilianew78@yahoo.com>
2023-04-27 15:44:18 +0400

    Сhange test_click_guide

--- tests/test_group_bugs_in_each_step.py
+++ tests/test_group_bugs_in_each_step.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 URL = 'https://openweathermap.org/'
 
 
@@ -16,7 +17,9 @@ def test_check_page_title(driver):
 
 def test_click_guide(driver):
     driver.get(URL)
-    search_button_guide = driver.find_element(By.XPATH, '//*[@id="desktop-menu"]/ul/li[1]/a')
-    search_button_guide.click()
+    search_link_guide = driver.find_element(By.XPATH, '//*[@id="desktop-menu"]/ul/li[1]/a')
+    action_chains = ActionChains(driver)
+    action_chains.move_to_element(search_link_guide)
+    driver.execute_script("arguments[0].click();", search_link_guide)
     assert driver.title == 'OpenWeatherMap API guide - OpenWeatherMap'
-    print(f'The page site title is: {driver.title}')
+    print(f'Website page title is: {driver.title}')
