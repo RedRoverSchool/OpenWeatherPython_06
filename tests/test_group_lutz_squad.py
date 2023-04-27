6c2d1c2224d9147e38e83c5f8c3968c9e65e9b17
irinagorlach <Goirinagorlach@yandex.by>
2023-04-28 01:13:49 +0300

    feat: add test_check_tab_blog

--- tests/test_group_lutz_squad.py
+++ tests/test_group_lutz_squad.py
@@ -8,6 +8,6 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def test_check_tab_blog(driver):
     driver.get('https://openweathermap.org/')
-    tab_blog = driver.find_element(By.CSS_SELECTOR, '#desktop-menu li:nth-child(9)')
-    assert tab_blog.text == 'Blog'
+    tab_blog = driver.find_element(By.CSS_SELECTOR, '#desktop-menu li:nth-child(9)').text
+    assert tab_blog == 'Blog'
 
