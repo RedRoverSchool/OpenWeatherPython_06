a6e50b2f5d4c1fa6ab6193e17bf3b9908eeb4e10
KetiLion <keti.lion.work@gmail.com>
2023-04-27 19:58:30 -0700

    Add test KATERYNA LADONENKO

--- tests/test_the_hardworking_club.py
+++ tests/test_the_hardworking_club.py
@@ -8,7 +8,15 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
+
+
 URL = 'https://openweathermap.org/'
+cities = ['New York', 'Los Angeles', 'Paris']
+load_div = (By.CSS_SELECTOR, 'div.owm-loader-container > div')
+displayed_city = (By.CSS_SELECTOR, '.grid-container.grid-4-5 h2')
+search_city_field = (By.CSS_SELECTOR, "input[placeholder='Search city']")
+search_button = (By.CSS_SELECTOR, "button[class ='button-round dark']")
 
 
 def test_should_open_given_link(driver):
@@ -114,7 +122,19 @@ def test_open_initiatives_title(driver):
     driver.get('https://openweathermap.org/our-initiatives')
     assert driver.title == 'Our Initiatives - OpenWeatherMap'
 
-
+@pytest.mark.parametrize('city', cities)
+def test_fill_search_city_field(driver, city):
+    driver.get('https://openweathermap.org/')
+    wait = WebDriverWait(driver, 15)
+    wait.until_not(EC.presence_of_element_located(load_div))
+    search_city_input = driver.find_element(*search_city_field)
+    search_city_input.send_keys(city)
+    driver.find_element(*search_button).click()
+    wait.until(EC.element_to_be_clickable(search_dropdown_option)).click()
+    expected_city = city
+    wait.until(EC.text_to_be_present_in_element(displayed_city, city))
+    actual_city = driver.find_element(*displayed_city).text
+    assert expected_city in actual_city
 
 
 
