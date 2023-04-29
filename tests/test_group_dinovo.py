41fbb9d480850e88714d49a74e1af48144b7f05d
Ilmira <dinovo.ilmira@gmail.com>
2023-04-29 09:29:25 -0400

    refactor code2

--- tests/test_group_dinovo.py
+++ tests/test_group_dinovo.py
@@ -8,20 +8,36 @@ def test_check_page_title(driver):
     assert driver.title == 'Сurrent weather and forecast - OpenWeatherMap'
 
 
-from selenium import webdriver
+# from selenium import webdriver
+# import pytest
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service
+# # from selenium.webdriver.common.by import By
+#
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+#
+# # def test_open_page():
+# def test_open_page(driver):
+#     driver.get('https://openweathermap.org/')
+#     driver.maximize_window()
+#     assert 'openweathermap'in driver.current_url
+#     print(driver.current_url)
+
 import pytest
+from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
-# from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+@pytest.fixture(scope="function")
+def driver():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    yield driver
+    driver.quit()
 
-# def test_open_page():
 def test_open_page(driver):
     driver.get('https://openweathermap.org/')
     driver.maximize_window()
     assert 'openweathermap'in driver.current_url
     print(driver.current_url)
-
 
 
