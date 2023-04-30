9323f9027a16494a3119b26e3c474ae502f1d5a0
aigulmustafina <aimustafina2016@gmail.com>
2023-04-30 21:42:22 +0600

    feat: add test verify_new_page_link_contains_requested_city_name

--- tests/test_teachers.py
+++ tests/test_teachers.py
@@ -16,8 +16,9 @@ sign_in_link = (By.CSS_SELECTOR, '.user-li a')
 pricing_link = (By.CSS_SELECTOR, '#desktop-menu a[href="/price"]')
 price_page_title = (By.CSS_SELECTOR, "h1[class='breadcrumb-title']")
 accept_cookies = (By.CSS_SELECTOR, 'button.stick-footer-panel__link')
-
-
+weather_in_your_city = (By.CSS_SELECTOR, "#desktop-menu input[placeholder='Weather in your city']")
+search_in_header = (By.CSS_SELECTOR, "#desktop-menu form[role='search']")
+city_query = (By.CSS_SELECTOR, '#search_str')
 def test_should_open_given_link(driver):
     driver.get(URL)
     assert 'openweathermap' in driver.current_url
@@ -89,3 +90,11 @@ def test_should_be_valid_text_in_sign_in_tab(driver, open_and_load_page, wait):
     element = driver.find_element(*sign_in_link)
     sign_in_text = driver.execute_script("return arguments[0].textContent", element)
     assert sign_in_text == expected_text
+
+
+def test_verify_new_page_link_contains_requested_city_name(driver, open_and_load_page, wait):
+    query = 'Florida'
+    search_city = driver.find_element(*weather_in_your_city)
+    search_city.send_keys(query)
+    search_city.submit()
+    assert query in driver.current_url
