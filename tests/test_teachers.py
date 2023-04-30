9c4cca5bbc79607978c682d9451a0d5c92251bde
civilizatr <chepurinda@gmail.com>
2023-04-30 22:50:32 +0300

    commented some tests

--- tests/test_teachers.py
+++ tests/test_teachers.py
@@ -1,7 +1,13 @@
-from selenium.webdriver.common.by import By
+иfrom selenium.webdriver.common.by import By
 URL = 'https://openweathermap.org/'
 
 
 def test_should_open_given_link(driver):
     driver.get(URL)
     assert 'openweathermap' in driver.current_url
+
+def test_open_page(driver):
+    driver.get('https://openweathermap.org/')
+    driver.maximize_window()
+    assert 'openweathermap' in driver.current_url
+    print(driver.current_url)
