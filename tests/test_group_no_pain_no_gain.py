f143a0f679181607d6e84da84412919e46d03445
PetrMurzin <petr.t.murzin@gmail.com>
2023-04-30 14:58:46 +0300

    test_authorization

--- tests/test_group_no_pain_no_gain.py
+++ tests/test_group_no_pain_no_gain.py
@@ -2,13 +2,13 @@ import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+
 
 URL = 'https://openweathermap.org/'
 WRONG_LOGIN = 'error@gmail.com'
 WRONG_PASSWORD = 'error'
+correct_login = 'murzin199593@gmail.com'
+correct_password = '1234567890'
 SIGN_IN_PAGE = 'https://home.openweathermap.org/users/sign_in'
 
 def test_should_open_given_link(driver):
@@ -42,20 +42,15 @@ def test_wrong_login_password(driver):
     driver.find_element(By.XPATH, "//div[@class='panel-heading']"), 'NO ALERT'
     driver.find_element(By.XPATH, "//*[contains(text(), 'Invalid Email or password.')]")
 
-browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-url1 = 'https://home.openweathermap.org/users/sign_in'
-
-user_name1 = 'murzin199593@gmail.com'
-password1 = '1234567890'
-def test_authorization():
-    browser.get(url1)
-    user_name = browser.find_element(By.XPATH, "//input[@id='user_email']")
-    user_name.send_keys(user_name1)
-    password = browser.find_element(By.XPATH, "//input[@id='user_password']")
-    password.send_keys(password1)
-    submit_btn = browser.find_element(By.XPATH, "//input[@name='commit']")
+def test_authorization(driver):
+    driver.get(SIGN_IN_PAGE)
+    user_name = driver.find_element(By.XPATH, "//input[@id='user_email']")
+    user_name.send_keys(correct_login)
+    password = driver.find_element(By.XPATH, "//input[@id='user_password']")
+    password.send_keys(correct_password)
+    submit_btn = driver.find_element(By.XPATH, "//input[@name='commit']")
     submit_btn.click()
-    notice = browser.find_element(By.XPATH, "//*[@class='panel-body']")
+    notice = driver.find_element(By.XPATH, "//*[@class='panel-body']")
     value_notice = notice.text
     assert value_notice == 'Signed in successfully.'
 
