b5333cbda538f6bbbdcfbb8d2706547ac2d1a51f
Mack <mack@Macks-MacBook-Pro.local>
2023-05-01 13:05:14 -0700

    change selector second page

--- tests/test_group_snake_oil.py
+++ tests/test_group_snake_oil.py
@@ -124,7 +124,7 @@ def test_fill_upper_search_field(driver):
     search_city.send_keys(city)
     actions = ActionChains(driver)
     actions.send_keys(Keys.ENTER).perform()
-    link_city = driver.find_element(By.XPATH, '//tbody/tr[1]/td[2]/b[1]/a[1]')
+    link_city = driver.find_element(By.CSS_SELECTOR, 'tbody tr:first-child a[href*='/city/']')
     link_city.click()
     expected_city = 'Almaty, KZ'
     WebDriverWait(driver, 20).until(EC.text_to_be_present_in_element(
