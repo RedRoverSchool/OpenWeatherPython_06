cb16822eb9babda1f276c9d67a8aea51edc68c4c
KetiLion <128004051+KetiLion@users.noreply.github.com>
2023-04-30 19:48:33 -0700

    Add test KATERYNA LADONENKO (#195)

--- tests/test_the_hardworking_club.py
+++ tests/test_the_hardworking_club.py
@@ -9,7 +9,15 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
+
+
+
 URL = 'https://openweathermap.org/'
+cities = ['New York', 'Los Angeles', 'Paris']
+load_div = (By.CSS_SELECTOR, 'div.owm-loader-container > div')
+displayed_city = (By.CSS_SELECTOR, '.grid-container.grid-4-5 h2')
+search_city_field = (By.CSS_SELECTOR, "input[placeholder='Search city']")
+search_button = (By.CSS_SELECTOR, "button[class ='button-round dark']")
 
 
 def test_should_open_given_link(driver):
@@ -116,6 +124,21 @@ def test_open_initiatives_title(driver):
     assert driver.title == 'Our Initiatives - OpenWeatherMap'
 
 
+
+@pytest.mark.parametrize('city', cities)
+def test_fill_search_city_field(driver, city):
+    driver.get('https://openweathermap.org/')
+    wait = WebDriverWait(driver, 15)
+    wait.until_not(EC.presence_of_element_located(load_div))
+    search_city_input = driver.find_element(*search_city_field)
+    search_city_input.send_keys(city)
+    driver.find_element(*search_button).click()
+    wait.until(EC.element_to_be_clickable(search_dropdown_option)).click()
+    expected_city = city
+    wait.until(EC.text_to_be_present_in_element(displayed_city, city))
+    actual_city = driver.find_element(*displayed_city).text
+    assert expected_city in actual_city
+
 def test_open_marketplace(driver):
     driver.get('https://openweathermap.org/')
     element = driver.find_element(By.CSS_SELECTOR, 'div#desktop-menu a[href*=marketplace]')
@@ -129,9 +152,11 @@ def test_open_marketplace(driver):
     assert 'history_bulks/new' in driver.current_url
 
 
+
 def test_blog_title(driver):
     driver.get('https://openweather.co.uk/blog/category/weather')
     assert driver.title == 'Blog - OpenWeatherMap'
+
 
 
 
