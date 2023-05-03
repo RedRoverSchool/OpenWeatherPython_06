ca88e81bd657c1335421ebde7b8980a4ad4e7468
Andrey Bozhko <556070@gmail.com>
2023-05-03 13:59:45 +0300

    TC_001.02.01 | Main page > Search city widget > Switching temperature measuring system > Verify temperature switched on metric system

--- tests/test_group_trust_me_i_am_engineer.py
+++ tests/test_group_trust_me_i_am_engineer.py
@@ -6,6 +6,7 @@ load_div = (By.CSS_SELECTOR, 'div.owm-loader-container > div')
 metric_button_loc = (By.XPATH, "//div[@class='switch-container']/div[contains(text(), 'Metric')]")
 current_temp_loc = (By.CSS_SELECTOR, "div.current-temp span.heading")
 
+
 def test_TC_001_02_01_verify_temperature_switched_on_metric_system(driver, open_and_load_main_page, wait):
     driver.find_element(*metric_button_loc).click()
     wait.until_not(EC.presence_of_element_located(load_div))
