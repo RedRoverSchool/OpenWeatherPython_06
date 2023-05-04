e35a6f112f2b703e23fc212f2ff3f4419f2e4dc0
blinkova-mv <blinkova.mar@gmail.com>
2023-05-04 22:27:34 +0300

    TC_002.02.04 | Header > Search field > Search city >Verify displaying entered city name in "Search" field on search results page

--- tests/test_pandoras_box.py
+++ tests/test_pandoras_box.py
@@ -1,9 +1,16 @@
+from selenium.webdriver import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+
 
 URL = 'https://openweathermap.org/'
 BUTTON_PRICING = (By.XPATH, '//div[@id="desktop-menu"]//a[text()="Pricing"]')
 DISPLAYED_TITLE = (By.CSS_SELECTOR, 'h1.breadcrumb-title')
+FIELD_WEATHER_IN_YUOR_CITY = (By.CSS_SELECTOR, "#desktop-menu input[placeholder='Weather in your city']")
+ALERT_NOTIFICATION = (By.CSS_SELECTOR, "#forecast_list_ul .alert.alert-warning")
+STRING_ENTERED_CITY = (By.CSS_SELECTOR, "#search_str")
 
 def test_TC_002_03_08_open_pricing(driver):
     driver.get(URL)
@@ -14,3 +21,14 @@ def test_TC_002_03_08_open_pricing(driver):
     expected_title = "Pricing"
     displayed_title = driver.find_element(*DISPLAYED_TITLE).text
     assert displayed_title == expected_title
+
+def test_TC_002_02_04_verify_displaying_entered_city_name_in_Search_field(driver, open_and_load_main_page, wait):
+    search_weather_in_your_city = driver.find_element(*FIELD_WEATHER_IN_YUOR_CITY)
+    entered_city_name = "LJKJJ"
+    search_weather_in_your_city.send_keys(entered_city_name)
+    actions = ActionChains(driver)
+    actions.send_keys(Keys.ENTER).perform()
+    wait.until(EC.presence_of_element_located(ALERT_NOTIFICATION) )
+    search_result_city_name = driver.find_element(*STRING_ENTERED_CITY)
+    found_city = search_result_city_name.get_property("value")
+    assert found_city == entered_city_name
