b3199d69b41d31adc8f1a82248e32693f0b30f9b
ChosenOnelru <chosenonex1@gmail.com>
2023-05-04 17:46:03 +0300

    TC_001.09.06 | Main page > Widgets constructor > Interacting with widgets' selection > Verify temperature switched on Fahrenheit

--- tests/test_pandoras_box.py
+++ tests/test_pandoras_box.py
@@ -6,16 +6,6 @@ URL = 'https://openweathermap.org/'
 BUTTON_PRICING = (By.XPATH, '//div[@id="desktop-menu"]//a[text()="Pricing"]')
 DISPLAYED_TITLE = (By.CSS_SELECTOR, 'h1.breadcrumb-title')
 
-def test_TC_002_03_08_open_pricing(driver):
-    driver.get(URL)
-    button_pricing = driver.find_element(*BUTTON_PRICING)
-    action_chains = ActionChains(driver)
-    action_chains.move_to_element(button_pricing)
-    driver.execute_script("arguments[0].click();", button_pricing)
-    expected_title = "Pricing"
-    displayed_title = driver.find_element(*DISPLAYED_TITLE).text
-    assert displayed_title == expected_title
-
 logo_locator = (By.XPATH, '//*[@class="logo"]/a/img')
 URLs = ['https://openweathermap.org/',
         'https://openweathermap.org/guide',
@@ -28,6 +18,16 @@ URLs = ['https://openweathermap.org/',
         'https://openweathermap.org/faq',
         'https://openweathermap.org/appid',
         'https://home.openweathermap.org/questions']
+
+def test_TC_002_03_08_open_pricing(driver):
+    driver.get(URL)
+    button_pricing = driver.find_element(*BUTTON_PRICING)
+    action_chains = ActionChains(driver)
+    action_chains.move_to_element(button_pricing)
+    driver.execute_script("arguments[0].click();", button_pricing)
+    expected_title = "Pricing"
+    displayed_title = driver.find_element(*DISPLAYED_TITLE).text
+    assert displayed_title == expected_title
 
 @pytest.mark.parametrize('URL', URLs)
 def test_TC_002_01_03_Logo_is_visible(driver, wait, URL):
