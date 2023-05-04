c4b019825c32e39986be39f573cc598bf91ddaea
chemkomaksim <maksim.chemka@teladochealth.com>
2023-05-04 14:25:41 +0300

    TC_001.01.02/01 | Main page > Search city widget > Verify requested city name displayed on widget by city name search

--- conftest.py
+++ conftest.py
@@ -1,9 +1,16 @@
 import os
 import pytest
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+URL = 'https://openweathermap.org/'
+load_div = (By.CSS_SELECTOR, 'div.owm-loader-container > div')
+
 
 @pytest.fixture(scope='function')
 def driver():
@@ -19,3 +26,15 @@ def driver():
     yield driver
     print('\nquit browser...')
     driver.quit()
+
+
+@pytest.fixture()
+def open_and_load_main_page(driver, wait):
+    driver.get(URL)
+    wait.until_not(EC.presence_of_element_located(load_div))
+
+
+@pytest.fixture()
+def wait(driver):
+    wait = WebDriverWait(driver, 25)
+    yield wait
