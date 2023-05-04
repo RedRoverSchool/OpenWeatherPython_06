bb8cc0de325f3b27d7121922db3089fb6ebe9f90
ChosenOnelru <chosenonex1@gmail.com>
2023-05-04 13:37:09 +0300

    TC_002.01.03 > Header > Logo > Verify Logo is visible from all pages specified in data

--- tests/test_pandoras_box.py
+++ tests/test_pandoras_box.py
@@ -1,7 +1,7 @@
+import pytest
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
+logo_locator = (By.XPATH, '//*[@class="logo"]/a/img')
 URLs = ['https://openweathermap.org/',
         'https://openweathermap.org/guide',
         'https://openweathermap.org/api',
@@ -14,11 +14,8 @@ URLs = ['https://openweathermap.org/',
         'https://openweathermap.org/appid',
         'https://home.openweathermap.org/questions']
 
-def test_TC_002_01_03_Logo_is_visible(driver):
-    for URL in URLs:
-        driver.get(URL)
-        WebDriverWait(driver, 15).until_not(EC.presence_of_element_located(
-        (By.CSS_SELECTOR, 'div.owm-loader-container > div')))
-        logo = driver.find_element(By.XPATH, '//*[@class="logo"]/a/img')
-        assert logo.is_displayed() == True, "Logo is not visible"
-        assert logo.get_attribute("width") != 0, "Logo is not loaded"
+@pytest.mark.parametrize('URL', URLs)
+def test_TC_002_01_03_Logo_is_visible(driver, wait, URL):
+    driver.get(URL)
+    logo = driver.find_element(*logo_locator)
+    assert logo.is_displayed(), "Logo is not visible"
