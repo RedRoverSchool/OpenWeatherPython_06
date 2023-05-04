506c3d8932a594e7f0d3bdbd053b1afbec3a828b
Anastasiya Pronina <apronina@piesoft.us>
2023-05-04 09:10:47 +0300

    TC_002.03.08 | Header > Links > Navigation > "Pricing" link leads to the correct page (code refactoring)

--- tests/test_pandoras_box.py
+++ tests/test_pandoras_box.py
@@ -7,10 +7,10 @@ DISPLAYED_TITLE = (By.CSS_SELECTOR, 'h1.breadcrumb-title')
 
 def test_TC_002_03_08_open_pricing(driver):
     driver.get(URL)
-    button_pricing = driver.find_element(BUTTON_PRICING)
+    button_pricing = driver.find_element(*BUTTON_PRICING)
     action_chains = ActionChains(driver)
     action_chains.move_to_element(button_pricing)
     driver.execute_script("arguments[0].click();", button_pricing)
     expected_title = "Pricing"
-    displayed_title = driver.find_element(DISPLAYED_TITLE).text
+    displayed_title = driver.find_element(*DISPLAYED_TITLE).text
     assert displayed_title == expected_title
