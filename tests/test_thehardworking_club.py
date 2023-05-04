1a09fa5453f9001d63a1f2e450a0cdf2e81b3728
GinaG <tgungaadorj@gmail.com>
2023-05-03 19:55:20 -0700

    fix: change per requirement

--- tests/test_thehardworking_club.py
+++ tests/test_thehardworking_club.py
@@ -1,18 +1,12 @@
-import os
-import pytest
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
+
 URL = 'https://openweathermap.org/widgets-constructor'
+api_key = (By.XPATH, "//input[@id='api-key']")
+city_name = (By.CSS_SELECTOR, "#city-name")
 
-
-def test_YourAPIKey_YourCityName_fields_visible(driver):
+def test_TC_001_09_04_YourAPIKey_YourCityName_fields_visible(driver):
     driver.get(URL)
-    driver.maximize_window()
-    your_api_key = driver.find_element(By.XPATH, "//input[@id='api-key']")
-    your_city_name = driver.find_element(By.CSS_SELECTOR, "#city-name")
-    print(f'Your API Key field is displayed: {your_api_key.is_displayed()}')
-    print(f'Your City Name field is displayed: {your_city_name.is_displayed()}')
+    your_api_key = driver.find_element(*api_key)
+    your_city_name = driver.find_element(*city_name)
+    assert your_api_key.is_displayed() and your_city_name.is_displayed()
