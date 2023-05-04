af1782a4756cd51d0fb7099a3595b58b5620b1b6
Taskvo <tatyana.skvortsova@triafly.ru>
2023-05-04 15:13:04 +0300

    US_013.04

--- tests/test_group_python13.py
+++ tests/test_group_python13.py
@@ -5,3 +5,10 @@ URL = 'https://openweathermap.org/'
 def test_should_open_given_link(driver):
     driver.get(URL)
     assert 'openweathermap' in driver.current_url
+
+def test_click_by_platform_leads_to_correct_page(driver):
+    driver.get('https://openweather.co.uk/blog/')
+    search_button = driver.find_element(By.LINK_TEXT, "PLATFORM")
+    search_button.click()
+    driver.implicitly_wait(10)
+    assert driver.find_element(By.XPATH, "//*[contains(text(), 'The World Data League (WDL)')]")
