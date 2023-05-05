ac971021004c778e5dd383b1619a7772fcd4aa15
Abe-Kid <v.ibragim13@gmail.com>
2023-05-05 12:54:49 -0700

    TC_003.10.06 | Footer > Footer > Social media module > Icons-links visibility and сlickability > Verify the LinkedIn-link is visible

--- tests/test_group_snake_oil.py
+++ tests/test_group_snake_oil.py
@@ -1,13 +1,9 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-HOME_PAGE_URL = "https://openweathermap.org/"
-LOADING_VEIL = (By.CSS_SELECTOR, "div[aria-label='Loading']")
 linkedIn_icon = (By.CSS_SELECTOR, "div[class='social'] a:nth-child(3)")
 
 
 def test_tc_003_10_06_verify_linkedIn_link_is_visible(driver, open_and_load_main_page, wait):
-    driver.maximize_window()
     element = wait.until(EC.visibility_of_element_located(linkedIn_icon))
     assert element.is_displayed(), "LinkedIn interactive icon is not visible on a page"
