5d4cefc044fdaaff21120790960517fae4396693
ChosenOnelru <chosenonex1@gmail.com>
2023-05-05 11:42:07 +0300

    TC_003.03.01 | Footer > Product Collections module > Visibility, structure, links сlickability > Verify the module title visibility on all pages specified in data

--- tests/test_pandoras_box.py
+++ tests/test_pandoras_box.py
@@ -1,9 +1,23 @@
+import pytest
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 
 URL = 'https://openweathermap.org/'
 BUTTON_PRICING = (By.XPATH, '//div[@id="desktop-menu"]//a[text()="Pricing"]')
 DISPLAYED_TITLE = (By.CSS_SELECTOR, 'h1.breadcrumb-title')
+
+title_locator = (By.XPATH, '//p[text()="Product Collections"]')
+URLs = ['https://openweathermap.org/',
+        'https://openweathermap.org/guide'
+        'https://openweathermap.org/api',
+        'https://openweathermap.org/weather-dashboard',
+        'https://openweathermap.org/price',
+        'https://openweathermap.org/our-initiatives',
+        'https://openweathermap.org/examples',
+        'https://home.openweathermap.org/users/sign_in',
+        'https://openweathermap.org/faq',
+        'https://openweathermap.org/appid',
+        'https://home.openweathermap.org/questions']
 
 def test_TC_002_03_08_open_pricing(driver):
     driver.get(URL)
@@ -14,3 +28,10 @@ def test_TC_002_03_08_open_pricing(driver):
     expected_title = "Pricing"
     displayed_title = driver.find_element(*DISPLAYED_TITLE).text
     assert displayed_title == expected_title
+
+
+@pytest.mark.parametrize('URL', URLs)
+def test_TC_003_03_01_Product_Collections_title_is_visible(driver, URL):
+    driver.get(URL)
+    module_title = driver.find_element(*title_locator)
+    assert module_title.is_displayed(), "Product Collections title is not visible"
