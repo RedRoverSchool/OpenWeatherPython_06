23fe95b752c27329fe165cf7480280079a9b28d6
NadyaSenyukova <nadyasenv86@gmail.com>
2023-05-06 00:06:27 +0300

    ref: TC_002.01.01 | changed test name

--- tests/test_group_no_pain_no_gain.py
+++ tests/test_group_no_pain_no_gain.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 
-def test_return_from_guide_page_to_main_page_by_clicking_on_logo(driver):
+def test_TC_002_01_01_return_from_guide_page_to_main_page_by_clicking_on_logo(driver):
     driver.get('https://openweathermap.org/guide')
     driver.find_element(By.CSS_SELECTOR, ".logo > a > img").click()
     assert driver.current_url == 'https://openweathermap.org/'
