3e14f5104ef8c77134b2e7072954ac3418e92c03
Abe-Kid <v.ibragim13@gmail.com>
2023-05-05 12:46:46 -0700

    Edit test_tc_003_10_06_verify_linkedIn_link_is_visible. Implemented open_and_load_main_page and wait fixtures

--- tests/test_group_snake_oil.py
+++ tests/test_group_snake_oil.py
@@ -7,10 +7,7 @@ LOADING_VEIL = (By.CSS_SELECTOR, "div[aria-label='Loading']")
 linkedIn_icon = (By.CSS_SELECTOR, "div[class='social'] a:nth-child(3)")
 
 
-def test_tc_003_10_06_verify_linkedIn_link_is_visible(driver):
-    driver.get(HOME_PAGE_URL)
+def test_tc_003_10_06_verify_linkedIn_link_is_visible(driver, open_and_load_main_page, wait):
     driver.maximize_window()
-    wait = WebDriverWait(driver, 25)
-    wait.until_not(EC.presence_of_element_located(LOADING_VEIL))
     element = wait.until(EC.visibility_of_element_located(linkedIn_icon))
-    assert element.is_displayed()
+    assert element.is_displayed(), "LinkedIn interactive icon is not visible on a page"
