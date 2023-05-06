dcbecbacf1d50873b5e324e659e27eeedfd1713b
Alexander_Dulalaev <camel1985@mail.ru>
2023-05-06 18:09:00 +0300

    minor update assert

--- tests/test_group_pytest.py
+++ tests/test_group_pytest.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 MEDIUM_LINK = (By.XPATH, '//a[@href="https://medium.com/@openweathermap"]')
 
 
-def test_tc_003_10_07_check_medium_icon_visible_and_clickable(driver, open_and_load_main_page, wait):
+def test_tc_003_10_07_check_medium_icon_is_visible(driver, open_and_load_main_page, wait):
     driver.find_element(By.TAG_NAME, 'html').send_keys(Keys.END)
     medium_link = wait.until(EC.element_to_be_clickable(MEDIUM_LINK))
-    assert medium_link.is_displayed() and medium_link.is_enabled(), "The medium icon is not displayed or not clickable"
+    assert medium_link.is_displayed() and medium_link.is_enabled(), "The medium icon is not displayed"
