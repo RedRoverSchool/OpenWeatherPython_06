1b6441452823f21ddc3f7557979a8cfee1c00c5d
aigulmustafina <aimustafina2016@gmail.com>
2023-05-06 09:26:02 +0600

    feat: add allure report, pytest make_report hook

--- conftest.py
+++ conftest.py
@@ -1,9 +1,18 @@
 import os
+import allure
 import pytest
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import shutil
+
+URL = 'https://openweathermap.org/'
+load_div = (By.CSS_SELECTOR, 'div.owm-loader-container > div')
+
 
 @pytest.fixture(scope='function')
 def driver():
@@ -14,8 +23,52 @@ def driver():
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        driver.set_window_size(1382, 754)
     else:
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.maximize_window()
     yield driver
     print('\nquit browser...')
     driver.quit()
+
+
+@pytest.fixture()
+def open_and_load_main_page(driver, wait):
+    driver.get(URL)
+    wait.until_not(EC.presence_of_element_located(load_div))
+
+
+@pytest.fixture()
+def wait(driver):
+    wait = WebDriverWait(driver, 25)
+    yield wait
+
+
+def pytest_runtest_makereport(item, call):
+    driver = item.funcargs["driver"]
+    if call.when == 'call':
+        # test_name = item.name
+        if call.excinfo is not None:
+            # status = 'failed'
+            # logger.error(f'{status} - {test_name}. Reason: {str(call.excinfo)}')
+            try:
+                driver.save_screenshot("allure_results/screenshot.png")
+                allure.attach.file("allure_results/screenshot.png", name='Screenshot',
+                                   attachment_type=allure.attachment_type.PNG)
+                allure.attach(driver.page_source, name="HTML source", attachment_type=allure.attachment_type.HTML)
+            except Exception as e:
+                print(f"Failed to take screenshot: {e}")
+
+
+def pytest_sessionstart(session):
+    allure_report_dir = "allure_results"
+    if os.path.exists(allure_report_dir):
+        for file_name in os.listdir(allure_report_dir):
+            file_path = os.path.join(allure_report_dir, file_name)
+            try:
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print(f"Failed to delete {file_path}. Reason: {e}")
