7b374f117eb73ada2978cb6227b837e1cfd360c8
ChosenOnelru <chosenonex1@gmail.com>
2023-05-06 13:53:23 +0300

    TC_002.02.01 | Header > Search field > Search city > Verify that search result contains requested city name

--- tests/test_pandoras_box.py
+++ tests/test_pandoras_box.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 import pytest
+from selenium.webdriver.common.keys import Keys
 
 URL = 'https://openweathermap.org/'
 BUTTON_PRICING = (By.XPATH, '//div[@id="desktop-menu"]//a[text()="Pricing"]')
@@ -19,6 +20,9 @@ URLs = ['https://openweathermap.org/',
         'https://openweathermap.org/appid',
         'https://home.openweathermap.org/questions']
 
+result_locator = (By.XPATH, '//a[contains(@href, "city")]')
+search_field_locator = (By.XPATH, '//*[@placeholder="Weather in your city"]')
+
 def test_TC_002_03_08_open_pricing(driver):
     driver.get(URL)
     button_pricing = driver.find_element(*BUTTON_PRICING)
@@ -34,3 +38,13 @@ def test_TC_002_01_03_Logo_is_visible(driver, wait, URL):
     driver.get(URL)
     logo = driver.find_element(*logo_locator)
     assert logo.is_displayed(), "Logo is not visible"
+
+def test_TC_002_02_01_search_result_contains_city(driver, open_and_load_main_page, wait):
+    search = driver.find_element(*search_field_locator)
+    search.click()
+    search.send_keys('Bangkok')
+    search.send_keys(Keys.ENTER)
+    driver.implicitly_wait(15)
+    cities = driver.find_elements(*result_locator)
+    for city in cities:
+        assert 'Bangkok' in city.text
