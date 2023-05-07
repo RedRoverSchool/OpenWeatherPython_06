b9930ce991d55098ba425fee2ebf4ab01ff8d526
ChosenOnelru <chosenonex1@gmail.com>
2023-05-07 12:30:58 +0300

    TC_003.03.01 | Footer > Product Collections module > Visibility, structure, links сlickability > Verify the module title visibility on all pages specified in data

--- tests/test_pandoras_box.py
+++ tests/test_pandoras_box.py
@@ -2,6 +2,7 @@ from selenium.webdriver import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 import pytest
 
 
