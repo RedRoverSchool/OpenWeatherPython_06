51a4453051f30905dce2b441642dc39d8875f9c2
Tatsiana Paleshka <ptg.stolin@gmail.com>
2023-05-07 16:05:00 +0300

    Added test_TC_003_01_01_verify_footer_is_visible_from_all_pages_specified_in_data

--- tests/test_group_roma.py
+++ tests/test_group_roma.py
@@ -1,9 +1,12 @@
 from selenium.webdriver.common.by import By
+import pytest
+
 
 footer_website_locator = (By.ID, "footer-website")
 company_title_locator = (By.XPATH, "//p[@class='section-heading' and text()='Company']")
 company_content_locator = (By.CSS_SELECTOR, ".footer-section > div > p")
-
+URL = 'https://openweathermap.org/'
+PAGES = ['', 'guide', 'api', 'weather-dashboard', 'price', 'our-initiatives', 'examples', 'users/sign_in', 'faq', 'appid']
 
 
 def test_TC_003_07_01_visibility_of_the_company_module(driver, open_and_load_main_page, wait):
@@ -13,3 +16,11 @@ def test_TC_003_07_01_visibility_of_the_company_module(driver, open_and_load_mai
     assert company_title.is_displayed()
     content = driver.find_element(*company_content_locator)
     assert content.is_displayed()
+
+
+@pytest.mark.parametrize('page', PAGES)
+def test_TC_003_01_01_verify_footer_is_visible_from_all_pages_specified_in_data(driver, wait, page):
+    driver.get(f'{URL}{page}')
+    footer_website = driver.find_element(*footer_website_locator)
+    driver.execute_script("arguments[0].scrollIntoView();", footer_website)
+    assert footer_website.is_displayed(), f"\nFooter is not present on page - {driver.current_url}"
