6d977a54cede1091400492e558ed488adafc2a36
Cruz06 <104589825+Cruz06@users.noreply.github.com>
2023-05-07 18:45:44 +0300

    Update test_pandoras_box.py

--- tests/test_pandoras_box.py
+++ tests/test_pandoras_box.py
@@ -16,7 +16,7 @@ SEARCH_DROPDOWN_OPTION = (By.CSS_SELECTOR, 'ul.search-dropdown-menu li:nth-child
 SEARCH_CITY_FIELD = (By.CSS_SELECTOR, "input[placeholder='Search city']")
 SEARCH_BUTTON = (By.CSS_SELECTOR, "button[class ='button-round dark']")
 DISPLAYED_CITY = (By.CSS_SELECTOR, '.grid-container.grid-4-5 h2')
-load_div = (By.CSS_SELECTOR, 'div.owm-loader-container > div')
+
 
 logo_locator = (By.XPATH, '//*[@class="logo"]/a/img')
 title_locator = (By.XPATH, '//p[text()="Product Collections"]')
@@ -98,10 +98,9 @@ def test_TC_002_01_03_Logo_is_visible(driver, wait, URL):
     assert logo.is_displayed(), "Logo is not visible"
 
 
-def test_TC_001_01_02_2_fill_city_field_in_cirillic(driver):
+def test_TC_001_01_02_2_fill_city_field_in_cirillic(driver, open_and_load_main_page, wait):
     driver.get(URL)
     wait_act = WebDriverWait(driver, 15)
-    wait_act.until_not(EC.presence_of_element_located(load_div))
     search_city_input = driver.find_element(*SEARCH_CITY_FIELD)
     search_city_input.send_keys('Кишинев')
     driver.find_element(*SEARCH_BUTTON).click()
