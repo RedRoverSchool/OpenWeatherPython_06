39777ecb35b4741f04951be4018efd55ef836678
Ekaterina Myshov <79929359+KatyaMy@users.noreply.github.com>
2023-05-08 15:04:20 -0400

    AT_006.02.03 | Weather Dashboard > Verify the transition to another page when clicking on the button [Try the Dashboard], for non-signed-in user (#319)
    * TC_006.02.03 Weather Dashboard Verify the transition to another page when clicking on the button [Try the Dashboard],for non-signed-in user
    
    * TC_006.02.03 Weather Dashboard Verify the transition to another page when clicking on the button[Try the Dashboard],for non-signed-in user

--- tests/test_group_pythonista.py
+++ tests/test_group_pythonista.py
@@ -10,6 +10,9 @@ TITLE_HOW_TO_START = (By.XPATH, "//div/h2[contains(text(),'How to Start')]")
 PROFESSIONAL_COLLESTION_TITLE = (By.XPATH, "//section[@id='pro']/h2")
 
 LOGO = (By.CSS_SELECTOR, ".logo > a > img")
+BTN_TRY_THE_DASHBOARD_2 = (By.XPATH, "//div[6]//a[text()='Try the Dashboard']")
+BTN_COOKIES = (By.CLASS_NAME, "stick-footer-panel__link")
+ALERT_PANEL_SINGIN = (By.CSS_SELECTOR, '.col-md-6 .panel-heading')
 
 
 
@@ -31,7 +34,7 @@ def test_TC_002_03_05_dashboard_is_visible_and_clickable(driver, open_and_load_m
     assert dashboard_tab.is_displayed() and dashboard_tab.is_enabled() and expected_dashboard_label in dashboard_tab.text
 
 
-def test_TC_006_02_01_Verify_display_of_How_to_Start_section(driver, open_and_load_main_page, wait):
+def test_TC_006_02_01_verify_display_of_how_to_start_section(driver, open_and_load_main_page, wait):
     driver.find_element(*BTN_DASHBOARD).click()
     section = driver.find_element(*TITLE_HOW_TO_START)
     assert section.is_displayed(), "Section not found"
@@ -44,6 +47,7 @@ def test_TC_002_03_06_dashboard_link_opens_correct_page(driver, open_and_load_ma
     expected_url = 'https://openweathermap.org/weather-dashboard'
     assert driver.current_url == expected_url
 
+
 def test_TC_005_04_02_Professional_collection_section_is_presented(driver, wait):
     driver.get(URL_API)
     wait.until(EC.presence_of_element_located(PROFESSIONAL_COLLESTION_TITLE))
@@ -52,8 +56,18 @@ def test_TC_005_04_02_Professional_collection_section_is_presented(driver, wait)
     assert expected_title in proffecional_collection_title.text
 
 
-def test_TC_002_01_04_Header_Logo_Verify_logo_redirects_from_dashboard_page_to_main_page(driver):
+def test_TC_002_01_04_header_logo_verify_logo_redirects_from_dashboard_page_to_main_page(driver):
     driver.get('https://openweathermap.org/weather-dashboard/')
     driver.find_element(*LOGO).click()
     assert driver.current_url == 'https://openweathermap.org/'
+
+
+def test_TC_006_02_03_weather_dashboard_verify_the_transition_to_another_page(driver, open_and_load_main_page, wait):
+    driver.find_element(*BTN_DASHBOARD).click()
+    cookie_close = driver.find_element(*BTN_COOKIES)
+    driver.execute_script("arguments[0].click();", cookie_close)
+    driver.find_element(*BTN_TRY_THE_DASHBOARD_2).click()
+    driver.switch_to.window(driver.window_handles[1])
+    alert_mms = driver.find_element(*ALERT_PANEL_SINGIN)
+    assert alert_mms.is_displayed(), 'WELCOME EVENTS'
 
