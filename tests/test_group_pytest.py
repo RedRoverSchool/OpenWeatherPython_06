7d4fae08ff492b2f108d47cb68ae5ffce0e6a297
Alexander_Dulalaev <camel1985@mail.ru>
2023-05-09 22:19:24 +0300

    add TC_008.01.04 | Pricing > Self> Page structure, functionality>Check 6 sections are visible

--- tests/test_group_pytest.py
+++ tests/test_group_pytest.py
@@ -1,3 +1,4 @@
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
@@ -12,6 +13,10 @@ HOW_TO_START_EVENTS_SECTION_LINK = (By.XPATH, '//a[contains(text(), "Events")]')
 HOW_TO_START_GO_TO_THE_NEW_TRIGGER_SECTION_LINK = (By.XPATH, '//b[contains(text(), "New trigger")]')
 HOW_TO_START_HERE_LINK = (By.XPATH, '//a[.="here"]')
 HOW_TO_START_DETAILED_USER_MANUAL_LINK = (By.XPATH, '//b[.="detailed user manual"]')
+HEADER_PRICING_LINK = (By.XPATH, '//div[@id="desktop-menu"]//a[.="Pricing"]')
+PRICING_SUBSCRIBE_TO_ONE_CALL_BY_CALL_BUTTON = (By.XPATH, '//a[.="Subscribe to One Call by Call"]')
+PRICING_PAGE_SECTIONS_LOCATOR = ()
+sections = ['onecall', 'current', 'alerts', 'history', 'historyspecial', 'offers']
 
 
 def test_tc_006_02_02_verify_how_to_start_block_7_links_are_visible(driver, open_and_load_main_page, wait):
@@ -29,3 +34,13 @@ def test_tc_006_02_02_verify_how_to_start_block_7_links_are_visible(driver, open
                      driver.find_element(*HOW_TO_START_DETAILED_USER_MANUAL_LINK)]
     for item in list_of_links:
         assert item.is_displayed(), f"{item.text} link not visible"
+
+
+@pytest.mark.parametrize('section', sections)
+def test_tc_008_01_04_check_6_sections_are_visible(driver, open_and_load_main_page, wait, section):
+    driver.find_element(*HEADER_PRICING_LINK).click()
+    wait.until(EC.element_to_be_clickable(PRICING_SUBSCRIBE_TO_ONE_CALL_BY_CALL_BUTTON))
+    actual_section = driver.find_element(By.XPATH, f'//section[@id="{section}"]')
+    actual_section.location_once_scrolled_into_view
+    assert actual_section.is_displayed(), \
+        f"Section {driver.find_element((By.CSS_SELECTOR, f'#{section} h2')).text} is not visible"
