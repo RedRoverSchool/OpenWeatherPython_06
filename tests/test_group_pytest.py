37637d2606913059de725fe23121c57845b0eed9
Alexander_Dulalaev <camel1985@mail.ru>
2023-05-09 18:24:35 +0300

    add TC_008.01.03 | Pricing > Self> Page structure, functionality>Check the Pricing page title

--- tests/test_group_pytest.py
+++ tests/test_group_pytest.py
@@ -1,3 +1,4 @@
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
@@ -12,6 +13,8 @@ HOW_TO_START_EVENTS_SECTION_LINK = (By.XPATH, '//a[contains(text(), "Events")]')
 HOW_TO_START_GO_TO_THE_NEW_TRIGGER_SECTION_LINK = (By.XPATH, '//b[contains(text(), "New trigger")]')
 HOW_TO_START_HERE_LINK = (By.XPATH, '//a[.="here"]')
 HOW_TO_START_DETAILED_USER_MANUAL_LINK = (By.XPATH, '//b[.="detailed user manual"]')
+HEADER_PRICING_LINK = (By.XPATH, '//div[@id="desktop-menu"]//a[.="Pricing"]')
+PRICING_SUBSCRIBE_TO_ONE_CALL_BY_CALL_BUTTON = (By.XPATH, '//a[.="Subscribe to One Call by Call"]')
 
 
 def test_tc_006_02_02_verify_how_to_start_block_7_links_are_visible(driver, open_and_load_main_page, wait):
@@ -29,3 +32,9 @@ def test_tc_006_02_02_verify_how_to_start_block_7_links_are_visible(driver, open
                      driver.find_element(*HOW_TO_START_DETAILED_USER_MANUAL_LINK)]
     for item in list_of_links:
         assert item.is_displayed(), f"{item.text} link not visible"
+
+
+def test_tc_008_01_03_check_the_pricing_page_title(driver, open_and_load_main_page,wait):
+    driver.find_element(*HEADER_PRICING_LINK).click()
+    wait.until(EC.element_to_be_clickable(PRICING_SUBSCRIBE_TO_ONE_CALL_BY_CALL_BUTTON))
+    assert driver.title == 'Pricing - OpenWeatherMap'
