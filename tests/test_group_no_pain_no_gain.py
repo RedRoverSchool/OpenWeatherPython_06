7bb51d6dd153ca4afc95de6334e76720d70e2b91
angelina sidoruk <ang.sidoruk@gmail.com>
2023-05-10 21:29:34 +0300

    TC_003.08.07 | Footer > Singl links set > Links visibility and clickability > Verify Blog link is visible https://trello.com/c/o2PPgXsb/187-tc0030807-footer-singl-links-set-links-visibility-and-clickability-verify-blog-link-is-visible

--- tests/test_group_no_pain_no_gain.py
+++ tests/test_group_no_pain_no_gain.py
@@ -7,3 +7,9 @@ def test_TC_002_01_01_return_from_guide_page_to_main_page_by_clicking_on_logo(dr
     driver.get('https://openweathermap.org/guide')
     driver.find_element(*logo_locator).click()
     assert driver.current_url == 'https://openweathermap.org/'
+
+
+def test_TC_003_08_07_verify_Blog_link_is_visible(driver, open_and_load_main_page):
+    element = driver.find_element(By.XPATH, "//*[@id='footer-website']/div/div[2]/div[3]/div/ul/li[2]/a")
+    assert element.is_displayed(), "Blog"
+
