f8dddb46995da72605553fcdbc82ae541fd7026c
ZarinaSadykovaQA <z.sadikova@mercuryo.io>
2023-05-10 15:55:42 +0500

    update AT_005.05.01 | Weather API > Daily forecast 16 days > Visibility and clickability "Call 16 day / daily forecast data" link

--- tests/test_group_moon_cat.py
+++ tests/test_group_moon_cat.py
@@ -6,4 +6,3 @@ def test_TC_005_05_01_visibility_and_clickability_call_16_day_daily_forecast_dat
     element = driver.find_element(By.XPATH, "//a[@href='#16days']")
     assert element.is_displayed(), "Call 16 day/daily forecast data interactive icon is not visible on a page"
     assert element.is_enabled(), "Call 16 day/daily forecast data interactive icon is not clickable on a page"
-    print(f'{element} is clickable')
