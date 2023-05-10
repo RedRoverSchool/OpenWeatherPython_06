e0ba4413665732d4817d8b5f4211d435d0f9c4b4
Markelova Svetlana <markelovasvetlana7@gmail.com>
2023-05-10 18:01:57 +0300

    RF test_TC_003_10_05_verify_visibility_of_github_icon

--- tests/test_group_roma.py
+++ tests/test_group_roma.py
@@ -60,7 +60,6 @@ def test_TC_003_01_02_verify_copyright_is_visible_from_all_pages_specified_in_da
     assert copyright_website.is_displayed() and copyright_flag == 1, f'\nCopyright is not present (actual) on the page - {driver.current_url}'
 
 def test_TC_003_10_05_verify_visibility_of_github_icon(driver, open_and_load_main_page):
-    driver.get(URL)
     github_icon = driver.find_element(*gitHub_icon_image)
-    assert github_icon.is_displayed() and github_icon.is_enabled(), "GitHub icon is not clickable"
+    assert github_icon.is_displayed() and github_icon.is_enabled()
 
