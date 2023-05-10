dcb0386953821195c01ff0ee697931e9a64bbc4a
AnnaPo24 <style4moms@gmail.com>
2023-05-10 16:25:17 +0300

    add TC_003.10.04 | Footer > Social media module > Icons-links visibility and сlickability > Verify the visibility of Telegram icon-link

--- tests/test_group_future_auto_qa.py
+++ tests/test_group_future_auto_qa.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 HEAD_SEARCH_FIELD = (By.NAME, "q")
 HEAD_SEARCH_PLACEHOLDER = (By.CSS_SELECTOR, 'input[name="q"]::placeholder')
+TELEGRAM_ICON = (By.CSS_SELECTOR, "div[class='social'] a:nth-child(5)")
 
 
 def test_TC_002_02_07_verify_placeholder_is_displayed_in_search_field(driver, open_and_load_main_page, wait):
@@ -20,3 +21,9 @@ def test_TC_002_02_09_verify_placeholder_disappers_if_symbol_is_typed_in_search_
     search_field.send_keys('a')
     assert placeholder_text not in search_field.get_attribute("value"), \
         "The placeholder text is still visible in the search field after typing a symbol"
+
+
+def test_tc_003_10_04_verify_telegram_link_is_visible(driver, open_and_load_main_page, wait):
+    element = wait.until(EC.visibility_of_element_located(TELEGRAM_ICON))
+    assert element.is_displayed(), "Telegram interactive icon is not visible on a page"
+
