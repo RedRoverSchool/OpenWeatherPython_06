8d5ce5ffb300899ca4e58ad8d57c092a19d041d2
Alexander_Dulalaev <70773950+camelclassik85@users.noreply.github.com>
2023-05-10 20:23:27 +0300

    AT_001.12.03 | Main Page > Weather Conditions > Weather condition codes > Visibility > Verify that Rain group of codes contains more than 1 item (#338)

--- tests/test_group_pytest.py
+++ tests/test_group_pytest.py
@@ -53,3 +53,19 @@ def test_tc_001_12_02_verify_that_rain_group_of_codes_is_visible(driver, open_an
     list_of_elements = driver.find_elements(*GROUP_5XX_RAIN_ROWS_LINK)
     for number, item in enumerate(list_of_elements):
         assert item.is_displayed(), f"{number} row not visible"
+
+
+def test_tc_001_12_03_verify_that_rain_group_of_codes_contains_more_than_1_item(driver, open_and_load_main_page, wait):
+    driver.find_element(*COOKIES_ALLOW_ALL_BUTTON_LINK).click()
+    driver.find_element(*HEADER_API_LINK).click()
+    wait.until(EC.element_to_be_clickable(HOME_LINK))
+    actions = ActionChains(driver)
+    actions.move_to_element(driver.find_element(*LIST_OF_WEATHER_CONDITION_CODES_LINK)).perform()
+    driver.find_element(*LIST_OF_WEATHER_CONDITION_CODES_LINK).click()
+    wait.until(EC.element_to_be_clickable(WEATHER_CONDITION_CODES_LINK))
+    driver.find_element(*WEATHER_CONDITION_CODES_LINK).click()
+    wait.until(EC.element_to_be_clickable(HOME_LINK))
+    actions1 = ActionChains(driver)
+    actions1.move_to_element(driver.find_element(*GROUP_6XX_SNOW_LINK)).perform()
+    list_of_elements = driver.find_elements(*GROUP_5XX_RAIN_ROWS_LINK)
+    assert len(list_of_elements) > 2, "rain_group_of_codes_contains 1 or less item"
