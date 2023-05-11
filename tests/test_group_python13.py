f59a498a38bc713f5a97a2cbd286b2d143902d33
Taskvo <tatyana.skvortsova@triafly.ru>
2023-05-11 23:22:09 +0300

    AT_013.04.01 | Header (Main page) >Blog > Verify if 'Platform' tab is opened, contains news, with appropriate tag

--- tests/test_group_python13.py
+++ tests/test_group_python13.py
@@ -2,11 +2,11 @@ from selenium.webdriver.common.by import By
 URL = 'https://openweathermap.org/'
 
 
-def test_should_open_given_link(driver):
+def test_TC_013_04_01_should_open_given_link(driver):
     driver.get(URL)
     assert 'openweathermap' in driver.current_url
 
-def test_click_by_platform_leads_to_correct_page(driver):
+def test_TC_013_04_01__click_by_platform_leads_to_correct_page(driver):
     driver.get('https://openweather.co.uk/blog/')
     search_button = driver.find_element(By.LINK_TEXT, "PLATFORM")
     search_button.click()
