4ae38d045f474bd8f77b94acc92c860bd957e6ea
Sergei Postalovski <postalovski@gmail.com>
2023-05-11 17:52:10 -0400

    Group test_group_100000.py added

--- tests/test_group_100_000.py
+++ tests/test_group_100_000.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 
 search_sky_in_words = (By.XPATH, "//div[@class='day-list-values']/span[contains(@class,'sub')]")
 
-
 def test_TC_001_04_02_Verify_state_of_sky_in_words_for_each_day_is_displayed(driver, open_and_load_main_page, wait):
     elements = driver.find_elements(*search_sky_in_words)
     for i in elements:
