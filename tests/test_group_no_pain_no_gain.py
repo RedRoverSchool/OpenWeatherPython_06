f94503c09ea30b7d52fb4d6499920a6983a302a2
angelina sidoruk <ang.sidoruk@gmail.com>
2023-05-11 12:00:06 +0300

    TC_003.08.08 | Footer > Single links set
    > Links visibility and clickability > Verify Blog link is clickable

--- tests/test_group_no_pain_no_gain.py
+++ tests/test_group_no_pain_no_gain.py
@@ -10,12 +10,17 @@ def test_TC_002_01_01_return_from_guide_page_to_main_page_by_clicking_on_logo(dr
     driver.get('https://openweathermap.org/guide')
     driver.find_element(*logo_locator).click()
     assert driver.current_url == 'https://openweathermap.org/'
-    
+
 def test_TC_003_08_02_ask_a_question_link_is_visible(driver, open_and_load_main_page, wait):
     element = wait.until(EC.visibility_of_element_located(ASK_A_QUESTION_LINK))
     assert element.is_displayed(), "Ask a question link is not visible in the footer"
- 
+
 def test_TC_003_13_03_verify_visibility_and_clickability_of_allow_all_button(driver, open_and_load_main_page, wait):
     element = driver.find_element(*allow_all_cookies_locator)
     wait.until(EC.element_to_be_clickable(allow_all_cookies_locator))
     assert element.is_displayed() and element.is_enabled()
+
+def test_TC_003_08_08_verify_Blog_link_is_clickable(driver, open_and_load_main_page):
+    element = driver.find_element(By.XPATH, "//a[@href='https://openweather.co.uk/blog/category/weather']")
+    assert element.is_displayed() and element.is_enabled()
+
