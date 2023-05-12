af8fcf13fe51f3bf3c7bbf7b4be01e318d04e058
Eliser14 <elinaa@Elinas-MacBook-Air.local>
2023-05-12 10:06:41 -0500

    AT_007.01.02 | Marketplace > Weather Conditions > Historical Weather Data by State (2) > Visibility, clickablity > Select year from dropdown list

--- tests/test_group_100_000.py
+++ tests/test_group_100_000.py
@@ -2,7 +2,6 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-
 search_sky_in_words = (By.XPATH, "//div[@class='day-list-values']/span[contains(@class,'sub')]")
 how_to_start_footer_loc = (By.CSS_SELECTOR, "div.section-content ul>li>a[href='/appid']")
 URL_HISTORICAL_WEATHER = 'https://home.openweathermap.org/zip_code_data/new'
@@ -30,7 +29,9 @@ def test_TC_007_01_01_Select_state_from_dropdown_list(driver, wait):
     assert expected_state == selected_state, '\n======== WRONG STATE! ========\n'
 
 
-def test_TC_001_05_03_Verify_humidity_percentage_in_detailed_weather_data_for_current_location(driver, open_and_load_main_page, wait):
+def test_TC_001_05_03_Verify_humidity_percentage_in_detailed_weather_data_for_current_location(driver,
+                                                                                               open_and_load_main_page,
+                                                                                               wait):
     humidity_symbol = driver.find_element(*weather_symbol)
     assert humidity_symbol.is_displayed()
 
