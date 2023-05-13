e72e5c6cd9b7c22292962fa66e3e6037253302f0
Stas Cherepanov <94304561+zaelcovsky@users.noreply.github.com>
2023-05-13 20:01:53 +0700

    AC_005.10.01 | Solar Irradiance API > Description, visibility, clickability, correct redirection > Visibility of ‘Weather API’ page title

--- tests/test_group_future_auto_qa.py
+++ tests/test_group_future_auto_qa.py
@@ -2,6 +2,7 @@ import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
+# Main page locators
 HEAD_SEARCH_FIELD = (By.NAME, "q")
 HEAD_SEARCH_PLACEHOLDER = (By.CSS_SELECTOR, 'input[name="q"]::placeholder')
 
@@ -20,3 +21,8 @@ def test_TC_002_02_09_verify_placeholder_disappers_if_symbol_is_typed_in_search_
     search_field.send_keys('a')
     assert placeholder_text not in search_field.get_attribute("value"), \
         "The placeholder text is still visible in the search field after typing a symbol"
+
+
+def test_TC_005_10_01_visibility_of_weather_api_page_title(driver):
+    driver.get('https://openweathermap.org/api')
+    assert driver.title == 'Weather API - OpenWeatherMap', "The title of the page is incorrect"
