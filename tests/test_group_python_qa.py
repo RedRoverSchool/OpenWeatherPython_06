8a35f62e20eddf3e56a1ef759cc6e9147c58d466
Anastasiia Zubenko <astassia2@gmail.com>
2023-05-12 22:33:57 -0700

    AT_005.10.02 | Solar Irradiance API > Visibility of Product concept article title

--- tests/test_group_python_qa.py
+++ tests/test_group_python_qa.py
@@ -3,13 +3,16 @@ import pytest
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
+URL_SOLAR_API = "https://openweathermap.org/api/solar-energy-prediction"
 cities = ['New York', 'Los Angeles', 'Paris']
 search_city_field_locator = (By.CSS_SELECTOR, "input[placeholder='Search city']")
 search_button_locator = (By.CSS_SELECTOR, "button[class ='button-round dark']")
 search_1st_option_locator = (By.CSS_SELECTOR, 'ul.search-dropdown-menu li:first-child span:first-child')
 loading_screen_locator = (By.CSS_SELECTOR, 'div.owm-loader-container > div')
 c_temp_locator = (By.CSS_SELECTOR, '.switch-container .option:nth-child(2)')
-line_in_8_days_forecast_locator = (By.XPATH, "(//div[@class='day-list-values']/div/span[contains(text(), '°C')])")
+line_in_8_days_forecast_locator = (By.XPATH, "//div[@class='day-list-values']/div/span[contains(text(), '°C')]")
+product_concept_title_locator = (By.CSS_SELECTOR, "#concept h2")
 
 
 @pytest.mark.parametrize('city', cities)
@@ -20,11 +23,16 @@ def test_TC_001_04_01_visibility_of_8_lines_in_8_day_forecast_block(driver, open
     search_button.click()
     search_option = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(search_1st_option_locator))
     search_option.click()
-    WebDriverWait(driver, 10).until_not(EC.presence_of_element_located(loading_screen_locator))
     c_temp = driver.find_element(*c_temp_locator)
     c_temp.click()
     lines = driver.find_elements(*line_in_8_days_forecast_locator)
     for line in lines:
         assert line.is_displayed()
+
+
+def test_TC_005_10_02_visibility_of_Product_concept_article_title(driver):
+    driver.get(URL_SOLAR_API)
+    product_concept_title = driver.find_element(*product_concept_title_locator)
+    assert product_concept_title.is_displayed()
 
 
