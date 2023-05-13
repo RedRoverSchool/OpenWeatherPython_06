0e1bccad3a91ecb17e50fa9db98bf045a590285a
AlinaP553 <131289445+AlinaP553@users.noreply.github.com>
2023-05-13 17:51:11 +0700

    AT_004.04.01| Guide > How to start using Weather API > Visibility, clickability, correct redirection> Verify visibility and clickability of the 'How to start and operate with API more efficiently' link (#432)
    * Added TC_004_04_01
    
    * Added file TC_004_04_01

--- tests/test_group_moon_cat.py
+++ tests/test_group_moon_cat.py
@@ -1,7 +1,17 @@
 from selenium.webdriver.common.by import By
 
+URL_guide = 'https://openweathermap.org/guide'
+how_to_start_link = (By.XPATH, "//a[contains(text(),'‘How to start and operate with API more efficientl')]")
+call_16_day_daily_forecast_data = (By.XPATH, "//a[@href='#16days']")
+
 
 def test_TC_005_05_01_visibility_and_clickability_call_16_day_dailyforecast_data_link(driver, wait):
     driver.get('https://openweathermap.org/forecast16')
-    element = driver.find_element(By.XPATH, "//a[@href='#16days']")
+    element = driver.find_element(*call_16_day_daily_forecast_data)
     assert element.is_displayed() and element.is_enabled()
+
+def test_TC_004_04_01_verify_visibility_and_clickability_of_the_link(driver):
+    driver.get(URL_guide)
+    element = driver.find_element(*how_to_start_link)
+    assert element.is_enabled() and element.is_displayed()
+
