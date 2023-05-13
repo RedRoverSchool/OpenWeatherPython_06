a556cfb1438faf2686769ceab905b76dab0e210f
Ekaterina Myshov <79929359+katyamy@users.noreply.github.com>
2023-05-12 21:38:37 -0400

    TC_005.08.04 | Road Risk API > Redirection to the How to request Road Risk API section of the page (#425)

--- tests/test_group_pythonista.py
+++ tests/test_group_pythonista.py
@@ -1,5 +1,3 @@
-import time
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -11,7 +9,7 @@ TITLE_FORCAST30 = (By.CSS_SELECTOR, '.col-sm-7 .breadcrumb-title')
 LINK_HOW_TO_MAKE = (By.CSS_SELECTOR, "a[href$='geo-year']")
 TITLE_HOW_TO_MAKE = (By.XPATH, '//*[@id="geo-year"]/h3')
 
-URL_ROAD_RISK ='https://openweathermap.org/api/road-risk'
+URL_ROAD_RISK = 'https://openweathermap.org/api/road-risk'
 SECTION_R_CONCEPTS = (By.XPATH, "//*[@id='concept']")
 
 FOOTER_PANEL = (By.XPATH, '//*[@id="stick-footer-panel"]/div')
@@ -129,14 +127,13 @@ def test_TC_003_11_02_verify_the_copyright_information_is_present_on_the_site_pa
         "The footer is not displayed or does not contain the expected text"
 
 
-
 def test_TC_005_08_03_road_risk_api_visibility_of_road_risk_api_concept_section(driver):
     driver.get(URL_ROAD_RISK)
     section_road_risk = driver.find_element(*SECTION_R_CONCEPTS)
-    assert section_road_risk.is_displayed(),'Section - NOT FOUND'
+    assert section_road_risk.is_displayed(), 'Section - NOT FOUND'
+
 
 def test_TC_002_01_08_header_logo_verify_logo_redirects_from_weather_model_page_to_main_page(driver):
     driver.get(URL_WEATHER_MODEL)
     driver.find_element(*LOGO).click()
     assert driver.current_url == 'https://openweathermap.org/'
-
