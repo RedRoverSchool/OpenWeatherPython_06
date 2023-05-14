5f12fdd4d287e966686fac8ac0052a33252daa28
Stas Cherepanov <94304561+zaelcovsky@users.noreply.github.com>
2023-05-14 13:45:59 +0700

    AC_005.10.01 | Solar Irradiance API > Description, visibility, clickability, correct redirection > Visibility of ‘Weather API’ page title (#434)

--- tests/test_group_future_auto_qa.py
+++ tests/test_group_future_auto_qa.py
@@ -2,10 +2,13 @@ import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
+# Main page locators
 HEAD_SEARCH_FIELD = (By.NAME, "q")
 HEAD_SEARCH_PLACEHOLDER = (By.CSS_SELECTOR, 'input[name="q"]::placeholder')
 WIDGETS = (By.XPATH, "//a[contains(text(), 'Widgets')]")
 COOKIES =(By.XPATH, "//button[contains(text(), 'Allow all')]")
+
+
 def test_TC_002_02_07_verify_placeholder_is_displayed_in_search_field(driver, open_and_load_main_page, wait):
     search_field = wait.until(EC.presence_of_element_located(HEAD_SEARCH_FIELD))
     search_placeholder_text = search_field.get_attribute("placeholder")
@@ -21,6 +24,7 @@ def test_TC_002_02_09_verify_placeholder_disappers_if_symbol_is_typed_in_search_
     assert placeholder_text not in search_field.get_attribute("value"), \
         "The placeholder text is still visible in the search field after typing a symbol"
 
+
 def test_TC_003_12_12_widgets_link_functionality(driver, open_and_load_main_page, wait):
     wait.until(EC.element_to_be_clickable(COOKIES)).click()
     widgets = wait.until(EC.presence_of_element_located(WIDGETS))
@@ -29,3 +33,8 @@ def test_TC_003_12_12_widgets_link_functionality(driver, open_and_load_main_page
     driver.implicitly_wait(5)
     current_url = driver.current_url
     assert widget in current_url
+
+
+def test_TC_005_10_01_visibility_of_weather_api_page_title(driver):
+    driver.get('https://openweathermap.org/api')
+    assert driver.title == 'Weather API - OpenWeatherMap', "The title of the page is incorrect"
