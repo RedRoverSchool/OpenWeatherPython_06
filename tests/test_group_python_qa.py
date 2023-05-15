1c917fd070aa5a3a8445bbbda69d08f7b174282c
Anastasiia Zubenko <astassia2@gmail.com>
2023-05-15 07:31:10 -0700

    Revert "AT_005.10.02 | Solar Irradiance API > Visibility of Product concept article title (#428)"
    This reverts commit 487aa3799ba6ba025654f3a604489aaa8c45f0f6.

--- tests/test_group_python_qa.py
+++ tests/test_group_python_qa.py
@@ -3,22 +3,17 @@ import pytest
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
-URL_SOLAR_API = "https://openweathermap.org/api/solar-energy-prediction"
 cities = ['New York', 'Los Angeles', 'Paris']
 search_city_field_locator = (By.CSS_SELECTOR, "input[placeholder='Search city']")
 search_button_locator = (By.CSS_SELECTOR, "button[class ='button-round dark']")
 search_1st_option_locator = (By.CSS_SELECTOR, 'ul.search-dropdown-menu li:first-child span:first-child')
 loading_screen_locator = (By.CSS_SELECTOR, 'div.owm-loader-container > div')
 c_temp_locator = (By.CSS_SELECTOR, '.switch-container .option:nth-child(2)')
-line_in_8_days_forecast_locator = (By.XPATH, "//div[@class='day-list-values']/div/span[contains(text(), '°C')]")
-product_concept_title_locator = (By.CSS_SELECTOR, "#concept h2")
 line_in_8_days_forecast_locator = (By.XPATH, "(//div[@class='day-list-values']/div/span[contains(text(), '°C')])")
 subscription_module_button = (By.CSS_SELECTOR, ".inner-footer-container div:first-of-type "
                                                ".footer-section:nth-child(2) p.section-heading")
 quality_info_page = "https://openweathermap.org/accuracy-and-quality"
 nwp_model = (By.CSS_SELECTOR, ".col-sm-12 > ul:first-of-type")
-
 
 
 @pytest.mark.parametrize('city', cities)
@@ -29,17 +24,12 @@ def test_TC_001_04_01_visibility_of_8_lines_in_8_day_forecast_block(driver, open
     search_button.click()
     search_option = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(search_1st_option_locator))
     search_option.click()
+    WebDriverWait(driver, 10).until_not(EC.presence_of_element_located(loading_screen_locator))
     c_temp = driver.find_element(*c_temp_locator)
     c_temp.click()
     lines = driver.find_elements(*line_in_8_days_forecast_locator)
     for line in lines:
         assert line.is_displayed()
-
-
-def test_TC_005_10_02_visibility_of_Product_concept_article_title(driver):
-    driver.get(URL_SOLAR_API)
-    product_concept_title = driver.find_element(*product_concept_title_locator)
-    assert product_concept_title.is_displayed()
 
 
 
@@ -54,4 +44,3 @@ def test_001_017_01_visibility_of_nwp_block(driver):
     driver.get(quality_info_page)
     nwp = driver.find_element(*nwp_model)
     assert nwp.is_displayed()
-
