71f4f05fdbf7a5cfda49f713605cf053b002d7b6
Aleksei Kanikovskii <vrashfor@gmail.com>
2023-05-15 22:31:02 +0300

    Created "Ducktales" group folder

---
+++ tests/test_group_ducktales/pages/main_page.py
@@ -0,0 +1,10 @@
+from .base_page import BasePage
+from tests.test_group_ducktales.locators.main_locators import MainLocator
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class MainPage(BasePage):
+    pass
+
+
+
