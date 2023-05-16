cf86c3e1755bd2bea12a2ddcb5bab04773a9cb5a
AlinaP553 <131289445+AlinaP553@users.noreply.github.com>
2023-05-16 20:07:08 +0700

    Add file TC_005_06_03

--- tests/test_group_moon_cat.py
+++ tests/test_group_moon_cat.py
@@ -5,7 +5,9 @@ URL_forecast = 'https://openweathermap.org/forecast16'
 how_to_start_link = (By.XPATH, "//a[contains(text(),'‘How to start and operate with API more efficientl')]")
 call_16_day_daily_forecast_data = (By.XPATH, "//a[@href='#16days']")
 weathefieldsapi = (By.CSS_SELECTOR, "a[href='#parameter']")
-
+URL_forecast30 = ('https://openweathermap.org/api/forecast30')
+other_features_link = (By.XPATH, "//a[normalize-space()='Other features']")
+other_features = (By.XPATH, "//h2[normalize-space()='Other features']")
 
 def test_TC_005_05_01_visibility_and_clickability_call_16_day_dailyforecast_data_link(driver):
     driver.get(URL_forecast)
@@ -21,3 +23,9 @@ def test_AT_005_05_07_weatherAPI_Daily_forecast_16_days_Visibility_and_clickabil
     driver.get(URL_forecast)
     element = driver.find_element(*weathefieldsapi)
     assert element.is_displayed() and element.is_enabled()
+
+def test_TC_005_06_03_redirect_to_other_features_section(driver):
+    driver.get(URL_forecast30)
+    driver.find_element(*other_features_link).click()
+    section_title = driver.find_element(*other_features)
+    assert section_title.is_displayed(), 'Title Not Found'
