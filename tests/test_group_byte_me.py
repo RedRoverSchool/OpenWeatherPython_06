2ad96f68da7b0b6beca7796282e3268e6a55c65d
Elena Pozhydaieva <epozhydaieva@gmail.com>
2023-05-15 23:50:21 -0400

    AT_002.02.01_01 | Code changes in test_group_byte_me.py

--- tests/test_group_byte_me.py
+++ tests/test_group_byte_me.py
@@ -1,31 +1,14 @@
-import time
 
-from selenium import webdriver
-import pytest
 from selenium.webdriver import ActionChains
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from keyboard import press
 
 
-URL = 'https://openweathermap.org/'
-LOAD_DIV = (By.CSS_SELECTOR, 'div.owm-loader-container > div')
 WEATHER_IN_YOUR_CITY_FLD = (By.CSS_SELECTOR, "#desktop-menu input:nth-child(1)")
 REQUESTED_CITY = 'London, GB'
 DISPLAYED_CITY = (By.CSS_SELECTOR, "table b a:nth-child(1)")
 
 
-@pytest.fixture()
-def wait(driver):
-    wait = WebDriverWait(driver, 15)
-    yield wait
-
-def test_Search_City_Results_Are_Visible(driver, wait):
-    driver.get(URL)
-    wait.until_not(EC.presence_of_element_located(LOAD_DIV))
+def test_Search_City_Results_Are_Visible(driver, wait, open_and_load_main_page):
     weather_in_your_city_field = driver.find_element(*WEATHER_IN_YOUR_CITY_FLD)
     action_chains = ActionChains(driver)
     action_chains.move_to_element(weather_in_your_city_field)
