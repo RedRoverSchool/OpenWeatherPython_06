13830a05af94fe0f3690cd265e5b6665b06ccc1e
Lipatnikova <aerovaya@gmail.com>
2023-05-17 21:42:14 +0400

    RF_AT_001.04.03 | Main page > Search city widget > 8-day-forecast > day-list > day by week

--- tests/test_group_ducktales/pages/main_page.py
+++ tests/test_group_ducktales/pages/main_page.py
@@ -1,10 +1,17 @@
 from .base_page import BasePage
 from tests.test_group_ducktales.locators.main_locators import MainLocator
-from selenium.webdriver.support import expected_conditions as EC
+from tests.test_group_ducktales.test_data.main_page_data import *
+from datetime import datetime
 
 
 class MainPage(BasePage):
-    pass
 
+    def get_day_by_weak(self):
+        element = self.element_is_visible(MainLocator.FIRST_DAY_IN_8_DAY_FORECAST).text[:3]
+        return element
 
+    def day_by_computer(self):
+        day_by_computer = datetime.now().weekday()
+        today = WEEKDAYS[day_by_computer]
+        return today
 
