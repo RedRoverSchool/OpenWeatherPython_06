f3b4d28c1932a335c357ef290a023b5183963eed
Julia <serebrennikova.julia@gmail.com>
2023-05-17 13:24:18 +0200

    Add TC_002.01.12

---
+++ tests/test_group_EPIC_team.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+import pytest
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
+URL = 'https://openweathermap.org/'
+Locator_002_01_12_Logo = (By.CSS_SELECTOR, '#first-level-nav > li.logo > a > img')
+
+
+def test_002_01_12_Verify_clicking_on_a_logo_from_the_Signin_page(driver):
+    driver.get('https://home.openweathermap.org/users/sign_in')
+    element = driver.find_element(*Locator_002_01_12_Logo)
+    element.click()
+    assert "openweathermap" in driver.current_url
