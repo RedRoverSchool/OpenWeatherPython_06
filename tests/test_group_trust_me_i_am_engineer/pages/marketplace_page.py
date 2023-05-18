a4f3a5a599917db9dcac07c2ed43b254209d6fc8
Andrey Bozhko <556070@gmail.com>
2023-05-18 19:32:37 +0300

    RF_AT_007.02.02 | Marketplace > Create New History Bulk > Visibility, clickability, search > Verify search by location name

--- tests/test_group_trust_me_i_am_engineer/pages/marketplace_page.py
+++ tests/test_group_trust_me_i_am_engineer/pages/marketplace_page.py
@@ -1,3 +1,7 @@
+import time
+
+from selenium.webdriver import Keys
+
 from pages.base_page import BasePage
 from tests.test_group_trust_me_i_am_engineer.locators.page_locators import MarketplacePageLocators
 
@@ -8,9 +12,25 @@ class MarketplacePage(BasePage):
     def verify_the_method_of_input_location(self):
         expected_method_list = ['By location', 'By coordinates', 'Import']
         self.driver.get(self.URL_MARKETPLACE)
-        self.driver.find_element(*self.locators.HISTORY_BILK_TITLE).click()
-        self.driver.find_element(*self.locators.HISTORY_BILK_SEARCH_LOCATION).click()
+        self.driver.find_element(*self.locators.HISTORY_BULK_TITLE).click()
+        self.driver.find_element(*self.locators.HISTORY_BULK_SEARCH_LOCATION).click()
         methods = self.driver.find_elements(*self.locators.BUTTON_SEARCH_METHODS)
         actual_method_list = [el.text for el in methods]
         assert expected_method_list == actual_method_list, \
             "The actual list of methods does not match the expected list of methods"
+
+    def verify_search_by_location_name(self):
+        expected_location = "Malta"
+        self.driver.get(self.URL_MARKETPLACE)
+        self.driver.find_element(*self.locators.HISTORY_BULK_TITLE).click()
+        search_loc = self.driver.find_element(*self.locators.HISTORY_BULK_SEARCH_LOCATION)
+
+        self.element_is_clickable(self.locators.MAP_BUTTON_LOC)
+        search_loc.click()
+        self.driver.find_element(*self.locators.BUTTON_BY_LOCATION).click()
+        search_loc.click()
+        search_loc.send_keys(expected_location + Keys.ARROW_DOWN)
+
+        self.element_is_clickable(self.locators.FIRST_SEARCH_ITEMS).click()
+        actual_search_result = self.element_is_visible(self.locators.SEARCH_POP_UP_HEADER)
+        assert expected_location == actual_search_result.text
