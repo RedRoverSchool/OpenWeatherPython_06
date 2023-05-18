7e19388c849ed85eb354af10d68fd91b1a374eca
marinapauli <marishka81@gmail.com>
2023-05-18 12:04:38 -0700

    Hourly forecast in the form of graphic for current location is displayed

--- tests/test_group_zmeyki.py
+++ tests/test_group_zmeyki.py
@@ -7,28 +7,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+URL = 'https://openweathermap.org/'
+search_city_field_locator = (By.CSS_SELECTOR, "input[placeholder='Search city']")
+search_button_locator = (By.CSS_SELECTOR, "button[class ='button-round dark']")
+search_dropdown_option = (By.CSS_SELECTOR, "ul.search-dropdown-menu li:nth-child(1) span:nth-child(1)")
+graphic_hourly_forecast_locator = (By.CSS_SELECTOR, "canvas[id=chart-component]")
 
-def test_graphic_hourly_forecast():
-    driver.get('https://openweathermap.org/')
-    driver.maximize_window()
-    search_city_field = driver.find_element(By.CSS_SELECTOR, "input[placeholder='Search city']")
-    search_city_field.send_keys('Los Angeles')
-    time.sleep(5)
-    search_button = driver.find_element(By.CSS_SELECTOR, "button[class='button-round dark']")
-    search_button.click()
-    search_option = WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
-        (By.CSS_SELECTOR, 'ul.search-dropdown-menu li:nth-child(1) span:nth-child(1)')))
-    time.sleep(5)
-    search_option.click()
-    time.sleep(5)
-    expected_city = 'Los Angeles, US'
-    displayed_city = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-        (By.CSS_SELECTOR, '.grid-container.grid-4-5 h2')))
-    displayed_city_text = displayed_city.text
-    print(displayed_city_text)
-    assert displayed_city_text == expected_city
-    graphic_hourly_forecast = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-        (By.CSS_SELECTOR, 'canvas[id=chart-component]')))
+
+def test_tc_001_08_01_graphic_hourly_forecast_is_displayed(driver, open_and_load_main_page, wait):
+    driver.get(URL)
+    graphic_hourly_forecast = driver.find_element(*graphic_hourly_forecast_locator)
     assert graphic_hourly_forecast.is_displayed()
-    print(graphic_hourly_forecast.is_displayed)
+
+
