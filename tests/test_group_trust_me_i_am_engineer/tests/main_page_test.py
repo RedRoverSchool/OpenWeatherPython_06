e7b138a1d30c185d9fd5b3bed2e73c02581c7778
AndreyBozhko <124488708+AndrSam1@users.noreply.github.com>
2023-05-18 09:59:17 +0300

    RF_AT_001.02.02 | Main page > Search city widget > Switching temperature measuring system > Verify temperature switched on imperial system (#494)
    RF_AT_001.02.02 | Main page > Search city widget > Switching temperature measuring system > Verify temperature switched on imperial system

--- tests/test_group_trust_me_i_am_engineer/tests/main_page_test.py
+++ tests/test_group_trust_me_i_am_engineer/tests/main_page_test.py
@@ -8,3 +8,7 @@ def test_tc_001_04_06_1_verify_visibility_of_week_days_in_8_days_forecast(driver
 def test_TC_001_02_01_verify_temperature_switched_on_metric_system(driver, open_and_load_main_page):
     page = MainPage(driver)
     page.checking_the_temperature_system_switching("°C")
+
+def test_TC_001_02_02_verify_temperature_switched_on_imperial_system(driver, open_and_load_main_page):
+    page = MainPage(driver)
+    page.checking_the_temperature_system_switching("°F")
