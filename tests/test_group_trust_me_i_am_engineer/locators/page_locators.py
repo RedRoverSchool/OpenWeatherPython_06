c6ca0ef5cb40a259447339b38790a12d3b315152
AndreyBozhko <124488708+AndrSam1@users.noreply.github.com>
2023-05-18 12:39:18 +0300

    RF_AT_001.05.01 | Main page > Search city widget > Weather for current location > Verify the current date and time (#497)

--- tests/test_group_trust_me_i_am_engineer/locators/page_locators.py
+++ tests/test_group_trust_me_i_am_engineer/locators/page_locators.py
@@ -4,6 +4,7 @@ class MainPageLocators:
     METRIC_BUTTON = (By.XPATH, "//div[@class='switch-container']/div[contains(text(), 'Metric')]")
     IMPERIAL_BUTTON = (By.XPATH, "//div[@class='switch-container']/div[contains(text(), 'Imperial')]")
     CURRENT_TEMP = (By.CSS_SELECTOR, "div.current-temp span.heading")
+    LOC_DATE_TIME = (By.XPATH, "//div[@class='current-container mobile-padding']/div/span[@class='orange-text']")
 
 class MarketplacePageLocators:
     pass
