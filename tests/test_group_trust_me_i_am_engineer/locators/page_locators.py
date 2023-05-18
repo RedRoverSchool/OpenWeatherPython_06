3c1a9712bb6328c9789a046e72e36b3d068bc674
Andrey Bozhko <556070@gmail.com>
2023-05-18 12:35:13 +0300

    RF_AT_005.04.01 | Weather API > Self > Page structure, functionality > Checking the title on the "Weather API" page

--- tests/test_group_trust_me_i_am_engineer/locators/page_locators.py
+++ tests/test_group_trust_me_i_am_engineer/locators/page_locators.py
@@ -5,5 +5,8 @@ class MainPageLocators:
     IMPERIAL_BUTTON = (By.XPATH, "//div[@class='switch-container']/div[contains(text(), 'Imperial')]")
     CURRENT_TEMP = (By.CSS_SELECTOR, "div.current-temp span.heading")
 
+class WeatherAPIPageLocators:
+    WEATHER_API_PAGE_TITLE = (By.CSS_SELECTOR, "h1.breadcrumb-title")
+
 class MarketplacePageLocators:
     pass
