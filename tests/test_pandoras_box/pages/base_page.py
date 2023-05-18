147c714950ffb43ed8ed3a28f99f5314a0bc394c
Anastasiya Pronina <apronina@piesoft.us>
2023-05-18 16:00:33 +0300

    RF_AT_002.03.03/01 | Header > Links > Navigation > "Guide" link leads to the correct page

--- tests/test_pandoras_box/pages/base_page.py
+++ tests/test_pandoras_box/pages/base_page.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 class BasePage:
 
     LINK_PRICING = (By.XPATH, '//div[@id="desktop-menu"]//a[text()="Pricing"]')
+    LINK_GUIDE = (By.XPATH, "//div[@id='desktop-menu']//a[text()='Guide']")
 
     def __init__(self, driver, link=None):
         self.driver = driver
@@ -17,3 +18,7 @@ class BasePage:
                 button_pricing = self.driver.find_element(*self.LINK_PRICING)
                 action_chains.move_to_element(button_pricing)
                 self.driver.execute_script("arguments[0].click();", button_pricing)
+            case 'guide':
+                button_guide = self.driver.find_element(*self.LINK_GUIDE)
+                action_chains.move_to_element(button_guide)
+                self.driver.execute_script("arguments[0].click();", button_guide)
