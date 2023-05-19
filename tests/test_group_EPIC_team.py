3069f6a36c255a01ac694c9a7565dd75e72c2cef
Julia Venskutoniene <julia.venskutone@gmail.com>
2023-05-19 13:14:06 +0100

    changes according to Aigul comments

--- tests/test_group_EPIC_team.py
+++ tests/test_group_EPIC_team.py
@@ -3,10 +3,10 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 
-def test_open_faq_page(driver):
+
+def test_TC_003_12_10_open_faq_page(driver, wait):
     driver.get('https://openweathermap.org/faq')
-    time.sleep(5)
-    assert 'openweathermap' in driver.current_url
-    print(driver.current_url)
+    assert driver.current_url == 'https://openweathermap.org/faq'
     driver.quit()
