9b18797347502cd0ffe7c842e92fe9241108f4cf
Badx86 <stasure@gmail.com>
2023-05-20 23:00:05 +0400

    add second execute script

--- tests/test_group_future_auto_qa/pages/for_business_page.py
+++ tests/test_group_future_auto_qa/pages/for_business_page.py
@@ -10,6 +10,6 @@ class ForBusinessPage(BasePage):
         self.driver.execute_script("arguments[0].click();", for_business)
         self.driver.switch_to.window(self.driver.window_handles[1])
         products = self.element_is_visible(self.locators.PRODUCTS_IN_HEADER, 25)
-        products.click()
+        self.driver.execute_script("arguments[0].click();", products)
         return self.elements_are_present(self.locators.PRODUCTS_HEADINGS, 25)
 
