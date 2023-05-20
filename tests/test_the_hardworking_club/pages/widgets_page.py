3f1ee51e8058cabd45a835e244052e3f6f8a3d63
MarinaNikita <marina94065@gmail.com>
2023-05-19 18:47:51 -0600

    feat: add RF_TC_001_09_07_verify_display_of_bottom_widget_1_for_selected_type

--- tests/test_the_hardworking_club/pages/widgets_page.py
+++ tests/test_the_hardworking_club/pages/widgets_page.py
@@ -5,11 +5,19 @@ from conftest import driver
 
 class WidgetsPage(BasePage):
     locators = WidgetsPageLocators()
+    url_widgets_page = 'https://openweathermap.org/widgets-constructor'
 
     def check_input_fields(self):
+        self.driver.get(WidgetsPage.url_widgets_page)
         fields = self.driver.find_elements(*self.locators.INPUT_FIELDS)
         for field in fields:
             assert field.is_displayed()
+
+    def verify_display_of_bottom_widget_1_for_selected_type(self):
+        self.driver.get(WidgetsPage.url_widgets_page)
+        self.driver.find_element(*self.locators.TYPE_WIDGET_1).click()
+        left_bottom_widget_appeared = self.element_is_visible(self.locators.LEFT_BOTTOM_WIDGET)
+        assert left_bottom_widget_appeared.is_displayed()
 
 
 
--- tests/test_the_hardworking_club/tests_my/test_widgets_elements.py
+++ tests/test_the_hardworking_club/tests_my/test_widgets_elements.py
@@ -3,10 +3,16 @@ from tests.test_the_hardworking_club.pages.widgets_page import WidgetsPage
 
 class TestWidgets:
 
+
     def test_TC_001_09_04_Input_fields_visible(self, driver):
 
-        widgets_page = WidgetsPage(driver, 'https://openweathermap.org/widgets-constructor')
-        widgets_page.open_page()
+        widgets_page = WidgetsPage(driver)
+        widgets_page.check_input_fields()
+
+    def test_TC_001_09_07_verify_display_of_bottom_widget_1_for_selected_type(self, driver):
+
+        widgets_page = WidgetsPage(driver)
+        widgets_page.verify_display_of_bottom_widget_1_for_selected_type()
 
 
 
