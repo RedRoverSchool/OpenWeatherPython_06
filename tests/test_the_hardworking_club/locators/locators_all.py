58a9b5e9094a04511eeac4891740757398047386
MarinaNikita <117684170+MarinaNikita@users.noreply.github.com>
2023-05-19 18:55:11 -0600

    feat: add RF_TC_001_09_07_verify_display_of_bottom_widget_1_for_selected_type (#523)

--- tests/test_the_hardworking_club/locators/locators_all.py
+++ tests/test_the_hardworking_club/locators/locators_all.py
@@ -8,6 +8,9 @@ class WidgetsPageLocators():
     # CITY_NAME = (By.CSS_SELECTOR, "#city-name")
 
     INPUT_FIELDS = (By.CSS_SELECTOR, '.form-control')
+    TYPE_WIDGET_1 = (
+        By.XPATH, '//img[contains(@src, "themes/openweathermap/assets/vendor/owm/img/widgets/type-brown.png")]')
+    LEFT_BOTTOM_WIDGET = (By.XPATH, '//div/*[@class="widget-left-menu widget-left-menu--brown"]')
 
 
 
