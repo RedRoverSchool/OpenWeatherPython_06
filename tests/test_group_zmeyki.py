b32a2a19fbf0fb4a4c4fe0fb3bb158eab332840e
Niko-03 <ksveta10@mail.ru>
2023-05-20 22:55:47 +0800

    AT_006.01.01 | Weather Dashboard > Description > Verify Description of Open Weather Dashboard

--- tests/test_group_zmeyki.py
+++ tests/test_group_zmeyki.py
@@ -20,3 +20,29 @@ def test_tc_001_08_01_graphic_hourly_forecast_is_displayed(driver, open_and_load
     assert graphic_hourly_forecast.is_displayed()
 
 
+# ======= TC_006_01_01 =================================
+
+button_dashboard_locator = (By.CSS_SELECTOR, '#desktop-menu a[href="/weather-dashboard"]')
+dashboard_h1_locator = (By.CSS_SELECTOR, 'h1[class="h1-break"]')
+dashboard_h3_locator = (By.CSS_SELECTOR, '.col-lg-6 h3')
+dashboard_img_locator = (By.CSS_SELECTOR, '.col-md-6 img')
+
+
+def test_tc_006_01_01_verify_description_of_open_weather_dashboard(driver, open_and_load_main_page, wait):
+    driver.get(URL)
+    button_dashboard = driver.find_element(*button_dashboard_locator)
+    button_dashboard.click()
+
+    expected_dashboard_h1 = 'OpenWeather\nDashboard'
+    actual_dashboard_h1 = driver.find_element(*dashboard_h1_locator)
+    actual_dashboard_h1_text = actual_dashboard_h1.text
+    assert actual_dashboard_h1_text == expected_dashboard_h1
+
+    expected_dashboard_h3 = 'A visual tool for working with weather data and timely tracking of dangerous phenomena'
+    actual_dashboard_h3 = driver.find_element(*dashboard_h3_locator)
+    actual_dashboard_h3_text = actual_dashboard_h3.text
+    assert actual_dashboard_h3_text == expected_dashboard_h3
+
+    expected_dashboard_img = driver.find_element(*dashboard_img_locator)
+    assert expected_dashboard_img
+
