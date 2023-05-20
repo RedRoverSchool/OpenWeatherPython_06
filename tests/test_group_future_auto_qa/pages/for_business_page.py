0d5f4e706658c1b0619ba474b6afeadd49ac8c5a
Badx86 <stasure@gmail.com>
2023-05-20 23:20:03 +0400

    add TimeoutException

--- tests/test_group_future_auto_qa/pages/for_business_page.py
+++ tests/test_group_future_auto_qa/pages/for_business_page.py
@@ -1,3 +1,5 @@
+from selenium.common import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
 from tests.test_group_future_auto_qa.locators.for_business_page_locators import ForBusinessPageLocators
 from pages.base_page import BasePage
 
@@ -6,10 +8,12 @@ class ForBusinessPage(BasePage):
     locators = ForBusinessPageLocators()
 
     def check_headings(self):
-        for_business = self.element_is_clickable(self.locators.FOR_BUSINESS, 25)
-        self.driver.execute_script("arguments[0].click();", for_business)
-        self.driver.switch_to.window(self.driver.window_handles[1])
-        products = self.element_is_visible(self.locators.PRODUCTS_IN_HEADER, 25)
-        self.driver.execute_script("arguments[0].click();", products)
-        return self.elements_are_present(self.locators.PRODUCTS_HEADINGS, 25)
-
+        try:
+            for_business = self.element_is_clickable(self.locators.FOR_BUSINESS, 25)
+            self.driver.execute_script("arguments[0].click();", for_business)
+            self.driver.switch_to.window(self.driver.window_handles[1])
+            products = self.element_is_visible(self.locators.PRODUCTS_IN_HEADER, 20)
+            self.driver.execute_script("arguments[0].click();", products)
+            return self.elements_are_present(self.locators.PRODUCTS_HEADINGS, 20)
+        except TimeoutException:
+            return []
