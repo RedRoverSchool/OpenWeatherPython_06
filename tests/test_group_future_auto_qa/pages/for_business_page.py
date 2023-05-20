5d9304d035a1842da616814c95eedf1d8c85aeb1
Badx86 <stasure@gmail.com>
2023-05-20 22:47:28 +0400

    remove open_and_load_main_page in TestBusinessPage and wait time from 15 to 25

--- tests/test_group_future_auto_qa/pages/for_business_page.py
+++ tests/test_group_future_auto_qa/pages/for_business_page.py
@@ -6,10 +6,10 @@ class ForBusinessPage(BasePage):
     locators = ForBusinessPageLocators()
 
     def check_headings(self):
-        for_business = self.element_is_clickable(self.locators.FOR_BUSINESS, 15)
+        for_business = self.element_is_clickable(self.locators.FOR_BUSINESS, 25)
         self.driver.execute_script("arguments[0].click();", for_business)
         self.driver.switch_to.window(self.driver.window_handles[1])
-        products = self.element_is_visible(self.locators.PRODUCTS_IN_HEADER, 15)
+        products = self.element_is_visible(self.locators.PRODUCTS_IN_HEADER, 25)
         products.click()
-        headings = self.elements_are_present(self.locators.PRODUCTS_HEADINGS, 15)
+        headings = self.elements_are_present(self.locators.PRODUCTS_HEADINGS, 25)
         return headings
