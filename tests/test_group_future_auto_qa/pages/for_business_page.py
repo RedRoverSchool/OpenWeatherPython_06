c852e09cb25dc78cdb146b652a15b89b7012467a
Badx86 <stasure@gmail.com>
2023-05-20 23:55:43 +0400

    nth try

--- tests/test_group_future_auto_qa/pages/for_business_page.py
+++ tests/test_group_future_auto_qa/pages/for_business_page.py
@@ -8,10 +8,10 @@ class ForBusinessPage(BasePage):
     locators = ForBusinessPageLocators()
 
     def check_headings(self):
-        for_business = self.element_is_clickable(self.locators.FOR_BUSINESS, 25)
+        for_business = self.element_is_clickable(self.locators.FOR_BUSINESS, 15)
         self.driver.execute_script("arguments[0].click();", for_business)
         self.driver.switch_to.window(self.driver.window_handles[1])
-        products = self.element_is_clickable(self.locators.PRODUCTS_IN_HEADER, 20)
-        self.driver.execute_script("arguments[0].click();", products)
-        return self.elements_are_present(self.locators.PRODUCTS_HEADINGS, 20)
-
+        products = self.element_is_visible(self.locators.PRODUCTS_IN_HEADER, 15)
+        products.click()
+        headings = self.elements_are_present(self.locators.PRODUCTS_HEADINGS, 15)
+        return headings
--- tests/test_group_future_auto_qa/tests/for_business_page_test.py
+++ tests/test_group_future_auto_qa/tests/for_business_page_test.py
@@ -3,8 +3,9 @@ from tests.test_group_future_auto_qa.pages.for_business_page import ForBusinessP
 
 
 class TestBusinessPage:
-    def test_tc_012_01_04_verify_the_headings_are_present_on_the_page(self, driver):
+    def test_tc_012_01_04_verify_the_headings_are_present_on_the_page(self, driver, open_and_load_main_page):
         business_page = ForBusinessPage(driver, URL)
         business_page.open_page()
-        assert business_page.check_headings(), "No headings are present on the page"
+        headers_count = business_page.check_headings()
+        assert len(headers_count) == 7, "Not all headings are present on the page"
 
