d03438bdb34420d988ef6abe613d821621fe1244
Badx86 <stasure@gmail.com>
2023-05-21 00:59:37 +0400

    another try

--- tests/test_group_future_auto_qa/locators/for_business_page_locators.py
+++ tests/test_group_future_auto_qa/locators/for_business_page_locators.py
@@ -5,3 +5,4 @@ class ForBusinessPageLocators:
     FOR_BUSINESS = (By.CSS_SELECTOR, "div[id='desktop-menu'] a[class='marketplace']")
     PRODUCTS_IN_HEADER = (By.CSS_SELECTOR, "a[href='/products']")
     PRODUCTS_HEADINGS = (By.CSS_SELECTOR, "a big")
+    ALLOW_ALL_COOKIES = (By.XPATH, "//button[contains(text(), 'Allow all')]")
--- tests/test_group_future_auto_qa/tests/for_business_page_test.py
+++ tests/test_group_future_auto_qa/tests/for_business_page_test.py
@@ -1,11 +1,8 @@
-from conftest import URL
 from tests.test_group_future_auto_qa.pages.for_business_page import ForBusinessPage
 
 
 class TestBusinessPage:
     def test_tc_012_01_04_verify_the_headings_are_present_on_the_page(self, driver, open_and_load_main_page):
-        business_page = ForBusinessPage(driver, URL)
-        business_page.open_page()
+        business_page = ForBusinessPage(driver)
         headers_count = business_page.check_headings()
         assert len(headers_count) == 7, "Not all headings are present on the page"
-
