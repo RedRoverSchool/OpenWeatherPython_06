8698b4896a00e150d5a87b67ff329244d1f24092
Evgeniia Demchenko <demchenkoeb04022012@gmail.com>
2023-05-21 08:07:02 +0300

    RF | AT test_TC_001_04_06_1 (#547)
    * RF | AT test_TC_001_04_06_1
    
    * RF | AT test_TC_001_04_06_1

--- tests/test_group_trust_me_i_am_engineer/tests/main_page_test.py
+++ tests/test_group_trust_me_i_am_engineer/tests/main_page_test.py
@@ -3,7 +3,7 @@ import pytest
 
 def test_tc_001_04_06_1_verify_visibility_of_week_days_in_8_days_forecast(driver, open_and_load_main_page, wait):
     page = MainPage(driver)
-    page.verify_weekdays_8days_forecast()
+    page.verify_weekdays_in_8_days_forecast()
 
 def test_TC_001_02_01_verify_temperature_switched_on_metric_system(driver, open_and_load_main_page):
     page = MainPage(driver)
