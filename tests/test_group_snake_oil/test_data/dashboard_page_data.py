414d075ff08a4791b2084738aaaa1569b92a6cd8
Ibragim Visitaev <v.ibragim13@gmail.com>
2023-05-21 16:26:41 -0700

    RF_AT_006.02.05 | Weather Dashboard > "How to start" block > Verify all links redirecting to the respective pages (signed in) (#576)

--- tests/test_group_snake_oil/test_data/dashboard_page_data.py
+++ tests/test_group_snake_oil/test_data/dashboard_page_data.py
@@ -3,12 +3,12 @@ from tests.test_group_snake_oil.links.all_links import EXPEXTED_URLS_SIGNED_IN_L
 
 # List of tuples, containing locators of text-links and respective URLs,
 # where user expected to be redirected to, when NOT signed-in user clicks the text-link
-EXPECTED_LINKS_AND_LOCATORS_SIGNED_OUT = [(DashboardPageLocators.ALL_TEXT_LINK_LOCATORS[i],
-                                           EXPECTED_URLS_SIGNED_OUT_LIST[i])
-                                          for i in range(len(DashboardPageLocators.ALL_TEXT_LINK_LOCATORS))]
+EXPECTED_URLS_AND_LOCATORS_SIGNED_OUT = [(DashboardPageLocators.ALL_TEXT_LINK_LOCATORS[i],
+                                         EXPECTED_URLS_SIGNED_OUT_LIST[i])
+                                         for i in range(len(DashboardPageLocators.ALL_TEXT_LINK_LOCATORS))]
 
 # List of tuples, containing locators of text-links in and respective URLs,
 # where user expected to be redirected to, when signed-in user clicks the text-link
-EXPECTED_LINKS_AND_LOCATORS_SIGNED_IN = [(DashboardPageLocators.ALL_TEXT_LINK_LOCATORS[i],
-                                          EXPEXTED_URLS_SIGNED_IN_LIST[i])
-                                         for i in range(len(DashboardPageLocators.ALL_TEXT_LINK_LOCATORS))]
+EXPECTED_URLS_AND_LOCATORS_SIGNED_IN = [(DashboardPageLocators.ALL_TEXT_LINK_LOCATORS[i],
+                                        EXPEXTED_URLS_SIGNED_IN_LIST[i])
+                                        for i in range(len(DashboardPageLocators.ALL_TEXT_LINK_LOCATORS))]
