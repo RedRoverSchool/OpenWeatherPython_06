b71a49a6fc6f1911a683be75a55a6ee873215c61
umabe <umabenzema@gmail.com>
2023-05-21 15:56:14 -0400

    test added

--- tests/test_group_100000/locators/dashboard_page_locators.py
+++ tests/test_group_100000/locators/dashboard_page_locators.py
@@ -4,3 +4,7 @@ from selenium.webdriver.common.by import By
 class DashboardLocators:
     BTN_DASHBOARD = (By.CSS_SELECTOR, "#desktop-menu [href$=-dashboard]")
     TITLE_HOW_TO_START = (By.XPATH, "//div/h2[contains(text(),'How to Start')]")
+
+
+class DashboardLocators:
+    WEATHER_SYMBOL = (By.CSS_SELECTOR, "ul  > li:nth-child(3) > span.symbol")
--- tests/test_group_100000/pages/dashboard_page.py
+++ tests/test_group_100000/pages/dashboard_page.py
@@ -1,5 +1,6 @@
 from pages.base_page import BasePage
 from tests.test_group_100000.locators.dashboard_page_locators import DashboardLocators as D
+from tests.test_group_100_000 import weather_symbol
 
 
 class DashboardPage(BasePage):
@@ -7,3 +8,8 @@ class DashboardPage(BasePage):
         self.click_header_link('dashboard')
         section = self.driver.find_element(*D.TITLE_HOW_TO_START)
         assert section.is_displayed(), "Section not found"
+
+class DashboardPage(BasePage):
+    def test_TC_001_05_03_Verify_humidity_percentage_in_detailed_weather_data_for_current_location(self):
+        humidity_symbol = self.driver.find_element(*weather_symbol)
+        assert humidity_symbol.is_displayed()
