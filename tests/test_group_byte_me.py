7d419affce14379c22b4f99c568691b55ffc3004
Elena Pozhydaieva <epozhydaieva@gmail.com>
2023-05-21 13:52:20 -0400

    change code and locator in test_tc_002_02_01_01_search_city_results_are_visible

--- tests/test_group_byte_me.py
+++ tests/test_group_byte_me.py
@@ -1,3 +1,4 @@
+from selenium.webdriver.support import expected_conditions as EC
 
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
@@ -5,7 +6,7 @@ from selenium.webdriver.common.by import By
 
 WEATHER_IN_YOUR_CITY_FLD = (By.CSS_SELECTOR, "#desktop-menu input:nth-child(1)")
 REQUESTED_CITY = 'London, GB'
-DISPLAYED_CITY = (By.XPATH, '//*[@id="forecast_list_ul"]/table/tbody/tr/td[2]/b[1]/a')
+DISPLAYED_CITY = (By.XPATH, "//*[@Class='table']//tr[1]//b/a")
 
 
 def test_tc_002_02_01_01_search_city_results_are_visible(driver, wait, open_and_load_main_page):
@@ -15,5 +16,5 @@ def test_tc_002_02_01_01_search_city_results_are_visible(driver, wait, open_and_
     driver.execute_script("arguments[0].click();", weather_in_your_city_field)
     weather_in_your_city_field.send_keys(*REQUESTED_CITY)
     weather_in_your_city_field.submit()
-    displayed_text = driver.find_element(*DISPLAYED_CITY)
-    assert displayed_text.text in REQUESTED_CITY
+    displayed_text = wait.until(EC.visibility_of_element_located(DISPLAYED_CITY))
+    assert displayed_text.text == REQUESTED_CITY
