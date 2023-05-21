a14b4411a2b756fd400a1617f1d12ab7750599fb
evgeniia demchenko <demchenkoeb04022012@gmail.com>
2023-05-21 05:49:58 +0300

    RF | AT test_TC_001_04_06_1

--- tests/test_group_trust_me_i_am_engineer/locators/page_locators.py
+++ tests/test_group_trust_me_i_am_engineer/locators/page_locators.py
@@ -8,6 +8,10 @@ class MainPageLocators:
     CITY_NAME = (By.CSS_SELECTOR, "div.current-container.mobile-padding div h2")
     LOC = (By.CSS_SELECTOR, "div.control-el svg.icon-current-location")
     LOAD_DIV = (By.CSS_SELECTOR, 'div.owm-loader-container > div')
+    SEARCH_CITY_FIELD = (By.CSS_SELECTOR, 'input[placeholder="Search city"]')
+    SEARCH_BUTTON = (By.CSS_SELECTOR, 'button[class ="button-round dark"]')
+    SEARCH_OPTION_FROM_DROPDOWN_LIST = (By.XPATH, "//span[contains(text(), city)]")
+    WEEKDAY_IN_8_DAYS_FORECAST = (By.XPATH, "//div//li[@data-v-5ed3171e]/span")
 
 class WeatherAPIPageLocators:
     WEATHER_API_PAGE_TITLE = (By.CSS_SELECTOR, "h1.breadcrumb-title")
