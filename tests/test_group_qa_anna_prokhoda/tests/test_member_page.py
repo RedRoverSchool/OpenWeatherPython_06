3e1d7b051d2c8686214fa511875f64adbe3fe55b
ilonaborsuk <ilonaborsuk@gmail.com>
2023-05-21 15:57:35 +0200

    AT_018.03.01 > Subscriptions >Subscribe to Developer> Subscription payment is available > Verify redirection to payment-service page for unauthorized user

---
+++ tests/test_group_qa_anna_prokhoda/tests/test_member_page.py
@@ -0,0 +1,12 @@
+import time
+
+from tests.test_group_qa_anna_prokhoda.pages.member_page import MemberPage
+
+
+def test_tc_018_03_01_verify_redirection_to_payment_service_page_for_unauthorized_user(driver):
+    member_page = MemberPage(driver)
+    member_page.get_link('dev')
+    member_page.fill_in_required_fields()
+    member_page.click_continue_button()
+    member_page.check_payment_url()
+    time.sleep(3)
