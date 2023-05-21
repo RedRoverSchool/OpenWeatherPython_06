26080280928d47acacac728aded74bd6efd49b69
ChosenOnelru <chosenonex1@gmail.com>
2023-05-21 20:50:07 +0300

    ref: RF AT_002.01.03 self.pricing_plans_locators

--- tests/test_pandoras_box/pages/weather_dashboard_page.py
+++ tests/test_pandoras_box/pages/weather_dashboard_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from pages.base_page import BasePage
+
 class WeatherDashboard(BasePage):
 
     dashboard_URL = 'https://openweathermap.org/weather-dashboard'
@@ -9,7 +10,7 @@ class WeatherDashboard(BasePage):
                               (By.XPATH, '//h4[text()="Professional"]'),
                               (By.XPATH, '//h4[text()="Enterprise"]')]
 
-    def check_pricing_plans(self, pricing_plans_locators):
-        for plan_locator in pricing_plans_locators:
+    def check_pricing_plans(self):
+        for plan_locator in self.pricing_plans_locators:
             plan = self.driver.find_element(*plan_locator)
             assert plan.is_displayed()
