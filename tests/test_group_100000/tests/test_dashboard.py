260bc29ca97566179c87356146b4c4f713b477be
Ekaterina Myshov <79929359+KatyaMy@users.noreply.github.com>
2023-05-21 15:40:26 -0400

    RF_AT_006.02.03 | Weather Dashboard > Verify the transition to another page when clicking on the button [Try the Dashboard], for non-signed-in user (#566)

--- tests/test_group_100000/tests/test_dashboard.py
+++ tests/test_group_100000/tests/test_dashboard.py
@@ -4,3 +4,8 @@ from tests.test_group_100000.pages.dashboard_page import DashboardPage
 def test_TC_006_02_01_verify_display_of_how_to_start_section(driver, open_and_load_main_page):
     page = DashboardPage(driver)
     page.verify_display_of_how_to_start_section()
+
+
+def test_TC_006_02_03_weather_dashboard_verify_the_transition_to_another_page(driver, open_and_load_main_page):
+    page = DashboardPage(driver)
+    page.transition_to_another_page()
