87c59b63c2200b0ae4efca633579e42d1bb874d4
alena-krauchuk <94075420+alena-krauchuk@users.noreply.github.com>
2023-05-22 11:51:54 +0300

    AT_003.03.03 | Footer > Product Collections module > Historical Weather Data link visibility (#571)

--- tests/test_group_bugs_in_each_step/locators/main_page_locators.py
+++ tests/test_group_bugs_in_each_step/locators/main_page_locators.py
@@ -2,5 +2,8 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators:
+
     COOKIES = (By.XPATH, "//button[contains(text(), 'Allow all')]")
+    HISTORICAL_WEATHER_DATA = (By.XPATH, "//a[contains(text(), 'Historical Weather Data')]")
+
 
--- tests/test_group_bugs_in_each_step/test_data/urls.py
+++ tests/test_group_bugs_in_each_step/test_data/urls.py
@@ -1,4 +1,7 @@
-class SignInUrls:
+class MainPageUrls:
+    url_main_page = "https://home.openweathermap.org"
 
+
+class SignInUrls:
     url_sign_in_page = "https://home.openweathermap.org/users/sign_in"
 
