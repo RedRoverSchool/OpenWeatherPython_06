1f901ecdbb4acbe54c7e8ca19385e2145030050e
ilonaborsuk <ilonaborsuk@gmail.com>
2023-05-22 16:24:12 +0200

    create group_anna_prokhoda POM structure

---
+++ tests/test_group_anna_prokhoda/tests/test_member_page.py
@@ -0,0 +1,7 @@
+import time
+
+from tests.test_group_anna_prokhoda.pages.member_page import MemberPage
+
+
+def test_tc_018_03_01_verify_redirection_to_payment_service_page_for_unauthorized_user(driver):
+    pass
