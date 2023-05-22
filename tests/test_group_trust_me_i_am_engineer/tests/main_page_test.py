6c4cd01baa3bc24b9b45a85b76e83a81947b135f
daria <yukhnina.d@gmail.com>
2023-05-22 18:51:18 +0300

    RF_AT_008.01.03_01 | Pricing > Self> Page structure, functionality > check a visibility of ‘Pricing’ page title

--- tests/test_group_trust_me_i_am_engineer/tests/main_page_test.py
+++ tests/test_group_trust_me_i_am_engineer/tests/main_page_test.py
@@ -1,4 +1,5 @@
-from OpenWeatherPython_06.tests.test_group_trust_me_i_am_engineer.pages.main_page import MainPage
+
+from ....pages.main_page import MainPage
 import pytest
 
 def test_tc_001_04_06_1_verify_visibility_of_week_days_in_8_days_forecast(driver, open_and_load_main_page, wait):
