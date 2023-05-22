5589c64a784258cb066b41a2c3d596ed1b0be345
Julia Venskutoniene <julia.venskutone@gmail.com>
2023-05-22 13:20:22 +0100

    driver.quit() line deleted

--- tests/test_group_EPIC_team.py
+++ tests/test_group_EPIC_team.py
@@ -9,4 +9,3 @@ from selenium.webdriver.support.wait import WebDriverWait
 def test_TC_003_12_10_open_faq_page(driver, wait):
     driver.get('https://openweathermap.org/faq')
     assert driver.current_url == 'https://openweathermap.org/faq'
-    driver.quit()
