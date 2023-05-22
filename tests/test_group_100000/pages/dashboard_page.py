9a168e357597c0ff1390e7e7acc3860fd79ac70e
Uma Talpakova <umabenzema@gmail.com>
2023-05-21 22:17:47 -0400

    test added (#570)

--- tests/test_group_100000/pages/dashboard_page.py
+++ tests/test_group_100000/pages/dashboard_page.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 
 from pages.base_page import BasePage
 from tests.test_group_100000.locators.dashboard_page_locators import DashboardLocators as D
+from tests.test_group_100_000 import weather_symbol
 
 BTN_COOKIES = (By.CLASS_NAME, "stick-footer-panel__link")
 
@@ -12,6 +13,7 @@ class DashboardPage(BasePage):
         section = self.driver.find_element(*D.TITLE_HOW_TO_START)
         assert section.is_displayed(), "Section not found"
 
+
     def transition_to_another_page(self):
         self.click_header_link('dashboard')
         cookie_close = self.driver.find_element(*BTN_COOKIES)
@@ -20,3 +22,9 @@ class DashboardPage(BasePage):
         self.driver.switch_to.window(self.driver.window_handles[1])
         alert_mms = self.driver.find_element(*D.PANEL_SIGN_IN_FORM)
         assert alert_mms.is_displayed(), 'WELCOME EVENTS'
+
+        
+    def test_TC_001_05_03_Verify_humidity_percentage_in_detailed_weather_data_for_current_location(self):
+        humidity_symbol = self.driver.find_element(*weather_symbol)
+        assert humidity_symbol.is_displayed()
+
