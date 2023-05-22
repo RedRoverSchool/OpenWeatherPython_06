865eed266d9077d517002d9b1fa88a7d919816f2
Nikita <devil732704@gmail.com>
2023-05-22 16:54:41 +0500

    RF_AT_003.03.02 | Footer > Product Collections module > Current and Forecast APIs link сlickability

--- tests/test_group_bugs_in_each_step.py
+++ tests/test_group_bugs_in_each_step.py
@@ -12,7 +12,7 @@ def test_TC_003_12_04_current_and_forecast_apis_functionality(driver, open_and_l
     assert 'https://openweathermap.org/api#current' in driver.current_url
 
 
-def test_TC_003_03_02_verify_current_and_forecast_apis_is_clickable(driver, open_and_load_main_page):
+def test_TC_003_03_02_verify_current_and_forecast_apis_is_clickable(driver, open_and_load_main_page, wait):
     element = driver.find_element(*CURRENT_AND_FORECAST_APIS)
     wait.until(EC.element_to_be_clickable(CURRENT_AND_FORECAST_APIS))
     assert element.is_displayed() and element.is_enabled()
