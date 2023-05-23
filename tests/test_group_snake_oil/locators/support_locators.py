c97accde120f2c11732bee4e146f727cd918e498
nicolaeva <lanaa89374@gmail.com>
2023-05-22 18:58:41 -0700

    RF_015.01.01 | Support > FAQ > Visibility, сlickability, correct links redirection and dropdown > Verify 'FAQ' is visible

--- tests/test_group_snake_oil/locators/support_locators.py
+++ tests/test_group_snake_oil/locators/support_locators.py
@@ -2,5 +2,6 @@ from selenium.webdriver.common.by import By
 
 
 class SupportPageLocators:
-    Support_dropdown = (By.ID, "support-dropdown")
-    FAQ_element = (By.XPATH, "//*[@id='support-dropdown-menu']/li[1]/a")
+    SUPPORT_DROPDOWN = (By.ID, "support-dropdown")
+    FAQ_ELEMENT = (By.XPATH, "//*[@id='support-dropdown-menu']/li[1]/a")
+    OVERLAY_LOCATOR = (By.CLASS_NAME, "owm-loader")
--- tests/test_group_snake_oil/pages/support_page.py
+++ tests/test_group_snake_oil/pages/support_page.py
@@ -1,21 +1,18 @@
 from pages.base_page import BasePage
 from tests.test_group_snake_oil.locators.support_locators import SupportPageLocators
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
+
 
 class SupportPage(BasePage):
     locators = SupportPageLocators
 
     def check_visibility_and_clickablity_of_Support_dropdown(self):
-        overlay_locator = (By.CLASS_NAME, "owm-loader")
-
-        wait(self.driver, timeout=10).until(EC.invisibility_of_element_located(overlay_locator))
-        dropdown = wait(self.driver, timeout=10).until(EC.element_to_be_clickable(self.locators.Support_dropdown))
+        wait(self.driver, timeout=10).until(EC.invisibility_of_element_located(self.locators.OVERLAY_LOCATOR))
+        dropdown = wait(self.driver, timeout=10).until(EC.element_to_be_clickable(self.locators.SUPPORT_DROPDOWN))
         dropdown.click()
         assert dropdown.is_enabled(), "Dropdown element is not clicked"
 
     def check_visibility_of_FAQ_element(self):
-        element = self.element_is_visible(self.locators.FAQ_element)
+        element = self.element_is_visible(self.locators.FAQ_ELEMENT)
         assert element.is_displayed(), "FAQ element is not clickable on the page"
-
