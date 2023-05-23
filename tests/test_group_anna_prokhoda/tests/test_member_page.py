82e41ca45a9864ecb258d0734604982a403e6fcb
ilonaborsuk <ilonaborsuk@gmail.com>
2023-05-23 09:42:41 +0200

    added AT_018.03.01

--- tests/test_group_anna_prokhoda/tests/test_member_page.py
+++ tests/test_group_anna_prokhoda/tests/test_member_page.py
@@ -4,4 +4,10 @@ from tests.test_group_anna_prokhoda.pages.member_page import MemberPage
 
 
 def test_tc_018_03_01_verify_redirection_to_payment_service_page_for_unauthorized_user(driver):
-    pass
+    member_page = MemberPage(driver)
+    member_page.get_link('dev')
+    member_page.fill_in_required_fields()
+    member_page.click_continue_button()
+    time.sleep(5)
+    member_page.check_payment_url()
+
