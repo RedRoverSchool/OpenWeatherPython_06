2a657d26d4ed2fbe73b2fa6a5ad1c7a00c1cee5f
Stanislav_Osipov <badlolpro@gmail.com>
2023-05-24 09:38:56 +0400

    RF_AT_010.01.02_02 | Our Initiatives > Verify the functionality of 'Our Initiatives' section (#625)

--- tests/test_group_ducktales/locators/our_initiatives_page_locators.py
+++ tests/test_group_ducktales/locators/our_initiatives_page_locators.py
@@ -4,5 +4,5 @@ from selenium.webdriver.common.by import By
 class OurInitiativesPageLocators:
     INITIATIVES = By.CSS_SELECTOR, "ul[id='first-level-nav'] li:nth-child(7) a:nth-child(1)"
     SECTION = (By.XPATH, "//span[contains(text(), '{}')]")
-
+    EDUCATION_LEARN_MORE = By.CSS_SELECTOR, ".ow-btn.round.btn-black"
 
--- tests/test_group_ducktales/tests/test_our_initiatives_page.py
+++ tests/test_group_ducktales/tests/test_our_initiatives_page.py
@@ -1,6 +1,7 @@
 import pytest
 from tests.test_group_ducktales.pages.our_initiatives_page import OurInitiativesPage
-from tests.test_group_ducktales.test_data.our_initiatives_page_data import sections, OUR_INITIATIVES_PAGE
+from tests.test_group_ducktales.test_data.our_initiatives_page_data import *
+from tests.test_group_ducktales.locators.our_initiatives_page_locators import OurInitiativesPageLocators
 
 
 class TestOurInitiativesPage:
@@ -11,3 +12,10 @@ class TestOurInitiativesPage:
         page.click_initiatives_link()
         section_element = page.get_section_element(section)
         assert section_element.is_displayed(), f"Section '{section}' not found on the page"
+
+    def test_010_01_02_02_functionality(self, driver, open_and_load_main_page):
+        page = OurInitiativesPage(driver, EDUCATION_SECTION_PAGE)
+        page.open_page()
+        page.click_initiatives_link()
+        page.click_education_learn_more()
+        assert driver.current_url == EDUCATION_SECTION_PAGE
