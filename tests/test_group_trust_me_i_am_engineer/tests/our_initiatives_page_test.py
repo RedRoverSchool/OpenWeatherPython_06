2c1824942dd14b40bf4dc25c263e78af59208797
TatianaYurchenko <102853445+TatianaYurchenko@users.noreply.github.com>
2023-05-24 10:05:40 +0300

    RF_AT_010.01.02 | Our Initiatives > Visibility, сlickability, correct redirection>Verify "Learn more" button is clickable (#632)

--- tests/test_group_trust_me_i_am_engineer/tests/our_initiatives_page_test.py
+++ tests/test_group_trust_me_i_am_engineer/tests/our_initiatives_page_test.py
@@ -4,3 +4,7 @@ import pytest
 def test_TC_010_01_03_verify_learn_more_link_redirects_to_valid_page(driver):
     page = OurInitiativesPage(driver)
     page.verify_learn_more_link_redirects_to_valid_page()
+
+def test_TC_010_01_02_verify_learn_more_button_is_clickable(driver, wait):
+    page = OurInitiativesPage(driver)
+    page.verify_learn_more_button_is_clickable(wait)
