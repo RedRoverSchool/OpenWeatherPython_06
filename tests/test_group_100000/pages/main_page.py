efb3c3fa900d34ed42584da5d1f995028bd7ebb5
Ekaterina <luchik.eg@gmail.com>
2023-05-24 10:48:56 -0500

    Refactoring AT_003.05.03 Verify 'How to start' link is сlickable

--- tests/test_group_100000/pages/main_page.py
+++ tests/test_group_100000/pages/main_page.py
@@ -1,11 +1,8 @@
 from pages.base_page import BasePage
-from tests.test_group_100000.locators.main_page_locators import EightDayForecast as D8
+from tests.test_group_100000.locators.main_page_locators  import FooterBlockLocators as F
 
 
-class EightDayForecastPage(BasePage):
-
-    def verify_state_of_sky_in_words_for_each_day_is_displayed(self):
-        elements = self.driver.find_elements(*D8.SEARCH_SKY_IN_WORDS)
-        for i in elements:
-            assert i.is_displayed()
-
+class FooterBlock(BasePage):
+    def verify_how_to_start_link_is_clickable(self):
+        how_to_start = self.driver.find_element(*F.HOW_TO_START_LINK)
+        assert how_to_start.is_enabled(), "The 'How to start' link does not clickable"
--- tests/test_group_100000/tests/test_main_page.py
+++ tests/test_group_100000/tests/test_main_page.py
@@ -1,8 +1,11 @@
-from tests.test_group_100000.pages.main_page import *
-from tests.test_group_100000.locators.main_page_locators import EightDayForecast as D8
+from tests.test_group_100000.pages.main_page import FooterBlock
 
 
-def test_RF_TC_001_04_02_verify_state_of_sky_in_words_for_each_day_is_displayed(driver, open_and_load_main_page, wait):
-    page = EightDayForecastPage(driver, link=D8.URL)
-    page.open_page()
-    page.verify_state_of_sky_in_words_for_each_day_is_displayed()
+class TestHowToStartLink:
+    def test_TC_003_05_03_verify_how_to_start_link_is_clickable(self, driver):
+        page = FooterBlock(driver, link='https://openweathermap.org/')
+        page.open_page()
+        page.verify_how_to_start_link_is_clickable()
+
+
+
