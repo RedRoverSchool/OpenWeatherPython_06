2668409c8e8c13f777d9a9bb9c871018442df56e
Julia Venskutoniene <julia.venskutone@gmail.com>
2023-05-24 13:02:40 +0100

    TC_002.03.20 | Header > Links > Navigation > Verify "Sign In" link leads to a correct page

--- tests/test_group_EPIC_team.py
+++ tests/test_group_EPIC_team.py
@@ -5,7 +5,15 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+# main page selectors:
+signin_button = (By.XPATH, "//li[@class='user-li']/*[text()='Sign in']")
+Signin_URL = 'https://home.openweathermap.org/users/sign_in'
 
 def test_TC_003_12_10_open_faq_page(driver, wait):
     driver.get('https://openweathermap.org/faq')
     assert driver.current_url == 'https://openweathermap.org/faq'
+
+def test_TC_002_03_20_Sign_in_link_leads_to_correct_page(driver, open_and_load_main_page, wait):
+    wait.until(EC.element_to_be_clickable(signin_button))
+    driver.find_element(*signin_button).click()
+    assert driver.current_url == Signin_URL
