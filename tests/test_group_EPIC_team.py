e4acc7ba29745d7f621c14e54f69acc144224ee0
Julia Venskutoniene <julia.venskutone@gmail.com>
2023-05-24 13:32:50 +0100

    very last changes to the code. Aigul, thanks a lot for your patience and help

--- tests/test_group_EPIC_team.py
+++ tests/test_group_EPIC_team.py
@@ -6,5 +6,4 @@ signin_button = (By.XPATH, "//li[@class='user-li']/*[text()='Sign in']")
 def test_TC_002_03_19_signin_button_visible_clickable(driver, wait):
     driver.get('https://openweathermap.org/')
     sign_in_button = driver.find_element(*signin_button)
-    assert sign_in_button.is_displayed()
-    assert sign_in_button.is_enabled()
+    assert sign_in_button.is_displayed() and sign_in_button.is_enabled()
