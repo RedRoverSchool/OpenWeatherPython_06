3e0eb872663b8a1e9fb76f509f3bdaf57e741c91
Tatiana Yurchenko <taf88156@mail.ru>
2023-05-24 09:37:52 +0300

    RF_AT_010.01.02 | Our Initiatives > Visibility, сlickability, correct redirection>Verify "Learn more" button is clickable

--- tests/test_group_trust_me_i_am_engineer/pages/our_initiatives_page.py
+++ tests/test_group_trust_me_i_am_engineer/pages/our_initiatives_page.py
@@ -1,5 +1,6 @@
 from pages.base_page import BasePage
 from tests.test_group_trust_me_i_am_engineer.locators.page_locators import OurInitiativesPageLocators
+from selenium.webdriver.support import expected_conditions as EC
 
 class OurInitiativesPage(BasePage):
     URL_OUR_INITIATIVES = 'https://openweathermap.org/our-initiatives'
@@ -10,3 +11,9 @@ class OurInitiativesPage(BasePage):
         self.driver.find_element(*self.locators.LEARN_MORE_LINK).click()
         pricing_text = self.driver.find_element(*self.locators.LEARN_MORE_PAGE_TITLE).text
         assert pricing_text == "Student initiative"
+
+    def verify_learn_more_button_is_clickable(self, wait):
+        self.driver.get(self.URL_OUR_INITIATIVES)
+        self.driver.execute_script("window.scrollTo(0, 500)")
+        element = wait.until(EC.element_to_be_clickable(self.locators.LEARN_MORE_LINK))
+        assert element.is_displayed() and element.is_enabled()
