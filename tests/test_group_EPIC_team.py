e71c7230c27374af94a47a8cfca463bd5962e465
Julia Venskutoniene <julia.venskutone@gmail.com>
2023-05-24 11:14:16 +0100

    changes (round 2) are made as per Aigul's instructions

--- tests/test_group_EPIC_team.py
+++ tests/test_group_EPIC_team.py
@@ -1,12 +1,10 @@
-import time
+
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 # main page selectors:
 signin_button = (By.XPATH, "//li[@class='user-li']/*[text()='Sign in']")
-def test_signin_button_visible_clickable(driver, wait):
+def test_TC_002_03_19_signin_button_visible_clickable(driver, wait):
     driver.get('https://openweathermap.org/')
-    signin_button = driver.find_element(By.XPATH, "//li[@class='user-li']/*[text()='Sign in']")
-    assert signin_button.is_displayed()
-    assert signin_button.is_enabled()
+    sign_in_button = driver.find_element(*signin_button)
+    assert sign_in_button.is_displayed()
+    assert sign_in_button.is_enabled()
