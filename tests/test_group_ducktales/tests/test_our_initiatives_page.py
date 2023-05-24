3ea81c683980d660fa4bc4d11185d923cf33fec1
Badx86 <stasure@gmail.com>
2023-05-24 23:20:15 +0400

    RF_AT_010.02.08 | Our Initiatives > Student initiative > Verify the visibility of FAQ question titles

--- tests/test_group_ducktales/tests/test_our_initiatives_page.py
+++ tests/test_group_ducktales/tests/test_our_initiatives_page.py
@@ -1,21 +1,27 @@
 import pytest
 from tests.test_group_ducktales.pages.our_initiatives_page import OurInitiativesPage
 from tests.test_group_ducktales.test_data.our_initiatives_page_data import *
-from tests.test_group_ducktales.locators.our_initiatives_page_locators import OurInitiativesPageLocators
 
 
 class TestOurInitiativesPage:
+
     @pytest.mark.parametrize("section", sections)
     def test_010_01_01_01_verify_sections(self, driver, open_and_load_main_page, section):
         page = OurInitiativesPage(driver, OUR_INITIATIVES_PAGE)
         page.open_page()
         page.click_initiatives_link()
-        section_element = page.get_section_element(section)
-        assert section_element.is_displayed(), f"Section '{section}' not found on the page"
+        page.get_section_element(section)
 
     def test_010_01_02_02_functionality(self, driver, open_and_load_main_page):
         page = OurInitiativesPage(driver, EDUCATION_SECTION_PAGE)
         page.open_page()
         page.click_initiatives_link()
         page.click_education_learn_more()
-        assert driver.current_url == EDUCATION_SECTION_PAGE
+        page.check_education_page_opened()
+
+    def test_010_02_08_accessibility_of_question_headings(self, driver, open_and_load_main_page):
+        page = OurInitiativesPage(driver, EDUCATION_SECTION_PAGE)
+        page.open_page()
+        page.click_initiatives_link()
+        page.click_education_learn_more()
+        page.get_question_headings()
