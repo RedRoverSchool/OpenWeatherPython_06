6c2f59a2d4f3f698e627b70ddc205d4bcd70ee09
hamilova <hamilovaevgeniya@gmail.com>
2023-05-25 10:47:16 +0300

    try#100

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -3,6 +3,7 @@ from pages.main_page import MainPage
 from test_data.main_page_data import data
 
 
+
 @pytest.mark.parametrize('city', data["cityName"])
 def test_tc_000_00_01_verify_requested_city_displayed_for_valid_input(driver, open_and_load_main_page, wait, city):
     page = MainPage(driver)
