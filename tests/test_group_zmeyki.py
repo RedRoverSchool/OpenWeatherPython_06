4dff46572f24d45e7e83dccc63af1e47f0a9216a
Niko-03 <ksveta10@mail.ru>
2023-05-25 21:52:00 +0800

    second fix: test_tc_006_01_01

--- tests/test_group_zmeyki.py
+++ tests/test_group_zmeyki.py
@@ -16,6 +16,7 @@ graphic_hourly_forecast_locator = (By.CSS_SELECTOR, "canvas[id=chart-component]"
 weather_items_locator = (By.CSS_SELECTOR, "ul.weather-items")
 weather_item_locator = (By.CSS_SELECTOR, 'ul.weather-items li:nth-child(1)')
 website_link_locator = (By.CSS_SELECTOR, 'section#terms.anchor_el a[href="/"]')
+ask_us_popup_locator = (By.CSS_SELECTOR, 'section#terms.anchor_el a[href="mailto:info@openweathermap.org"]')
 
 
 def test_tc_001_08_01_graphic_hourly_forecast_is_displayed(driver, open_and_load_main_page, wait):
@@ -31,7 +32,7 @@ def test_tc_001_08_02_weather_items_are_displayed(driver, open_and_load_main_pag
         assert weather_item.is_displayed()
 
 
-def test_TC_010_02_06_verify_website_link_redirects_to_main_page(driver, open_and_load_main_page, wait):
+def test_tc_010_02_06_verify_website_link_redirects_to_main_page(driver, open_and_load_main_page, wait):
     driver.get(STUDENT_INITIATIVE_URL)
     website_link = driver.find_element(*website_link_locator)
     driver.execute_script("arguments[0].click();", website_link)
@@ -50,7 +51,6 @@ def test_tc_006_01_01_verify_description_of_open_weather_dashboard(driver, open_
     driver.get(URL)
     driver.find_element(*button_dashboard_locator).click()
     driver.implicitly_wait(10)
-
 
     expected_dashboard_h1 = 'OpenWeather\nDashboard'
     actual_dashboard_h1 = driver.find_element(*dashboard_h1_locator)
@@ -77,3 +77,8 @@ def test_tc_006_01_01_dashboard_img_displayed(driver, open_and_load_main_page, w
     expected_dashboard_img = driver.find_element(*dashboard_img_locator)
     assert expected_dashboard_img.is_displayed()
 
+
+def test_tc_010_02_07_verify_correct_redirection_to_popup_window(driver, open_and_load_main_page, wait):
+    driver.get(STUDENT_INITIATIVE_URL)
+    ask_us_popup = driver.find_element(*ask_us_popup_locator)
+    assert ask_us_popup.is_enabled() and ask_us_popup.is_displayed()
