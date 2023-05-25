6b4d00098a9805c170f6cf310129df0766c488ee
hamilova <hamilovaevgeniya@gmail.com>
2023-05-25 11:40:50 +0300

    try#102

--- tests/test_group_ducktales.py
+++ tests/test_group_ducktales.py
@@ -1,4 +1,4 @@
-from selenium.common import ElementClickInterceptedException
+from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
