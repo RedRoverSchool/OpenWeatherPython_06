a32fd2b5d619f7be436aba01b69c31e7b26763cc
Sergei Postalovski <75713454+Postalovski@users.noreply.github.com>
2023-05-24 21:37:49 -0400

    Refactored test RF_AT_001_04_02 (#657)

--- tests/test_group_100000/locators/main_page_locators.py
+++ tests/test_group_100000/locators/main_page_locators.py
@@ -1,5 +1,9 @@
 from selenium.webdriver.common.by import By
 
+class EightDayForecast:
+    URL = 'https://openweathermap.org'
+    SEARCH_SKY_IN_WORDS = (By.XPATH, "//div[@class='day-list-values']/span[contains(@class,'sub')]")
+
 
 class FooterBlockLocators:
     COOKIES = (By.XPATH, "//button[contains(text(), 'Allow all')]")
