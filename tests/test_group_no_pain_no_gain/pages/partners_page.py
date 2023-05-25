7888d585bb8736075ac145639283fbacf444704e
Yuliya Fatkina <juliadigrigorio@hotmail.com>
2023-05-25 20:39:22 +0300

    RF_AT_011_01_01_Partners and solutions page title is correct (#671)
    * RF_AT_011_01_01_Verify that Partners and solutions page title is correct
    
    * RF_AT_011_01_01_Verify that Partners and solutions page title is correct

--- tests/test_group_no_pain_no_gain/pages/partners_page.py
+++ tests/test_group_no_pain_no_gain/pages/partners_page.py
@@ -3,7 +3,5 @@ from tests.test_group_no_pain_no_gain.locators.partners_page_locators import Par
 from tests.test_group_no_pain_no_gain import links
 
 class Partners(MainPage):
-    def check_partners_page_title(self):
-        self.driver.get(links.PARTNERS_AND_SOLUTIONS)
-        title = self.driver.title
-        assert 'Partners and solutions' in title
+    pass
+
