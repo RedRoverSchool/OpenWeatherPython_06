60846cafbb9c015862fede178d5912c7ef1eef73
Ksenia_Mish <nksenia@yandex.ru>
2023-05-25 12:21:37 +0300

    RF | AT_002.01.02 | Header > Logo > Verify returning from API page to main page by clicking on Logo

--- tests/test_group_roma/pages/api_page.py
+++ tests/test_group_roma/pages/api_page.py
@@ -9,14 +9,4 @@ class ApiPage(BasePage):
     def check_logo(self):
         self.driver.find_element(*self.locator.LOGO).click()
         assert self.driver.current_url == links.MAIN_PAGE
-    #
-    # def open_api_page(self):
-    #     api_page = ApiPage(self.driver)
-    #     api_page.open_page()
-    #
-    # def click_logo(self):
-    #     search_logo = self.driver.find_element(*self.locator.LOGO).click()
-    #     search_logo.open_page()
-    #
-    # def verify_main_page_url(self):
-    #     assert self.driver.current_url == links.MAIN_PAGE
+
