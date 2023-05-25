73eac32b362d31cbbf69ea64f2b2b34a25f1abe1
Stas Cherepanov <94304561+zaelcovsky@users.noreply.github.com>
2023-05-25 07:58:09 +0700

    AT_012.03.06 | For Business > Industries > Verify redirection of "Learn more" button in 'Agriculture' section (#656)

--- tests/test_group_future_auto_qa/pages/industries_page.py
+++ tests/test_group_future_auto_qa/pages/industries_page.py
@@ -22,5 +22,10 @@ class IndustriesPage(BasePage):
         assert button.is_displayed() and button.is_enabled(), \
             f"Button with XPath {str(button_locator)[10:-1]} is not visible or is not clickable"
 
-
+    def check_redirection_of_learn_more_button_in_agriculture_section(self, expected_link):
+        self.open_industries_page()
+        self.element_is_clickable(IndustriesPageLocators.LEARN_MORE_BUTTON).click()
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        button_url = self.driver.current_url
+        assert button_url == expected_link, "'Learn more' button link is not correct"
 
