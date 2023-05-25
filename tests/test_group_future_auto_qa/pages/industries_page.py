dbeecbd817fdc6f66a418e41671edde384ae5db5
Stas Cherepanov <94304561+zaelcovsky@users.noreply.github.com>
2023-05-25 07:43:38 +0700

    AT_012.03.05 | For Business > Industries > Verify the visibility and … (#655)
    * AT_012.03.05 | For Business > Industries > Verify the visibility and clickability of "Learn more" and "Talk to us" buttons in each section, excepting 'Agriculture’
    
    * Ref: AT_012.03.05 | some improvements

--- tests/test_group_future_auto_qa/pages/industries_page.py
+++ tests/test_group_future_auto_qa/pages/industries_page.py
@@ -1,11 +1,26 @@
 from pages.base_page import BasePage
+from selenium.webdriver.support import expected_conditions as EC
+from tests.test_group_future_auto_qa.locators.industries_page_locators import IndustriesPageLocators
 
 
 class IndustriesPage(BasePage):
 
-    def check_email_link_in_talk_to_us_button(self, wait, button_locator, expected_link):
+    # function-helper for opening Industries page
+    def open_industries_page(self):
         self.driver.get("https://openweather.co.uk/industries")
         self.allow_all_cookies()
+
+    def check_email_link_in_talk_to_us_button(self, button_locator, expected_link):
+        self.open_industries_page()
         simple_link = self.element_is_clickable(button_locator)
         link_href = simple_link.get_attribute('href')
-        assert link_href == expected_link
+        assert link_href == expected_link, "Email link is not correct"
+
+    def check_visibility_and_clickability_of_learn_more_and_talk_to_us_buttons(self, button_locator):
+        self.open_industries_page()
+        button = self.element_is_clickable(button_locator)
+        assert button.is_displayed() and button.is_enabled(), \
+            f"Button with XPath {str(button_locator)[10:-1]} is not visible or is not clickable"
+
+
+
--- tests/test_group_future_auto_qa/tests/industries_page_test.py
+++ tests/test_group_future_auto_qa/tests/industries_page_test.py
@@ -5,10 +5,22 @@ import pytest
 
 class TestIndustriesPage:
 
-    buttons_locators = IndustriesPageLocators.buttons_locators
+    talk_to_us_buttons_locators = IndustriesPageLocators.talk_to_us_buttons_locators
 
-    @pytest.mark.parametrize("button_locator", buttons_locators)
-    def test_tc_012_03_03_checking_email_link_in_talk_to_us_buttons(self, driver, wait, button_locator):
+    @pytest.mark.parametrize("button_locator", talk_to_us_buttons_locators)
+    def test_tc_012_03_03_checking_email_link_in_talk_to_us_buttons(self, driver, button_locator):
         page = IndustriesPage(driver)
         expected_link = "mailto:info@openweathermap.org"
-        page.check_email_link_in_talk_to_us_button(wait, button_locator, expected_link)
+        page.check_email_link_in_talk_to_us_button(button_locator, expected_link)
+
+    request_a_trial_buttons_locators = IndustriesPageLocators.request_a_trial_buttons_locators
+    all_buttons_locators = talk_to_us_buttons_locators + request_a_trial_buttons_locators
+
+    @pytest.mark.parametrize("button_locator", all_buttons_locators)
+    def test_tc_012_03_05_check_visibility_and_clickability_of_learn_more_and_talk_to_us_buttons(self, driver, wait, button_locator):
+        page = IndustriesPage(driver)
+        page.check_visibility_and_clickability_of_learn_more_and_talk_to_us_buttons(button_locator)
+
+
+
+
