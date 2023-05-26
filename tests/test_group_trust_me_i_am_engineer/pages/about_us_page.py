3570c91310b334014410a9ce20b2eb9c4ef62604
evgeniia demchenko <demchenkoeb04022012@gmail.com>
2023-05-26 15:56:56 +0300

    AT_tc_001_15_02_verify_image_beside_header_is_displayed

--- tests/test_group_trust_me_i_am_engineer/pages/about_us_page.py
+++ tests/test_group_trust_me_i_am_engineer/pages/about_us_page.py
@@ -17,3 +17,14 @@ class AboutUsPage(MainPage):
         self.element_is_visible(self.page_locators.HEADER)
 
         assert self.element_is_visible(self.page_locators.HEADER).text == "OpenWeather\nglobal services"
+
+    def verify_image_beside_header_is_displayed(self, wait):
+        self.driver.get(self.URL)
+        wait.until_not(EC.presence_of_element_located(load_div))
+
+        self.allow_all_cookies()
+        self.element_is_clickable(self.locators.ABOUT_US_BUTTON).click()
+
+        image_beside_header = self.element_is_visible(self.page_locators.IMAGE_BESIDE_HEADER)
+        assert image_beside_header.is_displayed(), "The image is not displayed beside the page header"
+
