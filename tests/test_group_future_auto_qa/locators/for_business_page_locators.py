7a9650b2b62e1ec3d49f24ffccb0cd4307299dc3
Badx86 <stasure@gmail.com>
2023-05-27 01:27:05 +0400

    AT_012.01.06 | For Business > Products > Verify the buttons are presented on the page

--- tests/test_group_future_auto_qa/locators/for_business_page_locators.py
+++ tests/test_group_future_auto_qa/locators/for_business_page_locators.py
@@ -2,7 +2,10 @@ from selenium.webdriver.common.by import By
 
 
 class ForBusinessPageLocators:
-    FOR_BUSINESS = (By.CSS_SELECTOR, "div[id='desktop-menu'] a[class='marketplace']")
-    PRODUCTS_IN_HEADER = (By.CSS_SELECTOR, "a[href='/products']")
-    PRODUCTS_HEADINGS = (By.CSS_SELECTOR, "a big")
     FOR_BUSINESS_PAGE_URL = "https://openweather.co.uk/"
+    FOR_BUSINESS = By.CSS_SELECTOR, "div[id='desktop-menu'] a[class='marketplace']"
+    PRODUCTS_IN_HEADER = By.CSS_SELECTOR, "a[href='/products']"
+    PRODUCTS_HEADINGS = By.CSS_SELECTOR, "a big"
+    BLACK_BUTTONS = By.CSS_SELECTOR, "a[class='ow-btn round btn-black']"
+    ORANGE_BUTTONS = By.CSS_SELECTOR, "a[class='btn_block orange round']"
+
