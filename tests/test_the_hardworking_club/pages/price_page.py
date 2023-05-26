31f1c2b43815d9a26b9ce58d4b182d07c1dda0b5
Ekaterina Zotova <ekatzotova@gmail.com>
2023-05-26 17:28:08 +0300

    fix: RF test_TC_008_01_01_subscribe_button_redirects

--- tests/test_the_hardworking_club/pages/price_page.py
+++ tests/test_the_hardworking_club/pages/price_page.py
@@ -1,5 +1,4 @@
 from pages.base_page import BasePage
-from selenium.webdriver.common.by import By
 from tests.test_the_hardworking_club.locators.locators_all import PricePageLocators
 
 class PricePage(BasePage):
