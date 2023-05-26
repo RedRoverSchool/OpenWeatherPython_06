2b0e8d63da739132db82c70138256a999d1f72fc
Stanislav_Osipov <badlolpro@gmail.com>
2023-05-27 02:18:17 +0400

    AT_012.01.06 | For Business > Products > Verify the buttons are presented on the page (#693)

--- tests/test_group_future_auto_qa/pages/for_business_page.py
+++ tests/test_group_future_auto_qa/pages/for_business_page.py
@@ -8,10 +8,27 @@ class ForBusinessPage(BasePage):
     locators = ForBusinessPageLocators()
 
     def check_headings(self):
-        # for_business = self.element_is_clickable(self.locators.FOR_BUSINESS, 15)
-        # self.driver.execute_script("arguments[0].click();", for_business)
-        # self.driver.switch_to.window(self.driver.window_handles[1])
         products = self.element_is_visible(self.locators.PRODUCTS_IN_HEADER, 15)
         products.click()
         headings = self.elements_are_present(self.locators.PRODUCTS_HEADINGS, 15)
         return headings
+
+    def assert_headings_present(self):
+        headings = self.check_headings()
+        assert len(headings) == 7, "Not all headings are present on the page"
+
+    def check_buttons(self):
+        products = self.element_is_visible(self.locators.PRODUCTS_IN_HEADER, 15)
+        products.click()
+        black_buttons = self.elements_are_present(self.locators.BLACK_BUTTONS, 15)
+        orange_buttons = self.elements_are_present(self.locators.ORANGE_BUTTONS, 15)
+        if orange_buttons:
+            orange_buttons.pop(0)
+        buttons = black_buttons + orange_buttons
+        return buttons
+
+    def assert_buttons_present(self):
+        buttons = self.check_buttons()
+        assert len(buttons) == 17, "The count of buttons is not as expected"
+
+
--- tests/test_group_future_auto_qa/tests/for_business_page_test.py
+++ tests/test_group_future_auto_qa/tests/for_business_page_test.py
@@ -6,6 +6,11 @@ class TestBusinessPage:
     def test_tc_012_01_04_verify_the_headings_are_present_on_the_page(self, driver):
         business_page = ForBusinessPage(driver, ForBusinessPageLocators.FOR_BUSINESS_PAGE_URL)
         business_page.open_page()
-        headers_count = business_page.check_headings()
-        assert len(headers_count) == 7, "Not all headings are present on the page"
+        business_page.check_headings()
+
+    def test_tc_012_01_06_verify_the_buttons_are_present_on_the_page(self, driver):
+        business_page = ForBusinessPage(driver, ForBusinessPageLocators.FOR_BUSINESS_PAGE_URL)
+        business_page.open_page()
+        business_page.check_buttons()
+
 
