e6bf7d0be922199e5273a54ca23d9df0da87f27d
Alex Mack <mack@Macks-MacBook-Pro.local>
2023-05-26 12:35:20 -0700

    RF_AT_001.15.03 wait fixture added

--- tests/test_group_snake_oil/tests/test_footer.py
+++ tests/test_group_snake_oil/tests/test_footer.py
@@ -19,8 +19,8 @@ class TestFooter:
         footer.open_page()
         footer.find_links_under_technologies_module(icon)
 
-    def test_tc_001_15_03_verify_redirection_to_about_us_page(self, driver):
+    def test_tc_001_15_03_verify_redirection_to_about_us_page(self, driver, wait):
         footer = Footer(driver, HOME_PAGE_URL)
         footer.open_page()
-        footer.check_redirection_to_about_us_page()
+        footer.check_redirection_to_about_us_page(wait)
 
