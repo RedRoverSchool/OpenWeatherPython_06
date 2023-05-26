b45eeeb3f86f4332d1396adb6564b56ff197c3fd
evgeniia demchenko <demchenkoeb04022012@gmail.com>
2023-05-26 07:29:13 +0300

    AT_tc_001_15_01_verify_correct_header_about_us_page

--- tests/test_group_trust_me_i_am_engineer/pages/about_us_page.py
+++ tests/test_group_trust_me_i_am_engineer/pages/about_us_page.py
@@ -1,13 +1,15 @@
 from tests.test_group_trust_me_i_am_engineer.locators.page_locators import MainPageLocators, AboutUsPageLocators
 from tests.test_group_trust_me_i_am_engineer.pages.main_page import MainPage
-
+from conftest import load_div
+from selenium.webdriver.support import expected_conditions as EC
 
 class AboutUsPage(MainPage):
     locators = MainPageLocators
     page_locators = AboutUsPageLocators
 
-    def verify_correct_header_about_us_page(self):
+    def verify_correct_header_about_us_page(self, wait):
         self.driver.get(self.URL)
+        wait.until_not(EC.presence_of_element_located(load_div))
 
         self.element_is_clickable(self.locators.ALLOW_ALL_COOKIES_BUTTON).click()
         self.element_is_clickable(self.locators.ABOUT_US_BUTTON).click()
--- tests/test_group_trust_me_i_am_engineer/tests/about_us_page_test.py
+++ tests/test_group_trust_me_i_am_engineer/tests/about_us_page_test.py
@@ -1,7 +1,6 @@
-from conftest import open_and_load_main_page
 from tests.test_group_trust_me_i_am_engineer.pages.about_us_page import AboutUsPage
 
 
 def test_tc_001_15_01_verify_correct_header_about_us_page(driver, wait):
-    page = AboutUsPage(driver, open_and_load_main_page)
-    page.verify_correct_header_about_us_page()
+    page = AboutUsPage(driver)
+    page.verify_correct_header_about_us_page(wait)
