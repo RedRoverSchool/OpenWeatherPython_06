a58cff0e84aacf09dd17d205155082899fa3b5ba
LiliaBez <112456861+LiliaBez@users.noreply.github.com>
2023-05-26 11:06:52 +0400

    AT_014.04.02 | Sign in > Functionality for registered User > Verify au… (#674)
    * AT_014.04.02 | Sign in > unctionality for registered User > Verify authorization with empty fields
    
    * AT_014.04.02 | Sign in > Functionality for registered User > Verify authorization with empty fields
    
    * ref: AT_014.04.02 | added a letter to the commit name
    
    ---------
    
    Co-authored-by: Liliya <lilianew78@yahoo.com>

--- tests/test_group_bugs_in_each_step/tests/test_sign_in_page.py
+++ tests/test_group_bugs_in_each_step/tests/test_sign_in_page.py
@@ -11,6 +11,13 @@ class TestRegistrationQuestion:
         element = sign_in_question.check_registration_question_is_visible()
         assert element is True, 'The element is not visible'
 
+    def test_tc_014_04_02_verify_authorization_with_empty_fields(self, driver):
+        sign_in_page = SignInPage(driver, SignInUrls.url_sign_in_page)
+        sign_in_page.open_page()
+        sign_in_page.check_authorization()
+        sign_in_page.check_error_alert_text()
+
+
 
 
 
