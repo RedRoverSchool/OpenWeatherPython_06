60be5086a6d26ad08f1032419f3090d8bb296ee5
Badx86 <stasure@gmail.com>
2023-05-27 03:50:18 +0400

    add def assert_equal

--- tests/test_group_future_auto_qa/tests/for_business_page_test.py
+++ tests/test_group_future_auto_qa/tests/for_business_page_test.py
@@ -6,15 +6,18 @@ class TestBusinessPage:
     def test_tc_012_01_04_verify_the_headings_are_present_on_the_page(self, driver):
         business_page = ForBusinessPage(driver, ForBusinessPageLocators.FOR_BUSINESS_PAGE_URL)
         business_page.open_page()
-        business_page.check_headings()
+        actual_headings = business_page.check_headings()
+        business_page.assert_equal(len(actual_headings), 7)
 
     def test_tc_012_01_06_verify_the_buttons_are_present_on_the_page(self, driver):
         business_page = ForBusinessPage(driver, ForBusinessPageLocators.FOR_BUSINESS_PAGE_URL)
         business_page.open_page()
-        business_page.check_buttons()
+        actual_buttons = business_page.check_buttons()
+        business_page.assert_equal(len(actual_buttons), 17)
 
     def test_tc_012_01_07_verify_the_elements_are_presented_on_the_page(self, driver):
         business_page = ForBusinessPage(driver, ForBusinessPageLocators.FOR_BUSINESS_PAGE_URL)
         business_page.open_page()
-        business_page.check_elements()
+        elements = business_page.check_elements()
+        business_page.assert_equal(all(elements), True)
 
