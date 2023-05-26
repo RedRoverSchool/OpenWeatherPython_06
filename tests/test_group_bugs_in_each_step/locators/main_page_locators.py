4fe1ec39f5b7f1d64dac97341bfdde8e7937f96a
Elena <peregovorun@gmail.com>
2023-05-26 11:45:50 +0300

    AT_003.12.19 | Footer > Link navigation > Verify Subscribe for free link functionality for unauthorized user

--- tests/test_group_bugs_in_each_step/locators/main_page_locators.py
+++ tests/test_group_bugs_in_each_step/locators/main_page_locators.py
@@ -3,7 +3,8 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
 
-    ACCURACY_AND_QUALITY_OF_WEATHER_DATA_LINK = (By.XPATH, "//a[contains(text(), 'Accuracy and quality of weather data')]")
+    ACCURACY_AND_QUALITY_OF_WEATHER_DATA_LINK = \
+        (By.XPATH, "//a[contains(text(), 'Accuracy and quality of weather data')]")
     CONNECT_YOUR_WEATHER_STATION_LINK = (By.CSS_SELECTOR, "li a[href*='station']")
     COOKIES = (By.XPATH, "//button[contains(text(), 'Allow all')]")
     CURRENT_AND_FORECAST_APIS = (By.XPATH, "//a[text()='Current and Forecast APIs']")
@@ -11,6 +12,8 @@ class MainPageLocators:
     HOW_TO_START = (By.XPATH, "//div[@id='footer-website']//a[text()='How to start']")
     HOW_TO_START_LINK = (By.CSS_SELECTOR, "div[class='section-content'] a[href*='appid']")
     OUR_TECHNOLOGY_LINK = (By.XPATH, "//a[contains(text(), 'Our technology')]")
+    SUBSCRIBE_FOR_FREE_LINK = \
+        (By.CSS_SELECTOR, ":nth-child(1) > :nth-child(2) > .section-content > ul > :nth-child(3) > a")
     WEATHER_MAPS_LINK = (By.XPATH, "//a[contains(text(), 'Weather Maps')]")
     WEATHER_DASHBOARD_LINK = (By.XPATH, "//a[contains(text(), 'Weather Dashboard')]")
 
--- tests/test_group_bugs_in_each_step/pages/main_page.py
+++ tests/test_group_bugs_in_each_step/pages/main_page.py
@@ -90,4 +90,9 @@ class MainPage(BasePage):
         assert '/appid' in self.driver.current_url, \
             "The How to start link leads to an incorrect page"
 
-
+    def check_subscribe_for_free_link_functionality(self):
+        subscribe_for_free_link = self.driver.find_element(*self.locators.SUBSCRIBE_FOR_FREE_LINK)
+        self.go_to_element(subscribe_for_free_link)
+        subscribe_for_free_link.click()
+        assert '/users/sign_up' in self.driver.current_url, \
+            "The Subscribe for free link leads to an incorrect page"
