4e452900c9cdb59aa0bf964cbd8761d7f173f572
Zoe Novak <e.zoe.novak@gmail.com>
2023-05-26 18:49:57 +0400

    RF | AT_004.08.02 | Guide > Professional collections/Historical weather collection > Link to History archive is clickable

---
+++ tests/test_group_python_qa/links/links.py
@@ -0,0 +1,9 @@
+
+
+URL_SOLAR_API="https://openweathermap.org/api/solar-energy-prediction"
+QUALITY_INFO_PAGE="https://openweathermap.org/accuracy-and-quality"
+URL_SUBSCRIPTION_BASE='https://home.openweathermap.org/subscriptions/unauth_subscribe/onecall_30/base'
+OUR_INITIATIVES_PAGE='https://openweathermap.org/our-initiatives'
+URL_HISTORY_BULK="https://openweathermap.org/history-bulk"
+URL_WEATHER_CONDITIONS="https://openweathermap.org/weather-conditions"
+URL_GUIDE_PAGE="https://openweathermap.org/guide"
