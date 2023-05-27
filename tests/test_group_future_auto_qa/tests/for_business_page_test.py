d51f02cbfdf6ec271946593ba8de0e67c22b53a5
Stanislav_Osipov <badlolpro@gmail.com>
2023-05-27 07:17:37 +0400

    TC_012.01.07 | For Business > Products > Verify the elements are presented on the page (#694)

--- tests/test_group_future_auto_qa/tests/for_business_page_test.py
+++ tests/test_group_future_auto_qa/tests/for_business_page_test.py
@@ -3,6 +3,7 @@ from tests.test_group_future_auto_qa.pages.for_business_page import ForBusinessP
 
 
 class TestBusinessPage:
+
     def test_tc_012_01_04_verify_the_headings_are_present_on_the_page(self, driver):
         business_page = ForBusinessPage(driver, ForBusinessPageLocators.FOR_BUSINESS_PAGE_URL)
         business_page.open_page()
@@ -12,5 +13,10 @@ class TestBusinessPage:
         business_page = ForBusinessPage(driver, ForBusinessPageLocators.FOR_BUSINESS_PAGE_URL)
         business_page.open_page()
         business_page.check_buttons()
+
+    def test_tc_012_01_07_verify_the_elements_are_presented_on_the_page(self, driver):
+        business_page = ForBusinessPage(driver, ForBusinessPageLocators.FOR_BUSINESS_PAGE_URL)
+        business_page.open_page()
+        business_page.check_elements()
 
 
