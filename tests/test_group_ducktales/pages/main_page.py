824018a97121d08694ac729a57df02e53e853301
Lipatnikova <aerovaya@gmail.com>
2023-05-28 22:22:17 +0400

    ref: change locator

--- tests/test_group_ducktales/pages/main_page.py
+++ tests/test_group_ducktales/pages/main_page.py
@@ -96,8 +96,8 @@ class MainPage(BasePage):
             assert 'California' in i.text, 'Not all search suggestions in the drop-down list contain "California"'
 
     def click_first_element_dropdown_menu(self):
-        self.element_is_present(MainLocator.SEARCH_DROPDOWN_MENU_FIRST_CHILD, 10)
-        self.driver.find_element(*MainLocator.SEARCH_DROPDOWN_MENU_FIRST_CHILD).click()
+        self.element_is_clickable(MainLocator.DROPDOWN_LIST, 10)
+        self.driver.find_element(*MainLocator.DROPDOWN_LIST).click()
         self.element_is_present(MainLocator.SEARCH_DROPDOWN_MENU_FIRST_CHILD_TEXT, 10)
 
     def get_city_text(self, wait):
