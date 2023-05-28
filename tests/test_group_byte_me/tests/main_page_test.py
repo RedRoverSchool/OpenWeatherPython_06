9e28b9cc65ecf274a27980cab06a5b08d63188f1
Alena24-80 <aleshka24_80@ukr.net>
2023-05-28 16:26:25 -0400

    TC_001_01_01_02_searching_requested_city_name_displayed

--- tests/test_group_byte_me/tests/main_page_test.py
+++ tests/test_group_byte_me/tests/main_page_test.py
@@ -11,6 +11,6 @@ class TestMainPageHeader:
 
 
 
-def test_tc_001_01_01_02_searching_requested_city_name_displayed_in_widget(driver, wait, open_and_load_main_page):
-    main_page = MainPage(driver)
-    main_page.check_search_city_result(wait, 'Kyiv, UA')
+    def test_tc_001_01_01_02_searching_requested_city_name_displayed_in_widget(driver, wait, open_and_load_main_page):
+        main_page = MainPage(driver)
+        main_page.check_search_city_result(wait, 'Kyiv, UA')
