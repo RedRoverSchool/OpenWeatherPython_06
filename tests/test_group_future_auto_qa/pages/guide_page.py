25eb92d00174d300bdc8a92a9619805f66cbda76
zubovam <118196036+zubovam@users.noreply.github.com>
2023-05-30 00:37:03 +0300

    AT_004.09.02 | Guide > Professional collections/Weather Maps collection > Link to Global Precipitation Maps is clickable (#750)

--- tests/test_group_future_auto_qa/pages/guide_page.py
+++ tests/test_group_future_auto_qa/pages/guide_page.py
@@ -12,3 +12,8 @@ class GuidePage(BasePage):
         self.open_guide_page()
         weather_maps_collection = self.driver.find_element(*GuidePageLocators.WEATHER_MAPS_COLLECTION_BLOCK)
         assert weather_maps_collection.is_displayed(), "The Weather Maps collection is not displaying"
+
+    def check_global_precipitation_maps_link_clickability(self):
+        self.open_guide_page()
+        global_precipitation_maps_link = self.element_is_clickable(self.locators.WEATHER_MAPS_COLLECTION_BLOCK)
+        assert global_precipitation_maps_link.is_enabled(), 'Link to Global Precipitation Maps is not clickable'
