7ec2989218ccabc454e57f4ad6622884f1577d95
Kristina Ozhog <kristinka_kuku@mail.ru>
2023-05-29 18:27:22 +0100

    Added in existing files

--- tests/test_group_lutz_squad/pages/accuracy_and_quality_page.py
+++
@@ -1,8 +0,0 @@
-from pages.base_page import BasePage
-from tests.test_group_lutz_squad.locators.accuracy_and_quality_page_locators import AccuracyAndQualityPageLocators as AC
-
-
-class AccuracyAndQualityPage(BasePage):
-    def check_visibility_of_number_of_cities(self):
-        number_of_cities = self.driver.find_element(*AC.number_of_cities_for_evaluation)
-        assert number_of_cities.is_displayed(), 'Number of cities link not found'
--- tests/test_group_lutz_squad/tests/test_accuracy_and_quality_page.py
+++
@@ -1,7 +0,0 @@
-from tests.test_group_lutz_squad.pages.accuracy_and_quality_page import AccuracyAndQualityPage
-from tests.test_group_lutz_squad.locators.accuracy_and_quality_page_locators import AccuracyAndQualityPageLocators as AC
-
-def test_TC_001_17_02_visibility_of_List_of_cities_module(driver):
-    page = AccuracyAndQualityPage(driver, link=AC.ACCURACY_AND_QUALITY_PAGE_LINK)
-    page.open_page()
-    page.check_visibility_of_number_of_cities()
