d03b0f460edc3778193e63e44c9e6dbeff003ed2
tatianaEl <tyelyashevich@yahoo.com>
2023-05-28 22:15:27 -0400

    RF_AT_001.12.04

--- tests/test_group_100000/locators/api_page_locators.py
+++ tests/test_group_100000/locators/api_page_locators.py
@@ -7,3 +7,7 @@ class RoadRiskApi:
     LINK_HOW_TO_REQUEST_RR_API = (By.CSS_SELECTOR, 'a[href="#how"]')
     SECTION_R_CONCEPTS = (By.XPATH, "//*[@id='concept']")
     TITLE_ROAD_RISK = (By.CSS_SELECTOR, '.breadcrumb-title')
+
+class WeatherConditions:
+    DRIZZLE_LOCATOR = (By.XPATH, '//a[contains(@href, "#Drizzle")]/ancestor-or-self::table')
+    CONDITION_URL = 'https://openweathermap.org/weather-conditions'
