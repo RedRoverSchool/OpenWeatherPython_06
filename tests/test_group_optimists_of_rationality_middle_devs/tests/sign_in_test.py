03d1d7f1afec7ce2e21dc8bb64bb81b6bce52362
ILLIA LEVCHENKO <illia.levchenko.work@gmail.com>
2023-05-28 23:47:22 -0700

    delete function test_TC_014_04_01_AT_014_04_01_01_Sign_in_Functionality_for_registered_User_Verify_authorization_with_valid_data

--- tests/test_group_optimists_of_rationality_middle_devs/tests/sign_in_test.py
+++ tests/test_group_optimists_of_rationality_middle_devs/tests/sign_in_test.py
@@ -9,9 +9,6 @@ from tests.test_group_optimists_of_rationality_middle_devs.locators.sign_in_loca
 import pytest
 
 class TestSignIn:
-    def test_TC_014_04_01_AT_014_04_01_01_Sign_in_Functionality_for_registered_User_Verify_authorization_with_valid_data(self, driver, open_and_load_main_page, sign_in):
-        assert driver.find_element(*SignInLocators.DIV_ALLERT_SUCCESSFULLY_LOGIN_GREEN)
-
     def test_TC_014_04_01_AT_014_04_01_01_Sign_in_Functionality_for_registered_User_Verify_authorization_with_valid_data_optimized(self, driver, open_and_load_sign_in_page, sign_in):
         """
         Triggering login through the created fixture
