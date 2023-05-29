3b3139d276de7582be396581beb41c3f17e07cd7
chemkomaksim <maksim.chemka@teladochealth.com>
2023-05-29 20:28:30 +0300

    change locators form accuracy page names to uppercase

--- tests/test_group_lutz_squad/locators/accuracy_and_quality_of_weather_data_page_locators.py
+++ tests/test_group_lutz_squad/locators/accuracy_and_quality_of_weather_data_page_locators.py
@@ -2,6 +2,6 @@ from selenium.webdriver.common.by import By
 
 
 class AccuracyAndQualityOfWeatherDataPageLocators:
-    accuracy_and_quality_of_weather_data_link_locator = (
+    ACCURACY_AND_QUALITY_OF_WEATHER_DATA_LINK_LOCATOR = (
     By.CSS_SELECTOR, 'a[href*="/accuracy-and-quality"]')
-    example_of_graphics_with_some_metrics_locator = (By.CSS_SELECTOR, 'img[src*="/themes/openweathermap/assets/img/accuracy_weather_data.png"]')
+    EXAMPLE_OF_GRAPHICS_WITH_SOME_METRICS_LOCATOR = (By.CSS_SELECTOR, 'img[src*="/themes/openweathermap/assets/img/accuracy_weather_data.png"]')
