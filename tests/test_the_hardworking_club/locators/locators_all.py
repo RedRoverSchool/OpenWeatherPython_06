7e5838574e5e05ce45495f6530facdce86e92ffc
dreamcat2022 <elena.ayrapetyan@gmail.com>
2023-05-29 16:38:34 -0700

    RF AT_018_01_01

--- tests/test_the_hardworking_club/locators/locators_all.py
+++ tests/test_the_hardworking_club/locators/locators_all.py
@@ -42,3 +42,15 @@ class GuidePageLocators:
     GLOBAL_PRECIP = (By.CSS_SELECTOR, "li a[href*='global-precipitation-map-forecast']")
     WEATHER_MAPS = (By.CSS_SELECTOR, "li a[href*='weather-map-1h']")
 
+class SubscriptionPagelocators():
+
+    URL_subscription_base = "https://home.openweathermap.org/subscriptions/unauth_subscribe/onecall_30/base"
+    RADIO_BUTTON_INDIVIDUAL = (By.CSS_SELECTOR, 'input:checked')
+    TEXT_EDIT_EMAIL = (By.XPATH, '//*[@id="unauth_subscription_form_email"]')
+    TEXT_EDIT_FIRST_NAME = (By.CSS_SELECTOR, '#unauth_subscription_form_first_name')
+    TEXT_EDIT_LAST_NAME = (By.XPATH, "//*[@id='unauth_subscription_form_last_name']")
+    TEXT_EDIT_ADDRESS_LINE_1 = (By.XPATH, "//*[@id='unauth_subscription_form_address_line_1']")
+    TEXT_EDIT_CITY = (By.XPATH, "//*[@id='unauth_subscription_form_city']")
+    TEXT_EDIT_POSTAL_CODE = (By.XPATH, "//*[@id='unauth_subscription_form_postal_code']")
+    TEXT_EDIT_PHONE = (By.XPATH, '//*[@id="unauth_subscription_form_phone"]')
+    CONTINUE_TO_PAYMENT_BUTTON = (By.XPATH, '//*[@id="new_unauth_subscription_form"]/div[3]/div/input')
