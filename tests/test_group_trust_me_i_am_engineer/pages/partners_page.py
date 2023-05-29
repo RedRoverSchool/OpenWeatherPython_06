f0f6c9df99ad4dda7bed8aa4314877727987a9e9
Яна Гуринович <49016274+gurinowicz@users.noreply.github.com>
2023-05-29 19:39:01 +0300

    AT_011.11.01 | Partners and solutions > CMS > Verify redirection of the link in the section 'Drupal OpenLayers Open Weather Map (#738)

--- tests/test_group_trust_me_i_am_engineer/pages/partners_page.py
+++ tests/test_group_trust_me_i_am_engineer/pages/partners_page.py
@@ -6,6 +6,7 @@ from tests.test_group_trust_me_i_am_engineer.locators.page_locators import Partn
 class PartnersPage(BasePage):
     URL_PARTNERS = 'https://openweathermap.org/examples'
     locators = PartnersPageLocators()
+    drupal_button = 'https://www.drupal.org/project/olowm'
 
     def verify_the_link_view_on_github_is_visible(self):
         self.driver.get(self.URL_PARTNERS)
@@ -13,3 +14,10 @@ class PartnersPage(BasePage):
         self.go_to_element(button)
         assert button.is_displayed(), \
             "The 'View on Github' button is not displayed"
+
+    def verify_redirection_drupal_button_leads_to_the_new_website(self):
+        self.driver.get(self.URL_PARTNERS)
+        button = self.driver.find_element(*self.locators.BUTTON_SEE_ON_THE_WEBSITE)
+        self.driver.execute_script("arguments[0].click();", button)
+        self.driver.switch_to.window(self.driver.window_handles[-1])
+        assert self.drupal_button, self.driver.current_url
