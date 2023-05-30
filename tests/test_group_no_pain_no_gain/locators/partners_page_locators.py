c5c721d4da1e808066c41364de09624fba97a969
NadyaSenyukova <nadyasenv86@gmail.com>
2023-05-30 21:47:16 +0300

    AT_011.06.01 Verify the first link in Android section is visible and clickable

--- tests/test_group_no_pain_no_gain/locators/partners_page_locators.py
+++ tests/test_group_no_pain_no_gain/locators/partners_page_locators.py
@@ -5,3 +5,4 @@ class PartnersPageLocators:
     PARTNERS_PAGE_HEADING = (By.XPATH, '//h1')
     PARTENERS_PAGE_INFO_BOARD = (By.XPATH, "//div[@class='info-board info-board-orange']")
     RASPBERRY = (By.CSS_SELECTOR, '#raspberry > a')
+    ANDROID_FIRST_LINK = (By.CSS_SELECTOR, '#android a[href*="/4-free-weather-providers-api-to-develop.html"]')
