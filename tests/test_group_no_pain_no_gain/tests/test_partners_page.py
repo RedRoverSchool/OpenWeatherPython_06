3b6deb7955deffcf363adbe1f1acb5ed98f2e3fd
Denis Prokofyev <denisprokofyev@gmail.com>
2023-05-30 20:58:59 +0300

    Add AT_011.12_01, Partners link to base_page, Correct vis and click method (#775)

--- tests/test_group_no_pain_no_gain/tests/test_partners_page.py
+++ tests/test_group_no_pain_no_gain/tests/test_partners_page.py
@@ -23,3 +23,8 @@ class TestPartnersPage:
         page = Partners(driver, links.PARTNERS_AND_SOLUTIONS)
         page.open_page()
         page.get_text_content_of_the_element(PPL.PARTNERS_PAGE_HEADING, PPD.PARTNERS_PAGE_HEADING)
+
+    def test_TC_011_12_01_verify_the_link_in_raspberry_is_visible_and_clickable(self, driver):
+        page = Partners(driver, links.PARTNERS_AND_SOLUTIONS)
+        page.open_page()
+        page.link_visibility_and_clickability(PPL.RASPBERRY, links.RASPBERRY_GITHUB)
