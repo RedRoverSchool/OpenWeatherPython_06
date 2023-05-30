26765fb3e2ed1c74cd3ca126e1e57507c65aba74
Yuliya Fatkina <juliadigrigorio@hotmail.com>
2023-05-30 20:41:35 +0300

    AT_011_01_02 Verify that the heading of the page equals to Partners and solutions (#769)

--- tests/test_group_no_pain_no_gain/locators/partners_page_locators.py
+++ tests/test_group_no_pain_no_gain/locators/partners_page_locators.py
@@ -2,5 +2,5 @@ from selenium.webdriver.common.by import By
 
 
 class PartnersPageLocators:
-    PARTNERS_PAGE_HEADER = (By.XPATH, '//h1')
+    PARTNERS_PAGE_HEADING = (By.XPATH, '//h1')
     PARTENERS_PAGE_INFO_BOARD = (By.XPATH, "//div[@class='info-board info-board-orange']")
