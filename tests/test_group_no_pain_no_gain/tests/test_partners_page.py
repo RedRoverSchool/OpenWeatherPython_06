809936ac936560c8fd5e2748a4dc29caa273d5fc
Yuliya Fatkina <juliadigrigorio@hotmail.com>
2023-05-30 15:59:58 +0300

    AT_011.20.02  Verify the color of the info-board message at the top of the page (#764)

--- tests/test_group_no_pain_no_gain/tests/test_partners_page.py
+++ tests/test_group_no_pain_no_gain/tests/test_partners_page.py
@@ -13,3 +13,8 @@ class TestPartnersPage:
         page = Partners(driver, links.PARTNERS_AND_SOLUTIONS)
         page.open_page()
         page.get_text_content_of_the_element(PPL.PARTENERS_PAGE_INFO_BOARD, PPD.PARTENERS_PAGE_INFO_BOARD_TEXT)
+
+    def test_TC_011_20_02_verify_the_color_of_the_info_board_message_at_the_top_of_the_page(self, driver):
+        page = Partners(driver, links.PARTNERS_AND_SOLUTIONS)
+        page.open_page()
+        page.get_background_color_of_the_element(PPL.PARTENERS_PAGE_INFO_BOARD, PPD.INFO_BOARD_BACKGROUND_COLOR)
