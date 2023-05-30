3cdaa5281478206604920db3d67a1994d0e88c6c
kengrad <xkenig@gmail.com>
2023-05-31 00:43:08 +0400

    AT_004.06.05 | Guide > Open weather products > Link 'industry standard APIs' has correct color

--- tests/test_group_lutz_squad/tests/test_guide_page.py
+++ tests/test_group_lutz_squad/tests/test_guide_page.py
@@ -6,3 +6,9 @@ def test_TC_004_06_01_verify_industry_standart_apis_link_is_visible_and_clickabl
     page = GuidePage(driver, link=GuidePageLocators.GUIDE_PAGE_LINK)
     page.open_page()
     page.industry_apis_link_is_visible_and_clickable()
+
+
+def test_TC_004_06_05_verify_industry_standart_apis_link_color(driver, wait):
+    page = GuidePage(driver, link=GuidePageLocators.GUIDE_PAGE_LINK)
+    page.open_page()
+    page.industry_check_color()
