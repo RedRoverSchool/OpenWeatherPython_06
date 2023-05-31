da3bbf5165fecf43816cf77ca62818519d59f4cf
Kristina Ozhog <kristinka_kuku@mail.ru>
2023-05-31 01:33:12 +0100

    TC_001.13.02 | Main page > Cookies settings > Radio buttons and 'Save changes' button visibility and clickability, changes saving functionality > Verify the saving of cookies settings

---
+++ tests/test_group_lutz_squad/tests/test_cookies_settings_page.py
@@ -0,0 +1,13 @@
+from tests.test_group_lutz_squad.pages.cookies_settings_page import CookiesSettingsPage
+from tests.test_group_lutz_squad.locators.cookies_settings_page_locators import \
+    CookiesSettingsPageLocators as CS
+
+
+def test_TC_001_13_02_verify_the_saving_of_cookies_settings(driver, wait):
+    page = CookiesSettingsPage(driver, link=CS.COOKIES_SETTINGS_PAGE_LINK)
+    page.open_page()
+    page.scroll_page_down_()
+    page.click_cookies_that_analyse_radiobutton(wait)
+    page.click_google_advertising_radiobutton(wait)
+    page.click_save_changes_button(wait)
+    page.verify_successful_message_appears(wait)
