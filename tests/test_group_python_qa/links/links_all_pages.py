3b1aeee33042a242ca89abb8a191f820578b3601
Zoe Novak <129424393+ezoenovak@users.noreply.github.com>
2023-05-31 07:07:07 +0400

    RF | AT_001.17.01 | Refactor according to POM (#763)
    * RF | AT_001.17.01 | Refactor according to POM
    
    * ref: RF | AT_001.17.01 | changed test name

---
+++ tests/test_group_python_qa/links/links_all_pages.py
@@ -0,0 +1,8 @@
+MAIN_PAGE = 'https://openweathermap.org/'
+URL_SOLAR_API = "https://openweathermap.org/api/solar-energy-prediction"
+QUALITY_INFO_PAGE = "https://openweathermap.org/accuracy-and-quality"
+URL_SUBSCRIPTION_BASE = 'https://home.openweathermap.org/subscriptions/unauth_subscribe/onecall_30/base'
+OUR_INITIATIVES_PAGE = 'https://openweathermap.org/our-initiatives'
+GUIDE_PAGE = "https://openweathermap.org/guide"
+URL_HISTORY_BULK = "https://openweathermap.org/history-bulk"
+URL_WEATHER_CONDITIONS = "https://openweathermap.org/weather-conditions"
