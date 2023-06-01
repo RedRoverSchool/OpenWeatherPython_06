6a9c5a742544a7e1c982f007fbaceedafd63b702
Galina Danilova <galya.danilova@gmail.com>
2023-06-01 19:48:19 -0400

    AT_001.15.06 | Main page > OpenWeather global services (About us) > Verify redirection to the email-form (#797)

--- tests/test_group_future_auto_qa/tests/about_us_page_test.py
+++ tests/test_group_future_auto_qa/tests/about_us_page_test.py
@@ -7,3 +7,11 @@ def test_TC_001_15_08_contact_us_button_is_clickable(driver):
     about_us_page.open()
     about_us_page.scroll_to_contact_us_button()
     assert about_us_page.contact_us_button_is_clickable()
+
+def test_TC_001_15_06_Verify_correct_redirection_of_the_contact_us_button(driver):
+    about_us_page = AboutUsPage(driver)
+    about_us_page.open()
+    # Allow all cookies so that the test can interact with the Contact Us button
+    about_us_page.allow_all_cookies()
+    about_us_page.verify_redirection_contact_us_button_to_the_new_webpage()
+
