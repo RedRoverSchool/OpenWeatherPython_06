484a7e1b0b074156f988effa0b87b25ddb7edefe
Cruz06 <104589825+Cruz06@users.noreply.github.com>
2023-06-01 21:20:17 +0300

    Update test_our_initiatives_page.py

--- tests/test_pandoras_box/tests/test_our_initiatives_page.py
+++ tests/test_pandoras_box/tests/test_our_initiatives_page.py
@@ -5,3 +5,4 @@ def test_010_02_02_check_title_student_initiatives(driver, open_and_load_main_pa
     our_initiatives = OurInitiativesPage(driver)
     our_initiatives.click_header_link('our initiatives')
     our_initiatives.verify_learn_more_link_redirects_to_valid_page()
+    
