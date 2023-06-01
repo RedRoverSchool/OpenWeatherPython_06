a8370ba2779ff84a5e8f05a0b4fbd8bc4c2e3032
Cruz06 <104589825+Cruz06@users.noreply.github.com>
2023-06-01 21:13:17 +0300

    Update our_initiatives_page.py
    corr2

--- tests/test_pandoras_box/pages/our_initiatives_page.py
+++ tests/test_pandoras_box/pages/our_initiatives_page.py
@@ -4,22 +4,13 @@ from pages.base_page import BasePage
 
 class OurInitiativesPage(BasePage):
 
-    DISPLAYED_TITLE = (By.CSS_SELECTOR, 'h1.breadcrumb-title')
+    DISPLAYED_TITLE = (By.XPATH, '//div[2]//h1')
     BUTTON_LEARN_MORE = (By.CSS_SELECTOR, 'a[class="ow-btn round btn-black"]')
-    our_initiatives_link = (By.CSS_SELECTOR, '#desktop-menu ul li:nth-child(7) a')
-    OUR_INITIATIVES_URL = 'https://openweathermap.org/our-initiatives'
+       
     
-    
-    def check_header_title(self, link_name):
-        self.click_header_link(link_name)
-        expected_title = "Our Initiatives"
-        displayed_title = self.driver.find_element(*self.DISPLAYED_TITLE).text
-        assert displayed_title == expected_title
-        
-        
-    def verify_learn_more_link_redirects_to_valid_page(self):
+   def verify_learn_more_link_redirects_to_valid_page(self):
         self.driver.execute_script("window.scrollTo(0, 500)")
         self.driver.find_element(*self.BUTTON_LEARN_MORE).click()
         actual_title = self.driver.find_element(*self.DISPLAYED_TITLE).text
-        expected_title = "Student initiative"
+        expected_title = "Free Data for Students"
         assert expected_title == actual_title
