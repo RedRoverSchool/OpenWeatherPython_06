9ae01377129323b1ef89606e83bc3129f39d32a4
Cruz06 <104589825+Cruz06@users.noreply.github.com>
2023-06-01 22:01:08 +0300

    RF | AT_010.02.02 | Our Initiatives > Student initiative > Visibility of t… (#767)
    * AT_010.02.02 | Our Initiatives > Student initiative > Visibility of title Free Data for Students
    
    * Update our_initiatives_page.py
    
    * Update test_our_initiatives_page.py
    
    * Update our_initiatives_page.py
    
    corr2
    
    * Update test_our_initiatives_page.py
    
    * Update test_our_initiatives_page.py
    
    * Update our_initiatives_page.py

---
+++ tests/test_pandoras_box/tests/test_our_initiatives_page.py
@@ -0,0 +1,8 @@
+from tests.test_pandoras_box.pages.our_initiatives_page import OurInitiativesPage
+
+
+def test_010_02_02_check_title_student_initiatives(driver, open_and_load_main_page):
+    our_initiatives = OurInitiativesPage(driver)
+    our_initiatives.click_header_link('our initiatives')
+    our_initiatives.verify_learn_more_link_redirects_to_valid_page()
+    
