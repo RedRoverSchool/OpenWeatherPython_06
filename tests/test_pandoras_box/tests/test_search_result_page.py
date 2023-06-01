7024f7a4a933c3290309e3ff2d877390acde605f
Cruz06 <Olarut72@gmail.com>
2023-06-01 22:09:38 +0300

    RF | AT_001.08.03 | Main page >Search city widget > Hourly forecast for current location> Chart is present and visible

--- tests/test_pandoras_box/tests/test_search_result_page.py
+++ tests/test_pandoras_box/tests/test_search_result_page.py
@@ -2,13 +2,13 @@ from tests.test_pandoras_box.pages.search_result_page import SearchResultPage
 from tests.test_pandoras_box.pages.main_page import MainPage
 
 
-
 def test_TC_002_02_03_verify_result_of_search_for_invalid_city_name(driver, open_and_load_main_page):
     main_page = MainPage(driver)
     main_page.enter_city_in_weather_in_your_city_field('LJKJJ')
     main_page.press_enter_button()
     search_result_page = SearchResultPage(driver)
     search_result_page.check_notification_display()
+
 
 def test_TC_002_02_06_verify_closing_of_NotFound_notification(driver, open_and_load_main_page, wait):
     main_page = MainPage(driver)
@@ -17,9 +17,15 @@ def test_TC_002_02_06_verify_closing_of_NotFound_notification(driver, open_and_l
     search_result_page = SearchResultPage(driver)
     search_result_page.check_notification_is_closed()
 
+
 def test_TC_002_02_04_verify_displaying_entered_city_name_in_Search_field(driver, open_and_load_main_page, wait):
     main_page = MainPage(driver)
     main_page.enter_city_in_weather_in_your_city_field(' ljKJJ')
     main_page.press_enter_button()
     search_result_page = SearchResultPage(driver)
     search_result_page.check_correspondence_of_entered_text(' ljKJJ')
+
+
+def test_tc_001_08_03_chart_current_weather(driver, open_and_load_main_page):
+    page = MainPage(driver)
+    page.check_chart_weather_is_displayed()
