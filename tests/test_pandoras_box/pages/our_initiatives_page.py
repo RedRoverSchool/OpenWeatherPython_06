89ee6ba122b65874a383f452f6a84c127a8e4d35
Cruz06 <104589825+Cruz06@users.noreply.github.com>
2023-06-02 17:17:40 +0300

    RF | AT_010.01.01_1 | Our Initiatives > Visibility, сlickability, correct redirection > Verify button "Learn more", visibility, text on the button (#817)

--- tests/test_pandoras_box/pages/our_initiatives_page.py
+++ tests/test_pandoras_box/pages/our_initiatives_page.py
@@ -16,7 +16,14 @@ class OurInitiativesPage(BasePage):
         assert expected_title == actual_title
 
 
+    def check_text_on_button_learn_more(self):
+        button_learn_more = self.driver.find_element(*self.BUTTON_LEARN_MORE)
+        expected_text = "Learn more"
+        assert expected_text == button_learn_more.text
+
+        
     def verify_correct_url_out_initiative(self):
         actual_url = self.driver.current_url
         expected_url = self.our_initiatives_url
         assert actual_url == expected_url
+
