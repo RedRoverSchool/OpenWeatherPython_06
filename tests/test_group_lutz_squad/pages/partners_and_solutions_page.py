6738d403c5b82cf31d3d3b75eaea31ee36a4baee
Kristina Ozhog <kristinka_kuku@mail.ru>
2023-06-03 00:11:49 +0100

    AT_011.02.02 | Partners and solutions > Google Weather-Based Campaign Management with OpenWeatherMap API > Verify that user is redirected to a new window page after click on the link 'More detailes with source code'

--- tests/test_group_lutz_squad/pages/partners_and_solutions_page.py
+++ tests/test_group_lutz_squad/pages/partners_and_solutions_page.py
@@ -9,3 +9,12 @@ class PartnersAndSolutionsPage(BasePage):
         self.go_to_element(more_details_link)
         self.element_is_displayed(PAS.MORE_DETAILS_LOCATOR, wait)
         assert self.element_is_clickable(PAS.MORE_DETAILS_LOCATOR)
+
+    def redirecting_to_more_details_with_source_code_page(self, wait):
+        more_details_link = self.driver.find_element(*PAS.MORE_DETAILS_LOCATOR)
+        self.driver.execute_script("arguments[0].click();", more_details_link)
+        new_window = self.driver.window_handles[1]
+        self.driver.switch_to.window(new_window)
+        weather_based = self.driver.find_element(*PAS.WEATHER_BASED_COMPAIGN_MANAGEMENT)
+        assert weather_based.is_displayed(), 'Weather-based Campaign Management header is not on this page'
+
