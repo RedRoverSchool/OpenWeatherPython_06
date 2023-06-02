49782a6ea61687b9693d577e37c02ba68379910a
evgeniia demchenko <demchenkoeb04022012@gmail.com>
2023-06-02 05:45:10 +0300

    RF_001_15_07 create class blog_category_weather_page and move there class method

---
+++ tests/test_group_trust_me_i_am_engineer/pages/blog_category_weather_page.py
@@ -0,0 +1,11 @@
+from pages.base_page import BasePage
+
+
+class BlogCategoryWeatherPage(BasePage):
+
+    URL = "https://openweather.co.uk/blog/category/weather"
+
+    def verify_page_url(self, button, expected_link):
+        page_url = self.driver.current_url
+        assert page_url == expected_link, button + " button link is not correct"
+
