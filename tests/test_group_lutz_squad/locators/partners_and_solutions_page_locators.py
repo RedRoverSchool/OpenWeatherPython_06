5b15ae3353dba9c38f5c72a16c588932904ed894
Maksim Chemka <77155968+chemkomaksim@users.noreply.github.com>
2023-06-02 11:25:36 +0300

    AT 011.02.01 Partners and solutions > Google Weather-Based Campaign Management with OpenWeatherMap API > Verify visibility and clickability of 'More detailes with source code' link (#790)
    * switched locators names to uppercase
    
    * AT_011.02.01 Partners and solutions > Google Weather-Based Campaign Management with OpenWeatherMap API > Verify visibility and clickability of 'More detailes with source code' link
    
    ---------
    
    Co-authored-by: chemkomaksim <maksim.chemka@teladochealth.com>

---
+++ tests/test_group_lutz_squad/locators/partners_and_solutions_page_locators.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+
+
+class PartnersAndSolutionsPageLocators:
+    PARTNERS_AND_SOLUTIONS_PAGE_LINK = 'https://openweathermap.org/examples'
+    MORE_DETAILS_LOCATOR = (By.CSS_SELECTOR, '#google1 > a')
