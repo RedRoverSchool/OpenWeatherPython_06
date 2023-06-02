3ccbf1e2ef1485414d623fffc6ea99bdf410ca86
gdanilova <130004322+gdanilova@users.noreply.github.com>
2023-06-01 23:30:54 -0400

    AT_011.13.01 | Partners and solutions > Python > Visibility and clickability of the link "View on Github"

--- tests/test_group_future_auto_qa/tests/partners_page_test.py
+++ tests/test_group_future_auto_qa/tests/partners_page_test.py
@@ -1,5 +1,9 @@
 from tests.test_group_future_auto_qa.pages.partners_page import PartnersPage
 
+def test_TC_011_13_01_Verify_visibility_and_clickability_of_the_link_View_on_Github(driver):
+    partners_page = PartnersPage(driver)
+    partners_page.verify_visibility_and_clickability_git_button_python()
+
 def test_TC_011_13_02_Verify_correct_redirection_of_the_link_View_on_Github(driver):
     partners_page = PartnersPage(driver)
     partners_page.verify_redirection_git_button_python_to_the_new_webpage()
