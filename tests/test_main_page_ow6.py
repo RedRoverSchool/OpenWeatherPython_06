1228277cfd7482b500d79407573542d5e3173e30
aigulmustafina <aimustafina2016@gmail.com>
2023-06-03 17:12:40 +0600

    Add example test

--- tests/test_main_page_ow6.py
+++ tests/test_main_page_ow6.py
@@ -2,5 +2,7 @@ from pages.main_page import MainPage
 
 
 class TestMainPage:
-    pass
+    def test_example_search_city(self, driver, open_and_load_main_page):
+        main_page = MainPage(driver)
+        main_page.search_city('Almaty')
 
