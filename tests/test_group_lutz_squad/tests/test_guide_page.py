d31df0a527a93b6a4774cc30c5b8e2ed87fb1a66
irinagorlach <105638690+irinagorlach@users.noreply.github.com>
2023-06-03 12:40:01 +0300

    feat: add AT_004.06.03 | Guide > Open weather products > Redirection to 'Weather API' page after click on the link 'industry standard APIs' (#812)

--- tests/test_group_lutz_squad/tests/test_guide_page.py
+++ tests/test_group_lutz_squad/tests/test_guide_page.py
@@ -1,3 +1,5 @@
+import time
+
 from tests.test_group_lutz_squad.pages.guide_page import GuidePage
 from tests.test_group_lutz_squad.locators.guide_page_locators import GuidePageLocators
 
@@ -12,3 +14,9 @@ def test_TC_004_06_05_verify_industry_standart_apis_link_color(driver, wait):
     page = GuidePage(driver, link=GuidePageLocators.GUIDE_PAGE_LINK)
     page.open_page()
     page.industry_check_color()
+
+def test_TC_004_06_03_verify_redirection_industry_standard_apis_link(driver, wait):
+    page = GuidePage(driver, link=GuidePageLocators.GUIDE_PAGE_LINK)
+    page.open_page()
+    page.industry_standard_apis_link_redirection()
+
