cf46a9cc35815d6ddbbbcff32aaa6f774936902d
Elena_S <sosnitsky.e@gmail.com>
2023-06-04 11:53:35 +0300

    RF_Final | group_ducktales |Sosnitsky_E (#836)
    * RF_Final | group_ducktales |Sosnitsky_E
    
    * rename pages
    
    * resolve conflicts
    
    * resolve conflicts

--- locators/locators.py
+++ locators/locators.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 
+
 class BasePageLocators:
     SIGN_IN_LINK = (By.CSS_SELECTOR, '.user-li a')
     GUIDE_LINK = (By.CSS_SELECTOR, "#desktop-menu a[href*='guide']")
@@ -13,7 +14,9 @@ class BasePageLocators:
     FAQ_OPTION = (By.XPATH, "//*[@id='support-dropdown-menu']//a[@href='/faq']")
     MAPS_LINK = (By.CSS_SELECTOR, '#desktop-menu ul li:nth-child(6) a')
     HOW_TO_START_OPTION = (By.XPATH, "//*[@id='support-dropdown-menu']//a[@href='/appid']")
-    ASK_A_QUESTION_OPTION = (By.XPATH, "//*[@id='support-dropdown-menu']//a[@href='https://home.openweathermap.org/questions']")
+    ASK_A_QUESTION_OPTION = (
+        By.XPATH, "//*[@id='support-dropdown-menu']//a[@href='https://home.openweathermap.org/questions']")
+
 
 class MainPageLocators:
     SEARCH_DROPDOWN = (By.CSS_SELECTOR, 'ul.search-dropdown-menu li')
@@ -25,11 +28,14 @@ class MainPageLocators:
     ALLOW_ALL_COOKIES_BUTTON = (By.XPATH, "//button[contains(text(), 'Allow all')]")
     ABOUT_US_BUTTON = (By.XPATH, "//a[@href='/about-us']")
 
+
 class DashboardPageLocators:
     pass
 
+
 class PricingPageLocators:
     pass
+
 
 class ApiPageLocators:
     pass
@@ -47,3 +53,26 @@ class AboutUsPageLocators:
 class AppStorePageLocators:
 
     APP_TITLE = (By.XPATH, "//h1")
+
+
+class ApiKeysLocator:
+    API_KEY_NAME_URL = 'https://home.openweathermap.org/api_keys'
+    API_KEY_EDIT_SELECTOR = By.CSS_SELECTOR, "i[class='fa fa-edit']"
+    API_KEY_NAME_SELECTOR = By.XPATH, "//table/tbody/tr/td[2]"
+    API_KEY_ENTER_SELECTOR = By.CSS_SELECTOR, "input[name='edit_key_form[name]']"
+    SAVE_BUTTON_SELECTOR = By.CSS_SELECTOR, "button[class='button-round dark']"
+    TAB_API_KEYS = By.CSS_SELECTOR, '#myTab [href="/api_keys"]'
+    MODULE_API_KEY_CREATE = By.CSS_SELECTOR, '.col-md-4 h4'
+    EDIT_API_KEY_ICON = By.CSS_SELECTOR, '.edit_key_btn .fa-edit'
+    API_KEY_FIELD = (By.CSS_SELECTOR, '#new_edit_key_form .owm_input')
+    SAVE_NEW_API_NAME_BUTTON = By.CSS_SELECTOR, '.pop-up-footer .button-round.dark'
+    API_KEY_NAME_FIRST_ROW = By.XPATH, "//div[@class='col-md-8']//tr[1]//td[2]"
+    NEW_API_KEY_NAME = By.CSS_SELECTOR, ".new_api_key_form .owm_input"
+    GENERATE_BUTTON = By.CSS_SELECTOR, '.new_api_key_form .button-round.dark'
+    TABLE_API_KEYS = By.CSS_SELECTOR, "tbody tr"
+
+
+class SignInLocator:
+    EMAIL_INPUT = By.CSS_SELECTOR, '#user_email'
+    PASSWORD_INPUT = By.CSS_SELECTOR, '#user_password'
+    SUBMIT_BUTTON = By.CSS_SELECTOR, "input[value='Submit']"
