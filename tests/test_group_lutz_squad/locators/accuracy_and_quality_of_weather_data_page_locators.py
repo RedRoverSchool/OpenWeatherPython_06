5fedf65464f0361b6ff4ffeac3f2e04562ba67d7
Kristina Ozhog <97105702+KristinaOzhog@users.noreply.github.com>
2023-06-04 14:55:13 +0100

    AT_004.06.02 | Guide > Open weather products > Verify visibility and clickability 'One Call API by call' link (#827)

--- tests/test_group_lutz_squad/locators/accuracy_and_quality_of_weather_data_page_locators.py
+++ tests/test_group_lutz_squad/locators/accuracy_and_quality_of_weather_data_page_locators.py
@@ -4,4 +4,4 @@ class AccuracyAndQualityOfWeatherDataPageLocators:
     ACCURACY_AND_QUALITY_PAGE_LINK = "https://openweathermap.org/accuracy-and-quality"
     ACCURACY_AND_QUALITY_OF_WEATHER_DATA_LINK_LOCATOR = (By.CSS_SELECTOR, 'a[href*="/accuracy-and-quality"]')
     EXAMPLE_OF_GRAPHICS_WITH_SOME_METRICS_LOCATOR = (By.CSS_SELECTOR, 'img[src*="/themes/openweathermap/assets/img/accuracy_weather_data.png"]')
-    number_of_cities_for_evaluation = (By.CSS_SELECTOR, ".col-sm-12 p>a")
+    NUMBER_OF_CITIES_FOR_EVALUATION = (By.CSS_SELECTOR, ".col-sm-12 p>a")
--- tests/test_group_lutz_squad/pages/guide_page.py
+++ tests/test_group_lutz_squad/pages/guide_page.py
@@ -19,6 +19,17 @@ class GuidePage(BasePage):
                 'color'))
         assert industry_apis_link_color.hex == industry_apis_link_color_hex
 
+
+    def one_call_api_link_is_visible(self):
+        one_call_api = self.driver.find_element(*GuidePageLocators.ONE_CALL_API_BY_CALL_LOCATOR)
+        assert one_call_api.is_displayed(), "One call api link is not visible"
+
+        
+    def one_call_api_link_is_clickable(self):
+        one_call_api = self.driver.find_element(*GuidePageLocators.ONE_CALL_API_BY_CALL_LOCATOR)
+        assert one_call_api.is_enabled(), "One call api link is not clackable"
+
+        
     def industry_standard_apis_link_redirection(self):
         element = self.driver.find_element(*GuidePageLocators.INDUSTRY_APIS_LOCATOR)
         self.driver.execute_script("arguments[0].click();", element)
@@ -26,10 +37,8 @@ class GuidePage(BasePage):
         assert self.driver.current_url == expected_link, "This link is not correct"
 
 
-
     def one_call_api_by_call_link_redirection(self):
         element = self.driver.find_element(*GuidePageLocators.ONE_CALL_API_BY_CALL_LOCATOR)
         self.driver.execute_script("arguments[0].click();", element)
         expected_link = ONE_CALL_3_LINK
         assert self.driver.current_url == expected_link, "This link is not correct"
-
