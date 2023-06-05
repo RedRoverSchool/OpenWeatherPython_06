13d4d0c3442913f2e377cd31bbe62dcb9b11e3de
Iuliia Patriarkh <yuliia.patriarh14@gmail.com>
2023-06-05 17:02:07 -0500

    Add: test_TC_003_12_06_verify_privacy_policy_is_opened_after_click

--- tests/test_main_page_ow6.py
+++ tests/test_main_page_ow6.py
@@ -94,9 +94,7 @@ class TestMainPage:
             page = MainPage(driver)
             page.verify_widgets_clickability()
 
-        def test_TC_003_12_06_verify_privacy_policy_is_opened_after_click(self, driver, wait, open_and_load_main_page):
-            main_page = MainPage(driver)
-            main_page.verify_privacy_policy_is_opened_after_click(driver, wait)
+
 
 
     class TestHowToStartLink:
