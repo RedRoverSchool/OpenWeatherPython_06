4503cc65ab1e0b1a929f67a654bc8876fabf53c5
Margo_Vodzi <margarita.vodzinskaya@ilogos.biz>
2023-06-05 13:03:02 -0400

    RF_AT_006.04.03 | Weather Dashboard > Pricing and limits >Verify that the "Subscribe" button is clickable in the "Pricing and limits" section

---
+++ tests/test_group_optimists_of_rationality_middle_devs/locators/dashboard_page_locators.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+
+
+class DashboardLocators:
+    HEADER_DASHBOARD_LINK = "https://openweathermap.org/weather-dashboard/"
+    HEADER_DASHBOARD = (By.XPATH, '//*[@id="desktop-menu"]/ul/li[3]/a')
+    PRICING_PLANS_SUBSCRIBE = By.XPATH, '//tr[1]/th[3]/p/a'
+    PRICING_PLANS_SUBSCRIBE1 = By.XPATH, '//tr[1]/th[4]/p/a'
+    PRICING_PLANS_SUBSCRIBE2 = By.XPATH, '//tr[1]/th[5]/p/a'
+    PRICING_PLANS_SUBSCRIBE3 = By.XPATH, '//tr[1]/th[6]/p/a'
