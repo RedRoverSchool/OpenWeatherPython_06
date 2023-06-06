8b6fd26630ef4a244c6772cbb3dc174c0038f2fa
Margo_Vodzi <margarita.vodzinskaya@ilogos.biz>
2023-06-06 19:30:59 -0400

    RF_AT_006.04.02 | Weather Dashboard > Pricing and limits >Verify that the "Sign up" button is clickable in the "Pricing and limits" section

--- tests/test_group_optimists_of_rationality_middle_devs/tests/dashboard_page_test.py
+++ tests/test_group_optimists_of_rationality_middle_devs/tests/dashboard_page_test.py
@@ -4,13 +4,13 @@ from tests.test_group_optimists_of_rationality_middle_devs.pages.dashboard_page 
 
 class TestDashboardPage:
 
-    def test_006_04_03_Verify_that_the_subscribe_button_are_clickable_in_the_Pricing_and_limits_ection(self,
+    def test_TC_006_04_03_Verify_that_the_subscribe_button_are_clickable_in_the_Pricing_and_limits_ection(self,
                                                                                                        driver):
         dashboard_page = Dashboard(driver, DashboardLocators.HEADER_DASHBOARD_LINK)
         dashboard_page.open_page()
         dashboard_page.subscribe_buttons_are_clickable()
 
-    def test_TC_006_03_01_verify_display_of_client_logos(self, driver):
+    def test_TC_006_03_01_Verify_display_of_client_logos(self, driver):
         page = Dashboard(driver, DashboardLocators.HEADER_DASHBOARD_LINK)
         page.open_page()
         page.verify_display_of_client_logos()
@@ -19,3 +19,8 @@ class TestDashboardPage:
         dashboard_page = Dashboard(driver, DashboardLocators.HEADER_DASHBOARD_LINK)
         dashboard_page.open_page()
         dashboard_page.verify_display_of_Pricing_and_limits_section()
+
+    def test_TC_006_04_02_Verify_that_the_Sign_up_button_is_clickable_in_the_Pricing_and_limits_section(self, driver):
+        dashboard_page = Dashboard(driver, DashboardLocators.HEADER_DASHBOARD_LINK)
+        dashboard_page.open_page()
+        dashboard_page.verify_sign_up_button_is_clickable()
