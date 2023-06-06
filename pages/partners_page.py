f64672704f5fb83fd53f3f90b5fd3e81ceb2f252
Natalya Mastyaeva <na_mastyaeva@mail.ru>
2023-06-06 13:30:31 +0400

    RF_Final | group_future_Auto_QA | Natalya_Mastyaeva

--- pages/partners_page.py
+++ pages/partners_page.py
@@ -1,4 +1,21 @@
+from locators.locators import PartnersLocators
 from pages.base_page import BasePage
+from test_data.urls import PartnersPageUrls
+
 
 class PartnersPage(BasePage):
-    pass
+    locators = PartnersLocators()
+
+    def verify_redirected_the_link_apache_camel_to_a_new_window(self, apache_camel):
+        self.driver.get(PartnersPageUrls.PARTNERS_AND_SOLUTIONS)
+        self.allow_all_cookies()
+        self.find_element_and_click(self.locators.APACHE_CAMEL_BUTTON)
+        self.switch_to_new_window()
+        apache_camel_link = self.driver.current_url
+        assert apache_camel_link == apache_camel, "Incorrect link"
+
+    def verify_visibility_and_clickability_the_link_apache_camel_to_a_new_window(self, apache_camel):
+        self.driver.get(PartnersPageUrls.PARTNERS_AND_SOLUTIONS)
+        self.allow_all_cookies()
+        assert self.element_is_visible(self.locators.APACHE_CAMEL_BUTTON) and self. \
+            element_is_clickable(self.locators.APACHE_CAMEL_BUTTON), "Apache Camel is not visible or not clickable"
