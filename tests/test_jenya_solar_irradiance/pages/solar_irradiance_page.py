e636a79dc47461d324a4dc4a2973002216153642
hamilova <hamilovaevgeniya@gmail.com>
2023-06-06 13:25:55 +0300

    feat:added test_TC_005_09_03_verify_the_most_common_indices_description

---
+++ tests/test_jenya_solar_irradiance/pages/solar_irradiance_page.py
@@ -0,0 +1,32 @@
+from pages.base_page import BasePage
+from locators.locators import SolarIrradianceLocators
+
+
+
+
+class SolarIrradiancePage(BasePage):
+    locators = SolarIrradianceLocators()
+
+
+    def verify_description_visibility(self):
+        locator = self.locators.SOLAR_IRRADIANCE_DESC
+        element = self.driver.find_element(*locator)
+        assert element.is_displayed()
+#check description visibility
+    def verify_indices_visibility(self):
+        locators = [self.locators.INDICES_DHI, self.locators.INDICES_DNI, self.locators.INDICES_GHI]
+
+        for locator in locators:
+            element = self.driver.find_element(*locator)
+            assert element.is_displayed()
+
+
+    def verify_reference_data_visibility(self):
+        locator = self.locators.REFERENCE_DATA
+        element = self.driver.find_element(*locator)
+        assert element.is_displayed()
+# check link hover
+
+# check link clickability
+
+#check table visibility
---
+++ tests/test_jenya_solar_irradiance/tests/test_jenya_solar_irradiance_page.py
@@ -0,0 +1,16 @@
+from tests.test_jenya_solar_irradiance.pages.solar_irradiance_page import SolarIrradiancePage
+from locators.locators import SolarIrradianceLocators
+def test_TC_005_09_01_verify_solar_irradiance_description(driver):
+    page = SolarIrradiancePage(driver, SolarIrradianceLocators.URL_SolarIrradiancePage)
+    page.open_page()
+    page.verify_description_visibility()
+
+def test_TC_005_09_02_verify_the_most_common_indices_description(driver):
+    page = SolarIrradiancePage(driver, SolarIrradianceLocators.URL_SolarIrradiancePage)
+    page.open_page()
+    page.verify_indices_visibility()
+
+def test_TC_005_09_03_verify_reference_data_visibility(driver):
+    page = SolarIrradiancePage(driver, SolarIrradianceLocators.URL_SolarIrradiancePage)
+    page.open_page()
+    page.verify_reference_data_visibility()
