22d3986d612e578e9222f115f2ed1da4cb68be57
Ekaterina Myshov <79929359+KatyaMy@users.noreply.github.com>
2023-06-06 01:00:24 -0400

    TC_005.06.02 | Weather API > Climatic forecast 30 days > Redirect to the How to make an API call section of the page (#873)

--- tests/test_group_100000/pages/api_page.py
+++ tests/test_group_100000/pages/api_page.py
@@ -82,3 +82,8 @@ class ClimaticForecast(BasePage):
     def check_visibility_climatic_forecast_30_days_page_title(self):
         title_page = self.driver.find_element(*ClimateForecast.TITLE_FORCAST30).text
         assert title_page == 'Climate forecast for 30 days', 'The title of the page does not match the expected value'
+
+    def check_redirect_to_the_how_to_make_of_the_page(self):
+        self.find_element_and_click(ClimateForecast.LINK_HOW_TO_MAKE)
+        new_page_title = self.driver.find_element(*ClimateForecast.TITLE_HOW_TO_MAKE)
+        assert new_page_title.is_displayed(), 'The title of the page does not match the expected value'
