1be7ecab419391cd8c0ad94e2323d23a833c9c6f
Andrei Novikau <andrei.novikau92@gmail.com>
2023-06-05 22:37:36 -0500

    ref: add a method in the cookies_settings_page_pqa.py

--- tests/test_group_python_qa/pages/cookies_settings_page_pqa.py
+++ tests/test_group_python_qa/pages/cookies_settings_page_pqa.py
@@ -4,3 +4,10 @@ from tests.test_group_python_qa.locators.locators import CookiesSettingsLocators
 
 class CookiesSettings(BasePage):
     locator = CookiesSettingsLocators()
+
+    def turn_on_cookies_and_verify_cookies_savings(self, wait):
+        self.allow_all_cookies()
+        self.find_element_and_click(CookiesSettingsLocators.RADIOBUTTON_1_LOCATOR)
+        self.find_element_and_click(CookiesSettingsLocators.RADIOBUTTON_2_LOCATOR)
+        self.find_element_and_click(CookiesSettingsLocators.SAVE_CHANGES_BUTTON)
+        self.element_is_displayed(CookiesSettingsLocators.ALERT_BANNER, wait)
--- tests/test_group_python_qa/tests/test_cookies_settings_page_pqa.py
+++ tests/test_group_python_qa/tests/test_cookies_settings_page_pqa.py
@@ -1,16 +1,10 @@
-from conftest import driver
 from tests.test_group_python_qa.pages.cookies_settings_page_pqa import CookiesSettings
 from tests.test_group_python_qa.links.links_all_pages import *
-from tests.test_group_python_qa.locators.locators import CookiesSettingsLocators
 
 
 class TestCookiesSettings:
     def test_tc_001_13_02_01_verify_the_savings_of_cookies_settings(self, driver, wait):
         settings_cookies_page = CookiesSettings(driver, URL_COOKIES_SETTINGS)
         settings_cookies_page.open_page()
-        settings_cookies_page.find_element_and_click(CookiesSettingsLocators.ALLOW_BUTTON)
-        settings_cookies_page.find_element_and_click(CookiesSettingsLocators.RADIOBUTTON_1_LOCATOR)
-        settings_cookies_page.find_element_and_click(CookiesSettingsLocators.RADIOBUTTON_2_LOCATOR)
-        settings_cookies_page.find_element_and_click(CookiesSettingsLocators.SAVE_CHANGES_BUTTON)
-        settings_cookies_page.element_is_displayed(CookiesSettingsLocators.ALERT_BANNER, wait)
+        settings_cookies_page.turn_on_cookies_and_verify_cookies_savings(wait)
 
