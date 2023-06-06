e33d73a88c79ecedfc8663f4256b95e283cf3868
hamilova <hamilovaevgeniya@gmail.com>
2023-06-06 13:48:14 +0300

    feat: conflict resolved

--- tests/test_jenya_solar_irradiance/tests/test_jenya_solar_irradiance_page.py
+++ tests/test_jenya_solar_irradiance/tests/test_jenya_solar_irradiance_page.py
@@ -5,7 +5,7 @@ def test_TC_005_09_01_verify_solar_irradiance_description(driver):
     page.open_page()
     page.verify_description_visibility()
 
-def test_TC_005_09_02_verify_the_most_common_indices_description(driver):
+def test_TC_005_09_03_verify_the_most_common_indices_description(driver):
     page = SolarIrradiancePage(driver, SolarIrradianceLocators.URL_SolarIrradiancePage)
     page.open_page()
     page.verify_indices_visibility()
