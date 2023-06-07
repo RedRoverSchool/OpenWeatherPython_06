ead7734181b07f4e5f1b3cc242304e0ad331c367
Ekaterina <luchik.eg@gmail.com>
2023-06-07 09:28:34 -0500

    Refactoring AT_003.13.01 | Footer > Cookies management module > Verify that the Cookies management module is visible

--- tests/test_group_100000/pages/main_page.py
+++ tests/test_group_100000/pages/main_page.py
@@ -1,6 +1,7 @@
 from pages.base_page import BasePage
 from tests.test_group_100000.locators.main_page_locators import FooterBlockLocators as F
 from tests.test_group_100000.locators.main_page_locators import EightDayForecast as D8
+from tests.test_group_100000.locators.main_page_locators import MainPageLocators
 
 
 class FooterBlock(BasePage):
@@ -23,3 +24,9 @@ class FooterMainPage(BasePage):
         footer = self.driver.find_element(*F.FOOTER_COPYRIGHT)
         assert footer.is_displayed() and expected_footer_text in footer.text, \
             "The footer is not displayed or does not contain the expected text"
+
+class MainPage(BasePage):
+    locators = MainPageLocators()
+    def check_manage_cookies_link_is_visible(self):
+        manage_cookies_btn = self.element_is_visible(self.locators.MANAGE_COOKIES_BTN)
+        assert manage_cookies_btn.is_displayed(), "The Manage cookies module is not visible"
