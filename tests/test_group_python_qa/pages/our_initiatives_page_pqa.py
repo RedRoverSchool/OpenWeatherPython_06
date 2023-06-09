ddaa2919651b632a16b6710593cf9bbd7dd40a4c
Margarita Jewell <rnelyubova@gmail.com>
2023-06-08 21:00:14 -0700

    RF | AT_002_01_11 | Header > Logo > Accessibility from all pages > Verify return to Main page from "Our Initiatives" | deleted unnecessary 'imports'

--- tests/test_group_python_qa/pages/our_initiatives_page_pqa.py
+++ tests/test_group_python_qa/pages/our_initiatives_page_pqa.py
@@ -1,7 +1,4 @@
 from pages.base_page import BasePage
-from tests.test_group_python_qa.locators.locators import OurInitiativesPageLocators
-import requests
-from tests.test_group_python_qa.links.links_all_pages import MAIN_PAGE
 
 
 class OurInitiativesPage(BasePage):
