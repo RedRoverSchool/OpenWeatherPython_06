8a25c2e421b1b408a81def71832fda9a176bb376
regevrita <regevrita@gmail.com>
2023-06-10 15:46:02 +0400

    REF: RF | AT_001.12.07 | unnecessary prints are deleted

--- tests/test_group_ducktales/pages/weather_conditions_page_d.py
+++ tests/test_group_ducktales/pages/weather_conditions_page_d.py
@@ -10,13 +10,11 @@ class WeatherConditionsPage(BasePage):
 
     def check_codes_are_visible(self, driver, table):
         list_of_codes_each_table = driver.find_elements(*locs.ELEMENTS_LOCATOR_CODES(table))
-        print(list_of_codes_each_table[0].text)
         for code in list_of_codes_each_table:
             assert code.is_displayed()
 
 
     def check_desc_are_visible(self, driver, table):
         list_of_desc_each_table = driver.find_elements(*locs.ELEMENTS_LOCATOR_DESC(table))
-        print(list_of_desc_each_table[0].text)
         for desc in list_of_desc_each_table:
             assert desc.is_displayed()
