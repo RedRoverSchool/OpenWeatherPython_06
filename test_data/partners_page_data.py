b451f3765115aff75d1e9f1141ed9e6df11245fd
Yulia Fatkina <juliadigrigorio@hotmail.com>
2023-06-12 20:02:50 +0300

    RF_Final | group_no_pain_no_gain | Yulia Fatkina

--- test_data/partners_page_data.py
+++ test_data/partners_page_data.py
@@ -6,3 +6,8 @@ data = {
                 "JavaScript", "CMS", "Raspberry Pi", "Python", "PHP", "Apache Camel",
                 "Desktop", "Mobile applications", "6,000+ repositories on GitHub"]
 }
+
+PARTNERS_PAGE_TITLE = 'Partners and solutions'
+PARTNERS_PAGE_HEADING = 'Partners and solutions'
+PARTNERS_PAGE_INFO_BOARD_TEXT = "If you have not found a proper solution on this page, just look at the 6,000+ OpenWeatherMap repositories on GitHub"
+INFO_BOARD_BACKGROUND_COLOR = 'rgba(252, 244, 237, 1)'
