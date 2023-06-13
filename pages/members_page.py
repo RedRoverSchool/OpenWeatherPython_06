b7dd991d9e4afb13679109af1f3037b9977f6980
Iryna Kulakova <kulakova.ira@gmail.com>
2023-06-12 22:19:43 -0700

    ref: RF | Final | group_python_qa | Iryna Kulakova

--- pages/members_page.py
+++ pages/members_page.py
@@ -1,6 +1,6 @@
 from pages.base_page import BasePage
 from locators.locators import MembersLocators
-from test_data.members_page_data import EXPECTED_NUMBER_OF_FIELDS
+from test_data.miscellaneous import MembersPageData
 
 
 class MembersPage(BasePage):
@@ -8,4 +8,5 @@ class MembersPage(BasePage):
 
     def verify_error_messages_for_empty_required_fields(self):
         error_messages = self.elements_are_visible(MembersLocators.CANT_BE_BLANK)
-        assert len(error_messages) == EXPECTED_NUMBER_OF_FIELDS, "The number of fields with error massages do not match"
+        assert len(error_messages) == MembersPageData.EXPECTED_NUMBER_OF_FIELDS, \
+        "The number of fields with error massages do not match"
--- test_data/miscellaneous.py
+++ test_data/miscellaneous.py
@@ -1,3 +1,7 @@
 
 class BasePageMisc:
     EXPECTED_LINK_COLOR_HEX = "#e96e50"
+
+
+class MembersPageData:
+    EXPECTED_NUMBER_OF_FIELDS = 7
