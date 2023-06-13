40cdd253ea2be5d9d4b13fbcf847c4fb874b79f6
aigulmustafina <aimustafina2016@gmail.com>
2023-06-13 10:10:49 +0600

    Move old tests in separate directory, refactor yaml files

---
+++ oldTests/test_group_no_pain_no_gain/tests/test_cookies_settings_page_npng.py
@@ -0,0 +1,6 @@
+from oldTests.test_group_no_pain_no_gain.pages.cookies_settings_page import CookiesSettings
+from oldTests.test_group_no_pain_no_gain import links
+
+
+class TestCookiesSettingsPage:
+    pass
