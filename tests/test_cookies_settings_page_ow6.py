280a80adbb1fd181116d0a7266e5c58059a2c804
Kristina Ozhog <97105702+KristinaOzhog@users.noreply.github.com>
2023-06-13 12:00:27 +0100

    Update test_cookies_settings_page_ow6.py
    Updated locators name in cookies settings

--- tests/test_cookies_settings_page_ow6.py
+++ tests/test_cookies_settings_page_ow6.py
@@ -1,9 +1,9 @@
 from pages.cookies_settings_page import CookiesSettingsPage
-from test_data.urls import CookiesSettingsPageUrls
+from test_data.urls import CookiesSettingPageUrls
 
 class TestCookiesSettingsPage:
     def test_TC_001_13_02_verify_the_saving_of_cookies_settings(self, driver, wait):
-        page = CookiesSettingsPage(driver, CookiesSettingsPageUrls.COOKIES_SETTINGS_PAGE_LINK)
+        page = CookiesSettingsPage(driver, CookiesSettingPageUrls.COOKIES_SETTINGS)
         page.open_page()
         page.scroll_page_down_to_chekboxes_and_button()
         page.click_cookies_that_analyse_radiobutton(wait)
