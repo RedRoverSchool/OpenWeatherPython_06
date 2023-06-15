212f650d26818c360021d83d87ffdc2b579c4ee4
Jenny_Kadzova <kadzovaea@gmail.com>
2023-06-15 15:06:06 -0800

    fix: file_handler.py

--- api_testing/utils/FileHandler/file_handler.py
+++ api_testing/utils/FileHandler/file_handler.py
@@ -1,14 +1,15 @@
 import json
+import os
 import requests
 
 
 class FileHandler:
-    def write_into_json_file(self, data, filename):
-        with open(filename, 'w') as file:
+    def write_into_json_file(self, data, file_path):
+        with open(file_path, 'w') as file:
             json.dump(data, file)
 
-    def read_from_json_file(self, filename):
-        with open(filename, 'r') as file:
+    def read_from_json_file(self, file_path):
+        with open(file_path, 'r') as file:
             data = json.load(file)
         return data
 
@@ -18,7 +19,7 @@ class TestRequestHandler(FileHandler):
         url = 'https://reqres.in/api/users?page=2'
         response = requests.get(url)
         data_from_request = response.json()
-        filename = 'data_from_request_response.json'
-        FileHandler.write_into_json_file(self, data_from_request, filename)
-        data_from_file = FileHandler.read_from_json_file(self, filename)
+        file_path = os.path.join(os.path.dirname(__file__), '..', '..', 'data', 'data_from_request_response.json')
+        FileHandler.write_into_json_file(self, data_from_request, file_path)
+        data_from_file = FileHandler.read_from_json_file(self, file_path)
         assert data_from_request == data_from_file, "Data from request is not equal to data from JSON file"
