113257d94f0bf867f1adf70b903e24eceddfa590
ILLIA LEVCHENKO <illia.levchenko.work@gmail.com>
2023-06-16 03:03:58 -0700

    Очередной промежуточный вариант

--- api_testing/utils/handlers/http_handler.py
+++ api_testing/utils/handlers/http_handler.py
@@ -6,6 +6,9 @@
 
 import requests
 import json
+from ..logger import setup_logging
+from ..validator import validator, _load_json_schema
+
 
 class HTTPHandler:
     base_url = "https://reqres.in/api"
@@ -60,55 +63,55 @@ handler = HTTPHandler()
 response = handler.get("users?page=2")
 print(response)
 
-import allure
-import requests
-from src.logger import Logger
-from data.urls import BASE_URL
-
-
-class MyRequests:
-    @staticmethod
-    def post(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
-        with allure.step(f"POST request to URL '{url}'"):
-            return MyRequests._send(url, data, headers, cookies, "POST")
-
-    @staticmethod
-    def get(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
-        with allure.step(f"GET request to URL '{url}'"):
-            return MyRequests._send(url, data, headers, cookies, "GET")
-
-    @staticmethod
-    def put(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
-        with allure.step(f"PUT request to URL '{url}'"):
-            return MyRequests._send(url, data, headers, cookies, "PUT")
-
-    @staticmethod
-    def delete(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
-        with allure.step(f"DELETE request to URL '{url}'"):
-            return MyRequests._send(url, data, headers, cookies, "DELETE")
-
-    @staticmethod
-    def _send(url: str, data: dict, headers: dict, cookies: dict, method: str):
-        url = f"""{BASE_URL}{url}"""
-
-        if headers is None:
-            headers = {}
-        if cookies is None:
-            cookies = {}
-
-        Logger.add_request(url, data, headers, cookies, method)
-
-        if method == "GET":
-            response = requests.get(url, params=data, headers=headers, cookies=cookies)
-        elif method == "POST":
-            response = requests.post(url, data=data, headers=headers, cookies=cookies)
-        elif method == "PUT":
-            response = requests.put(url, data=data, headers=headers, cookies=cookies)
-        elif method == "DELETE":
-            response = requests.delete(url, data=data, headers=headers, cookies=cookies)
-        else:
-            raise Exception(f"""Bad method '{method}' was received""")
-
-        Logger.add_response(response)
-
-        return response
+# import allure
+# import requests
+# from src.logger import Logger
+# from data.urls import BASE_URL
+#
+#
+# class MyRequests:
+#     @staticmethod
+#     def post(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+#         with allure.step(f"POST request to URL '{url}'"):
+#             return MyRequests._send(url, data, headers, cookies, "POST")
+#
+#     @staticmethod
+#     def get(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+#         with allure.step(f"GET request to URL '{url}'"):
+#             return MyRequests._send(url, data, headers, cookies, "GET")
+#
+#     @staticmethod
+#     def put(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+#         with allure.step(f"PUT request to URL '{url}'"):
+#             return MyRequests._send(url, data, headers, cookies, "PUT")
+#
+#     @staticmethod
+#     def delete(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
+#         with allure.step(f"DELETE request to URL '{url}'"):
+#             return MyRequests._send(url, data, headers, cookies, "DELETE")
+#
+#     @staticmethod
+#     def _send(url: str, data: dict, headers: dict, cookies: dict, method: str):
+#         url = f"""{BASE_URL}{url}"""
+#
+#         if headers is None:
+#             headers = {}
+#         if cookies is None:
+#             cookies = {}
+#
+#         Logger.add_request(url, data, headers, cookies, method)
+#
+#         if method == "GET":
+#             response = requests.get(url, params=data, headers=headers, cookies=cookies)
+#         elif method == "POST":
+#             response = requests.post(url, data=data, headers=headers, cookies=cookies)
+#         elif method == "PUT":
+#             response = requests.put(url, data=data, headers=headers, cookies=cookies)
+#         elif method == "DELETE":
+#             response = requests.delete(url, data=data, headers=headers, cookies=cookies)
+#         else:
+#             raise Exception(f"""Bad method '{method}' was received""")
+#
+#         Logger.add_response(response)
+#
+#         return response
