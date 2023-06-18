6724f9cbff5e3ee17e182031a1ec53b91ae00091
Tatsiana Paleshka <ptg.stolin@gmail.com>
2023-06-18 14:41:35 +0300

    Moved RF | AT_002.01.02 | Header > Logo > Verify returning from API page to main page by clicking on Logo

--- pages/api_page.py
+++ pages/api_page.py
@@ -1,5 +1,6 @@
 from pages.base_page import BasePage
 from locators.locators import ApiPageLocators as APL
+from test_data.all_links import Links
 
 
 class APIPage(BasePage):
@@ -29,3 +30,8 @@ class APIPage(BasePage):
         self.driver.find_element(*APL.ONE_CALL_API_3).click()
         expected_link = APL.ONE_CALL_API_LINK
         assert self.driver.current_url == expected_link, "This link is not correct"
+
+    def check_logo(self):
+        self.driver.find_element(*APL.API_LOGO).click()
+        assert self.driver.current_url == Links.URL_MAIN_PAGE
+
