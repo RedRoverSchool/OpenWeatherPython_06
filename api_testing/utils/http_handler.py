dce10f4d8b173f3c02352879d164c4ebabcba30e
ILLIA LEVCHENKO <illia.levchenko.work@gmail.com>
2023-06-17 17:45:44 -0700

    Добавлен IF schemas

--- api_testing/utils/http_handler.py
+++ api_testing/utils/http_handler.py
@@ -18,42 +18,48 @@ class HTTPHandler:
 
     @classmethod
     def get(cls, endpoint, schemas=None):
-        schemas_path_and_name = os.path.join('..', 'utils', 'schemas', schemas)
-        absolute_schemas_path_and_name = os.path.abspath(schemas_path_and_name)
         url = f"{cls.BASE_URL}{endpoint}"
         response = requests.get(url)
-        cls.validate_response(response, absolute_schemas_path_and_name)
+        if schemas:
+            schemas_path_and_name = os.path.join('..', 'utils', 'schemas', schemas)
+            absolute_schemas_path_and_name = os.path.abspath(schemas_path_and_name)
+            cls.validate_response(response, absolute_schemas_path_and_name)
         return response.json()
 
     @classmethod
     def post(cls, endpoint, data, schemas=None):
-        schemas_path_and_name = os.path.join('..', 'utils', 'schemas', schemas)
-        absolute_schemas_path_and_name = os.path.abspath(schemas_path_and_name)
         url = f"{cls.BASE_URL}{endpoint}"
         response = requests.post(url, json=data)
-        cls.validate_response(response, absolute_schemas_path_and_name)
+        if schemas:
+            schemas_path_and_name = os.path.join('..', 'utils', 'schemas', schemas)
+            absolute_schemas_path_and_name = os.path.abspath(schemas_path_and_name)
+            cls.validate_response(response, absolute_schemas_path_and_name)
         return response.json()
 
     @classmethod
     def put(cls, endpoint, data, schemas=None):
-        schemas_path_and_name = os.path.join('..', 'utils', 'schemas', schemas)
-        absolute_schemas_path_and_name = os.path.abspath(schemas_path_and_name)
         url = f"{cls.BASE_URL}{endpoint}"
         response = requests.put(url, json=data)
-        cls.validate_response(response, absolute_schemas_path_and_name)
+        if schemas:
+            schemas_path_and_name = os.path.join('..', 'utils', 'schemas', schemas)
+            absolute_schemas_path_and_name = os.path.abspath(schemas_path_and_name)
+            cls.validate_response(response, absolute_schemas_path_and_name)
         return response.json()
 
     @classmethod
     def patch(cls, endpoint, data, schemas=None):
-        schemas_path_and_name = os.path.join('..', 'utils', 'schemas', schemas)
-        absolute_schemas_path_and_name = os.path.abspath(schemas_path_and_name)
         url = f"{cls.BASE_URL}{endpoint}"
         response = requests.patch(url, json=data)
-        cls.validate_response(response, absolute_schemas_path_and_name)
+        if schemas:
+            schemas_path_and_name = os.path.join('..', 'utils', 'schemas', schemas)
+            absolute_schemas_path_and_name = os.path.abspath(schemas_path_and_name)
+            cls.validate_response(response, absolute_schemas_path_and_name)
         return response.json()
 
     @classmethod
-    def delete(cls, endpoint):
+    def delete(cls, endpoint, schemas=None):
         url = f"{cls.BASE_URL}{endpoint}"
         response = requests.delete(url)
+        if schemas:
+            print('No schemas needed')
         return response.status_code
