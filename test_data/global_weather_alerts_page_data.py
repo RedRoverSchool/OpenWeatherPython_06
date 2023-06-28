13ca1cd8ba6afe8fade9aa4131aec3fa2c1982b1
Denis Prokofyev <denisprokofyev@gmail.com>
2023-06-28 18:38:45 +0300

    AT 005.13.06 Verify 16 body links have the same color (#994)

---
+++ test_data/global_weather_alerts_page_data.py
@@ -0,0 +1,1 @@
+body_link_color = "#e96e50"
--- tests/test_global_weather_alerts_page_ow6.py
+++ tests/test_global_weather_alerts_page_ow6.py
@@ -15,3 +15,8 @@ class TestGlobalWeatherAlertsPage:
         page = GlobalWeatherAlertsPage(driver, ApiPageUrls.GLOBAL_WEATHER_ALERTS_LINK)
         page.open_page()
         page.verify_visibility_and_headings_correctness_of_5_blocks_()
+
+    def test_TC_005_13_06_verify_16_body_links_have_the_same_color(self, driver):
+        page = GlobalWeatherAlertsPage(driver, ApiPageUrls.GLOBAL_WEATHER_ALERTS_LINK)
+        page.open_page()
+        page.verify_16_body_links_have_the_same_color()
