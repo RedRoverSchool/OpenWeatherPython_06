e7382634af48f25873a95b8e78f3f88635efa9c3
Elena <peregovorun@gmail.com>
2023-06-29 18:44:06 +0300

    AT_003.02.02 Footer > Uniform display format > Verify display of Product Collections module on each page specified in Data

--- tests/test_suits_of_pages_ow6.py
+++ tests/test_suits_of_pages_ow6.py
@@ -4,13 +4,6 @@ from pages.main_page import MainPage
 
 
 @pytest.mark.parametrize('URL', SuitsUrls.URLs)
-def test_tc_003_08_05_about_us_link_is_visible_on_each_page_specified_in_data(driver, URL):
-    page = MainPage(driver, link=URL)
-    page.open_page()
-    page.check_about_us_link_is_visible()
-
-
-@pytest.mark.parametrize('URL', SuitsUrls.URLs)
 def test_tc_003_03_01_01_verify_the_product_collections_module_title_is_visible_on_each_page(driver, URL):
     page = MainPage(driver, link=URL)
     page.open_page()
