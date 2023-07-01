42165c3162e464c4f3d8ab682ccb1d8803c55640
Cruz06 <Olarut72@gmail.com>
2023-07-01 16:51:54 +0300

    AT_007.01.10_2 | Marketplace > Weather Conditions > Historical Weather Data by State (2) > Select year 2018 from dropdown list

--- locators/locators.py
+++ locators/locators.py
@@ -281,7 +281,7 @@ class MarketplaceLocators:
     LST_NAME_CITY = (By.XPATH, "//*[contains(text(), 'Paris')]")
     LST_LATITUDE_COORDINATE = (By.XPATH, '//*[@id="app"]/div[2]/div/div[1]/div[3]/table/tbody/tr/td[3]')
     LST_LONGITUDE_COORDINATE = (By.XPATH, '//*[@id="app"]/div[2]/div/div[1]/div[3]/table/tbody/tr/td[4]')
-
+    YEAR_2018 = (By.CSS_SELECTOR, "#__BVID__13 li:first-child")
     FEELS_LIKE_PARAMETER = (By.XPATH, "//*[@id='app']//ul/li[contains(text(),'Feels like')]")
     WEATHER_PAR_MARKERS = (By.XPATH, '//*[@id="app"]/div[2]/div/div[1]/div[2]/div[1]//li')
     HEADER_WEATHER_PAR = (By.XPATH, '//*[@class="accented"][contains(text(),"Weather parameters")]')
